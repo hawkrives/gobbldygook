504ad63ed5c2fb7cf53593667934114ccd525cb3
rives <rives@cdf3a981-b4d7-4157-af83-b01e64c43c1b>
2013-04-27 17:32:30 +0000

    Added a ui.h
    git-svn-id: https://svn.cs.stolaf.edu/project/sds13-gobbldygook@5 cdf3a981-b4d7-4157-af83-b01e64c43c1b

---
+++ ui.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+#ifdef MACOSX
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <sstream>
