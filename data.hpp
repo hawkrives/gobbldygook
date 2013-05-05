b1212e5ca128bd022f763a1ebf1ea506fecddf87
Hawken Rives <hawkrives@gmail.com>
2013-05-05 02:50:58 -0500

    Update data

--- data.hpp
+++ data.hpp
@@ -6,9 +6,59 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>    // has the for_each
-#include "templates.h"
 
 using namespace std;
+
+string tostring(int i) {
+	ostringstream tmp;
+	tmp << i;
+	return tmp.str();
+}
+
+int stringToInt(string const& str) {
+  istringstream i(str);
+  int x;
+  i >> x;
+  return x;
+}
+
+float stringToFloat(string const& str) {
+  istringstream i(str);
+  float x;
+  i >> x;
+  return x;
+}
+
+vector<string> &split(const string &s, char delim, vector<string> &elems) {
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+vector<string> split(const string &s, char delim) {
+	// taken from http://stackoverflow.com/a/236803
+    vector<string> elems;
+    split(s, delim, elems);
+    return elems;
+}
+
+void removeAllQuotes(char *str) {
+	// originally taken from http://stackoverflow.com/a/7144045
+	int len = strlen(str);
+	char lineWithoutQuotes[len];
+	int i, j;
+	if (str[0] != '"')
+		lineWithoutQuotes[0] = str[0];
+	for (i = j = 1; i < strlen(str); i++){
+		if (str[i] == '"' && str[i-1] != '\\')
+			continue;
+		lineWithoutQuotes[j++] = str[i];
+	}
+	strcpy(str, lineWithoutQuotes);
+}
 
 enum GenEd {
 	// Foundation Studies
@@ -67,7 +117,7 @@ protected:
 	int number;
 	string title;
 	string description;
-	string section;
+	char section;
 	
 	vector<Major> majors;
 	Department department;
@@ -75,36 +125,73 @@ protected:
 	vector<Conversation> conversations;
 	vector<Instructor> professor;
 
+	int half_semester;
+	bool pass_fail;
 	float credits;
 	string location;
 
 	bool lab;
-	GenEd geneds;
+	GenEd* geneds;
 
 	bool days[7];
 	float time[7];
 public:
-	Course(istream & is) {
-		string St, Num, L, half, CR, PN, GEreqs, Times, Instructors;
-		//	getline(is, St, ",");
-		//	getline(is, Num, ",");
-		//	getline(is, section, ",");
-		//	getline(is, L, ",");
-		//	getline(is, title, ",");
-		//	getline(is, half, ",");
-		//	getline(is, CR, ",");
-		//	getline(is, PN, ",");
-		//	getline(is, GEreqs, ",");
-		//	getline(is, Times, ",");
-		//	getline(is, location, ",");
-		//	getline(is, Instructors, ",");
-		//	number = stringToInt(Num);
-		if (L != "") {lab = true;}
-		else {lab = false;}
-		//functions to look up instructors and such
+	Course(istream &is) {
+		if (is == 0) return;
+		string tmpLine;
+		getline(is, tmpLine);
+		vector<string> record = split(tmpLine, ',');
+		
+		// Ignore the first column;
+		record.at(0);
+
+		// Second column has the course ID,
+		number = stringToInt(record.at(1));
+
+		// Third column has the section,
+		section = record.at(2)[0];
+
+		// Fourth holds the lab boolean,
+		if (record.at(3).empty())
+			lab = false;
+		else
+			lab = true;
+
+		// while Fifth contains the title of the course;
+		title = record.at(4);
+
+		// Sixth hands over the length (half semester or not)
+		// it's actually an int that tells us how many times the course is offered per semester.
+		half_semester = stringToInt(record.at(5));
+
+		// Seventh tells us the number of credits,
+		credits = stringToFloat(record.at(6));
+
+		// Eighth shows us if it can be taken pass/no-pass, 
+		if (record.at(7) == "Y")
+			pass_fail = true;
+		else
+			pass_fail = false;
+
+		// while Nine gives us the GEs of the course,
+		// GEreqs = record.at(9);
+
+		// and Ten spits out the days and times;
+		// Times = record.at(10);
+
+		// Eleven holds the location,
+		location = record.at(11);
+
+		// and Twelve knows who teaches.
+		// Instructors = record.at(12);
+	}
+	void parseID(char* str) {
+		string tmp = str;
+		ID = tmp;
+		// TODO: do this.
 	}
 	void updateID() {
-		ID = department.shorthand + lexical_cast<string>(number) + section;
+		ID = department.shorthand + tostring(number) + section;
 	}
 	string getID() {
 		return ID;
@@ -124,15 +211,8 @@ public:
 ostream &operator<<(ostream &os, Course &item) { return item.getData(os); }
 void Course::display() { cout << *this << endl; }
 
-Course spit(Course n) {
-	return n;
-}
-
-int stringToInt(string s){
-	int thevalue;
-	istringstream ss(s);
-	ss >> thevalue;
-	return thevalue;
+void spit(Course n) {
+	n.display();
 }
 
 class Student {
