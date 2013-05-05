386008a6fe0e0411a30241b560ac0379b1679944
rives <rives@cdf3a981-b4d7-4157-af83-b01e64c43c1b>
2013-05-05 17:01:43 +0000

    Update data
    git-svn-id: https://svn.cs.stolaf.edu/project/sds13-gobbldygook@56 cdf3a981-b4d7-4157-af83-b01e64c43c1b

--- data.cpp
+++ data.cpp
@@ -4,10 +4,16 @@ int main() {
 	ifstream g("data/courses-s2-1213.csv");
 	vector<Course> courses;
 
+	int i = 0;
+	string dummyLine;
+	getline(g, dummyLine);
 	while (!g.eof()) {
 		Course incourse(g);
 		courses.push_back(incourse);
+		incourse.display();
+		i++; cout << i << endl;
 	}
-
+	
 	for_each(courses.begin(), courses.end(), spit);
 }
+
