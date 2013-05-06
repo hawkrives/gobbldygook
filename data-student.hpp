7382336fd7a042993afcd0bad22da1a515193eb5
xandrasings <xandrasings@yahoo.com>
2013-05-05 20:50:42 -0500

    migrated student class out of data-general.hpp

---
+++ data-student.hpp
@@ -0,0 +1,33 @@
+#ifndef __Data_student__
+#define __Data_student__
+
+//data-student.hpp
+#include "data-general.hpp"
+
+class Student {
+public:
+	string name;
+	vector<Course> takenCourses;
+	vector<Major> majors;
+	vector<Concentration> concentrations;
+	vector<Conversation> conversations;
+	vector<Instructor> favInstructors;
+
+	string interests;
+	double gradYear;
+
+	ostream& getData(ostream &os) {
+		os << name << " ";
+
+		for (vector<Course>::iterator i = takenCourses.begin(); i != takenCourses.end(); ++i)
+			os << i->getID() << " ";
+
+		for (vector<Major>::iterator i = majors.begin(); i != majors.end(); ++i)
+			os << i->name << " ";
+
+		return os;
+	}
+	void display();
+};
+
+#endif
