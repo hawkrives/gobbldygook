fe752b056965cdec5ea6d1c193ed0415ddc45055
Hawken Rives <hawkrives@gmail.com>
2013-05-08 17:05:37 -0500

    Restart work on data-test

--- data-test.cpp
+++ data-test.cpp
@@ -1,12 +1,7 @@
 #include "data-general.hpp"
-#include "data-concentration.hpp"
-#include "data-conversation.hpp"
 #include "data-course.hpp"
-#include "data-department.hpp"
-#include "data-instructor.hpp"
 #include "data-major.hpp"
 #include "data-student.hpp"
-
 
 int main() {
 	ifstream g("data/2012-13-s2.csv");
@@ -15,11 +10,9 @@ int main() {
 	string dummyLine;
 	getline(g, dummyLine);
 
-	//	while (g.peek() != -1){
-	//		Course incourse(g);
-	//		courses.push_back(incourse);
-	//		cout << incourse << endl;
-	//}
-
-	cout << JsonBox::loadFromFile("data/user.json") << endl;
+	while (g.peek() != -1){
+		Course incourse(g);
+		courses.push_back(incourse);
+		cout << incourse << endl;
+	}
 }
