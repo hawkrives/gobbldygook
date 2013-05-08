38cf8fdbad7604ece1380673ffaa67fc4b879e16
Hawken Rives <hawkrives@gmail.com>
2013-05-08 18:49:23 -0500

    Switch data-department to tabs

---
+++ data-department.hpp
@@ -0,0 +1,67 @@
+#ifndef __Data_department__
+#define __Data_department__
+
+#include "data-general.hpp"
+using namespace std;
+
+enum Department {
+	AFAM,  //0 Africa and the Americas
+	ALSO,  //1 Alternate Language Study Option
+	AMCON, //2 American Conversation
+	AMST,  //3 American Studies
+	ARMS,  //4 American Racial and Multicultural Studies
+	ART,   //5 Art and Art History
+	ASIAN, //6 Asian Studies (AS)
+	BIO,   //7 Biology (BI)
+	BMOLS, //8 Biomolecular Science
+	CHEM,  //9 Chemistry (CH)
+	CHIN,  //10 Chinese
+	CLASS, //11 Classics
+	CSCI,  //12 Computer Science
+	DANCE, //13 Dance
+	ECON,  //14 Economics
+	EDUC,  //15 Education
+	ENGL,  //16 English
+	ENVST, //17 Environmental Studies (ES)
+	ESAC,  //18 Exercise Science Activity
+	ESTH,  //19 Exercise Science Theory
+	FAMST, //20 Family Studies
+	FILM,  //21 Film Studies
+	FREN,  //22 French
+	GCON,  //23 Great Conversation
+	GERM,  //24 German
+	GREEK, //25 Greek
+	HIST,  //26 History (HI)
+	HSPST, //27 Hispanic Studies
+	ID,    //28 Interdisciplinary
+	IDFA,  //29 
+	INTD,  //30 Interdepartmental
+	IS,    //31 Integrative Studies
+	JAPAN, //32 Japanese
+	LATIN, //33 Latin
+	MATH,  //34 Mathematics
+	MEDIA, //35 Media Studies
+	MEDVL, //36 Medieval Studies
+	MGMT,  //37 Management Studies
+	MUSIC, //38 Music
+	MUSPF, //39 Music Performance
+	NEURO, //40 Neuroscience
+	NORW,  //41 Norwegian
+	NURS,  //42 Nursing
+	PHIL,  //43 Philosphy
+	PHYS,  //44 Physics
+	PSCI,  //45 Political Science
+	PSYCH, //46 Psychology
+	REL,   //47 Religion (RE)
+	RUSSN, //48 Russian
+	SCICN, //49 Science Conversation
+	SOAN,  //50 Sociology and Anthropology
+	SPAN,  //51 Spanish
+	STAT,  //52 Statistics
+	SWRK,  //53 Social Work
+	THEAT, //54 Theater
+	WMGST, //55 Women's & Gender Studies
+	WRIT,  //56 Writing
+};
+
+#endif
