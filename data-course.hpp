9ad92329e53d5e5d4b2a63b7bf9ec0f0579e23f9
Hawken Rives <hawkrives@gmail.com>
2013-05-08 23:20:51 -0500

    Update parseID to use the Department class' handy helper functions

--- data-course.hpp
+++ data-course.hpp
@@ -87,88 +87,19 @@ public:
 		professor = record.at(12);
 	}
 
-	int parseID(string s) {
-		// return stringToInt(s.substr(s.find(' ')+1,3)); // Xandra, what is this?
-		string tempDept;
-		unsigned int found = tempDept.find('/');
-		tempDept = s.substr(0,s.find(' ')-1);
-		if (found != s.npos) {
-			department = new Department[2];
-			string tmp = tempDept.substr(0,2);
+	void parseID(string str) {
+		unsigned int foundLoc = str.find('/');
+		string tempDept = str.substr(0,str.find(' ')-1);
 
-			     if ( tmp == "AS" ) department[0] = ASIAN;
-			else if ( tmp == "BI" ) department[0] = BIO;
-			else if ( tmp == "CH" ) department[0] = CHEM;
-			else if ( tmp == "ES" ) department[0] = ENVST;
-			else if ( tmp == "HI" ) department[0] = HIST;
-			else if ( tmp == "RE" ) department[0] = REL;
+		if (foundLoc != str.npos) {
+			string dept1 = tempDept.substr(0,2);
+			department.push_back(Department(dept1));
 
-			tmp = tempDept.substr(3,2);
-			     if ( tmp == "AS" ) department[1] = ASIAN;
-			else if ( tmp == "BI" ) department[1] = BIO;
-			else if ( tmp == "CH" ) department[1] = CHEM;
-			else if ( tmp == "ES" ) department[1] = ENVST;
-			else if ( tmp == "HI" ) department[1] = HIST;
-			else if ( tmp == "RE" ) department[1] = REL;
+			string dept2 = tempDept.substr(2,2);
+			department.push_back(Department(dept2));
 		}
 		else {
-			     if ( tempDept == "AFAM"  ) department = AFAM;
-			else if ( tempDept == "ALSO"  ) department = ALSO;
-			else if ( tempDept == "AMCON" ) department = AMCON;
-			else if ( tempDept == "AMST"  ) department = AMST;
-			else if ( tempDept == "ARMS"  ) department = ARMS;
-			else if ( tempDept == "ART"   ) department = ART;
-			else if ( tempDept == "ASIAN" ) department = ASIAN;
-			else if ( tempDept == "BIO"   ) department = BIO;
-			else if ( tempDept == "BMOLS" ) department = BMOLS;
-			else if ( tempDept == "CHEM"  ) department = CHEM;
-			else if ( tempDept == "CHIN"  ) department = CHIN;
-			else if ( tempDept == "CLASS" ) department = CLASS;
-			else if ( tempDept == "CSCI"  ) department = CSCI;
-			else if ( tempDept == "DANCE" ) department = DANCE;
-			else if ( tempDept == "ECON"  ) department = ECON;
-			else if ( tempDept == "EDUC"  ) department = EDUC;
-			else if ( tempDept == "ENGL"  ) department = ENGL;
-			else if ( tempDept == "ENVST" ) department = ENVST;
-			else if ( tempDept == "ESAC"  ) department = ESAC;
-			else if ( tempDept == "ESTH"  ) department = ESTH;
-			else if ( tempDept == "FAMST" ) department = FAMST;
-			else if ( tempDept == "FILM"  ) department = FILM;
-			else if ( tempDept == "FREN"  ) department = FREN;
-			else if ( tempDept == "GCON"  ) department = GCON;
-			else if ( tempDept == "GERM"  ) department = GERM;
-			else if ( tempDept == "GREEK" ) department = GREEK;
-			else if ( tempDept == "HIST"  ) department = HIST;
-			else if ( tempDept == "HSPST" ) department = HSPST;
-			else if ( tempDept == "ID"    ) department = ID;
-			else if ( tempDept == "IDFA"  ) department = IDFA;
-			else if ( tempDept == "INTD"  ) department = INTD;
-			else if ( tempDept == "IS"    ) department = IS;
-			else if ( tempDept == "JAPAN" ) department = JAPAN;
-			else if ( tempDept == "LATIN" ) department = LATIN;
-			else if ( tempDept == "MATH"  ) department = MATH;
-			else if ( tempDept == "MEDIA" ) department = MEDIA;
-			else if ( tempDept == "MEDVL" ) department = MEDVL;
-			else if ( tempDept == "MGMT"  ) department = MGMT;
-			else if ( tempDept == "MUSIC" ) department = MUSIC;
-			else if ( tempDept == "MUSPF" ) department = MUSPF;
-			else if ( tempDept == "NEURO" ) department = NEURO;
-			else if ( tempDept == "NORW"  ) department = NORW;
-			else if ( tempDept == "NURS"  ) department = NURS;
-			else if ( tempDept == "PHIL"  ) department = PHIL;
-			else if ( tempDept == "PHYS"  ) department = PHYS;
-			else if ( tempDept == "PSCI"  ) department = PSCI;
-			else if ( tempDept == "PSYCH" ) department = PSYCH;
-			else if ( tempDept == "REL"   ) department = REL;
-			else if ( tempDept == "RUSSN" ) department = RUSSN;
-			else if ( tempDept == "SCICN" ) department = SCICN;
-			else if ( tempDept == "SOAN"  ) department = SOAN;
-			else if ( tempDept == "SPAN"  ) department = SPAN;
-			else if ( tempDept == "STAT"  ) department = STAT;
-			else if ( tempDept == "SWRK"  ) department = SWRK;
-			else if ( tempDept == "THEAT" ) department = THEAT;
-			else if ( tempDept == "WMGST" ) department = WMGST;
-			else if ( tempDept == "WRIT"  ) department = WRIT;
+			department.push_back(Department(tempDept));
 		}
 	}
 
