ec60269fa562a81d86ce2d9247e3fa69ad26cc90
Hawken Rives <hawkrives@gmail.com>
2013-05-10 00:33:32 -0500

    reindent data-majorRequirement

--- data-majorRequirement.hpp
+++ data-majorRequirement.hpp
@@ -6,20 +6,22 @@ using namespace std;
 
 class MajorRequirement {
 private:
-  int needed;
-  int has;
-  bool satisfied;
-  vector<Course> validCourses;
+	int needed;
+	int has;
+	bool satisfied;
+	vector<Course> validCourses;
 public:
-  bool fulfillsRequirement(const Course& c) {
-    for (vector<Course>::iterator i=validCourses.begin(); i!=validCourses.end(); ++i) {
-      if (*i==c)
-	return true;
-    }
-    return false;
-  }
+	bool fulfillsRequirement(const Course& c) {
+		for (vector<Course>::iterator i=validCourses.begin(); i!=validCourses.end(); ++i) {
+			if (*i==c)
+				return true;
+		}
+		return false;
+	}
 
-  void incrementHas() {
-    ++has;
-  }
-}
+	void incrementHas() {
+		++has;
+	}
+};
+
+#endif
