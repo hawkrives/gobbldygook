d7b1d27fa6afa209d22b38fa95a11f110040a8bc
Hawken Rives <hawkrives@gmail.com>
2013-05-12 12:45:50 -0500

    Simplify the ostream bits of Student::getData

--- data-student.cpp
+++ data-student.cpp
@@ -78,14 +78,15 @@ bool Student::hasTakenCourse(string str) {
 }
 
 ostream& Student::getData(ostream &os) {
-	os << name << ", ";
-	os << "you are majoring in ";
+	os << name << ", you are majoring in ";
+	
 	for (vector<Major>::iterator i = majors.begin(); i != majors.end(); ++i){
 		if (majors.size() == 2) {
+			os << *i;
 			if (i != majors.end()-1)
-				os << *i << " and ";
+				os << " and ";
 			else
-				os << *i << " ";
+				os << " ";
 		}
 		else {
 			if (i != majors.end()-1)
@@ -94,9 +95,11 @@ ostream& Student::getData(ostream &os) {
 				os << "and " << *i << ", ";
 		}
 	}
+
 	os << "while taking:" << endl;
 	for (vector<Course>::iterator i = courses.begin(); i != courses.end(); ++i)
 		os << *i << endl;
+
 	return os;
 }
 
