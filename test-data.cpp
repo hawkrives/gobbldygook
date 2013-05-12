8a261c746eafada6b18b5155380a4102020785b6
Hawken Rives <hawkrives@gmail.com>
2013-05-12 01:23:34 -0500

    Update the testcases

--- test-data.cpp
+++ test-data.cpp
@@ -21,47 +21,30 @@ void loadCourses() {
 	// 			c->displayMany();
 }
 
-void whatDidICallThisWith(int argc, const char *argv[]) {
-	printf ("This program was called with \"%s\".\n",argv[0]);
-
-	if (argc > 1)
-		for (int count = 1; count < argc; count++)
-			printf("argv[%d] = %s\n", count, argv[count]);
-	else
-		printf("The command had no other arguments.\n");
-}
-
 void welcome() {
 	string name, yearS = "", yearE = "", majors;
-	// cout << "Welcome!" << endl;
-	// cout << "What is your name? ";
-	// getline(cin, name);
-	name = "Xandra Best";
-	// cout << "What year do you graduate? ";
-	// cin >> yearE;
-	// cout << "What are your majors (ex. CSCI, ASIAN) ";
-	// getline(cin, majors);
-	majors = "CSCI, STAT, AsIaN, math";
+	cout << "Welcome!" << endl;
+	cout << "What is your name? ";
+	getline(cin, name);
+	cout << "What year do you graduate? ";
+	getline(cin, yearE);
+	cout << "What are your majors (ex. CSCI, ASIAN) ";
+	getline(cin, majors);
 	user = Student(name, yearS, yearE, majors);
 }
 
 void getCourses() {
 	string courses;
-	// cout << "What are some courses that you have taken? (ex. CSCI125, STAT 110)" << endl;
-	// cout << "> ";
-	// getline(cin, courses);
-	courses = "PHIL 251A,REL296A,rel 296b, STAT 110,";
-	courses += "CSCI251, stat 110, THEAT398, writ211";
+	cout << "What are some courses that you have taken? (ex. CSCI125, STAT 110)" << endl;
+	getline(cin, courses);
 	user.addCourses(courses);
 }
 
 int main(int argc, const char *argv[]) {
 	loadCourses();
-	// cout << getCourse("STAT 10") << endl;
-	// Course c("BIO 126");
-	// cout << c << endl;
-	welcome();
-	getCourses();
+	// welcome();
+	// getCourses();
+	Student user("data/user.json");
 	user.display();
 
 	cout << "Question: Has the user taken CSCI 251? " << user.hasTakenCourse("CSCI251") << endl;
