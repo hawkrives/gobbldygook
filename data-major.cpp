c203d6f333e98293bda2936027218a690cb09e99
Hawken Rives <hawkrives@gmail.com>
2013-05-11 19:12:21 -0500

    Renovated Major
    Actually, this was more like a full-scale gutting and rebuilding, but
    whatever.

--- data-major.cpp
+++ data-major.cpp
@@ -1,2 +1,43 @@
 #include "data-major.hpp"
 using namespace std;
+
+Major::Major() {
+	cout << "Called Major constructor with nothing." << endl;
+	name = "None";
+	department = Department("NONE");
+}
+
+Major::Major(string str) {
+	// cout << "Called Major constructor with string '" << str << "'" << endl;
+	department = Department(str);
+	name = department.getFullName();
+}
+
+Major::Major(const Major &c) {
+	copy(c);
+}
+
+Major& Major::operator= (const Major &c) {
+	if (this == &c) return *this;
+	copy(c);
+	return *this;
+}
+
+void Major::copy(const Major &c) {
+	name = c.name;
+	department = c.department;
+	requirements = c.requirements;
+}
+
+ostream& Major::getData(ostream &os) {
+	os << name;
+	return os;
+}
+
+ostream &operator<<(ostream &os, Major &item) { 
+	return item.getData(os); 
+}
+
+void Major::display() {
+	cout << *this << endl;
+}
--- data-major.hpp
+++ data-major.hpp
@@ -3,13 +3,25 @@
 
 #include "data-general.hpp"
 #include "data-majorRequirement.hpp"
+#include "data-department.hpp"
 using namespace std;
 
 class Major {
 private:
 	string name;
-	// int difficulty;
+	Department department;
 	vector<MajorRequirement> requirements;
+	void copy(const Major &c);
+public:
+	Major();
+	Major(string str);
+	Major(const Major& c);
+	Major& operator= (const Major &c);
+
+	ostream& getData(ostream &os);
+	void display();
 };
+
+ostream &operator<<(ostream &os, Major &item);
 
 #endif
