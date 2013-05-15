a23ad3b8c797f10768d5cb429fac155c47b39ab0
Hawken Rives <hawkrives@gmail.com>
2013-05-14 21:02:18 -0500

    Add a requirement subclass

---
+++ requirement.hpp
@@ -0,0 +1,8 @@
+#ifndef __gobbldygook__requirement__
+#define __gobbldygook__requirement__
+
+#include "general.hpp"
+#include "id.hpp"
+using namespace std;
+
+#endif /* defined(__gobbldygook__requirement__) */
