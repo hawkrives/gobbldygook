36d126d63c03deaf3fbda587cb78be92ba61a50e
Hawken Rives <hawkrives@gmail.com>
2013-05-15 00:02:04 -0500

    Make Xcode happy

--- requirement.cpp
+++ requirement.cpp
@@ -4,27 +4,27 @@ Requirement::Requirement() {}
 
 Requirement::Requirement(const Requirement &c) {
 	copy(c);
-};
+}
 
 Requirement Requirement::operator= (const Requirement &c) {
 	if (this == &c) return *this;
 	copy(c);
 	return *this;
-};
+}
 
 void Requirement::init(string n, int need) {
 	name = n;
 	needed = need;
 	has = 0;
 	satisfied = false;
-};
+}
 
 void Requirement::copy(const Requirement &c) {
 	name = c.name;
 	needed = c.needed;
 	has = c.has;
 	satisfied = c.satisfied;
-};
+}
 
 void Requirement::incrementHas() {
 	++has;
