1ab1aaca8f0509f7b7260395507851bdc6acc5c0
Hawken Rives <hawkrives@gmail.com>
2013-05-15 21:53:26 -0500

    Add initial drafts of GenEd

---
+++ gened.cpp
@@ -0,0 +1,11 @@
+#include "gened.hpp"
+
+void GenEd::init(string n) {
+
+}
+
+GenEd::GenEd() {
+	init("", 0);
+}
+
+GenEd::GenEd(string str) {};
---
+++ gened.hpp
@@ -0,0 +1,33 @@
+#ifndef __gobbldygook__gened__
+#define __gobbldygook__gened__
+
+#include "general.hpp"
+#include "requirement.hpp"
+
+class GenEd : public Requirement {
+public: //todo: make private
+	void init(string n);
+	void copy(const GenEd &c);
+public:
+	GenEd();
+	GenEd(string str);
+	GenEd(string str, int n);
+	GenEd(const MajorRequirement &c);
+	GenEd operator= (const GenEd &c);
+	
+	bool fulfillsRequirement(const ID& c);
+	void addCourse(const ID& c);
+	bool checkCourseValidity(const ID& c);
+	
+	friend bool operator== (const GenEd &l, const GenEd&r);
+	friend bool operator!= (const GenEd &l, const GenEd &r);
+	friend bool operator== (const GenEd &l, const Requirement &r);
+	friend bool operator!= (const GenEd &l, const Requirement &r);
+	
+	ostream& getData(ostream &os);
+	void display();
+};
+
+ostream &operator<<(ostream &os, GenEd &item);
+
+#endif /* defined(__gobbldygook__gened__) */
