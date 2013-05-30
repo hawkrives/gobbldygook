fdf53f67e54b177e1022e720a34b4263d5876e4e
Hawken Rives <hawkrives@gmail.com>
2013-05-29 21:23:36 -0500

    Add some command-line arguments to the program

--- test-data.cpp
+++ test-data.cpp
@@ -15,14 +15,6 @@ void loadCourses(string filename) {
 		all_courses.push_back(incourse);
 		// cout << incourse << endl;
 	}
-
-	// for (vector<Course>::iterator c = all_courses.begin(); c != all_courses.end(); ++c) {
-	// 	if (c->getProfessor()[1] == ' ')
-	// 		if (c->getProfessor()[2] == '0' || c->getProfessor()[2] == '1')
-	// 			cout << *c << endl;
-	// 	if (c->getDepartment(0) == NONE)
-	// 		cout << *c << endl;
-	// }
 }
 
 void readData() {
@@ -39,40 +31,81 @@ void readData() {
 	loadCourses("data/2010-11-s1.csv");
 }
 
-void welcome() {
-	string name, yearS = "", yearE = "", majors;
-	cout << "Welcome!" << endl;
-	cout << "What is your name? ";
-	getline(cin, name);
-	cout << "What year do you graduate? ";
-	getline(cin, yearE);
-	cout << "What are your majors (ex. CSCI, ASIAN) ";
-	getline(cin, majors);
-	user = Student(name, yearS, yearE, majors);
-}
-
-void requestCourses() {
-	string courses;
-	cout << "What are some courses that you have taken? (ex. CSCI125, STAT 110)" << endl;
-	getline(cin, courses);
-	user.addCourses(courses);
+vector<string> parseArguments(int argc, const char* argv[]) {
+//	cout << argc << endl;
+	vector<string> arguments;
+	for (int i = 1; i < argc; i++) {
+//		cout << argv[i] << endl;
+		arguments.push_back(argv[i]);
+	}
+	return arguments;
 }
 
 int main(int argc, const char *argv[]) {
+	vector<string> arguments = parseArguments(argc, argv);
 	readData();
 	
-	// Method 1: Dynamic.
-	// welcome();
-	// requestCourses();
+	if (arguments.size() > 0) {
+		string arg0 = arguments.at(0);
+		if (arg0 == "--find" || arg0 == "-f") {
+			if (arguments.size() == 2) {
+				Course c = Course(arguments.at(1));
+				c.display();
+			}
+		}
 
-	// Method 2: Hard-coded file path.
-	user = Student("data/example.txt");
-	
-	// Method 3: File path as an argument.
-	// Student user(argv[1]);
+		else if (arg0 == "--load" || arg0 == "-l") {
+			if (arguments.size() == 2) {
+				user = Student(arguments.at(1));
+				user.display();
+			}
+		}
 
-	user.updateStanding();
-	user.display();
+		else if (arg0 == "--demo") {
+			user = Student("data/example.txt");
+			user.display();
+		}
+
+		else if (arg0 == "--stress") {
+			user = Student("data/stress.txt");
+			user.display();
+		}
+		
+		else if (arg0 == "--debug") {
+			for (vector<Course>::iterator c = all_courses.begin(); c != all_courses.end(); ++c) {
+				if (c->getProfessor()[1] == ' ')
+					if (c->getProfessor()[2] == '0' || c->getProfessor()[2] == '1')
+						cout << *c << endl;
+				if (c->getDepartment(0) == NONE)
+					cout << *c << endl;
+			}
+		}
+
+		else if (arg0 == "--interactive" || arg0 == "-i") {
+			string name, yearS = "", yearE = "", majors;
+			cout << "Welcome!" << endl;
+			cout << "What is your name? ";
+			getline(cin, name);
+			cout << "What year do you graduate? ";
+			getline(cin, yearE);
+			cout << "What are your majors (ex. CSCI, ASIAN) ";
+			getline(cin, majors);
+			user = Student(name, yearS, yearE, majors);
+			
+			string courses;
+			cout << "What are some courses that you have taken? (ex. CSCI125, STAT 110)" << endl;
+			getline(cin, courses);
+			user.addCourses(courses);
+			
+			user.display();
+		}
+	}
+	else {
+		cout << "This program works best if you give it some data ;)" << endl;
+		cout << "However, we have some example stuff to show you anyway." << endl;
+		user = Student("data/example.txt");
+		user.display();
+	}
 
 	return 0;
 }
