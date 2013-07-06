a7c85ecc107e9a88158f046135ba37b139eea622
Hawken Rives <hawkrives@gmail.com>
2013-07-06 08:59:49 -0500

    Add boolean comparison operators to Concentration

--- concentration.hpp
+++ concentration.hpp
@@ -14,8 +14,19 @@ public:
 	
 	ostream& getData(ostream &os);
 	void display();
+
+	friend bool operator== (const Concentration &l, const Concentration &r);
+	friend bool operator!= (Concentration &l, Concentration &r);
 };
 
 ostream &operator<<(ostream &os, Major &item);
+
+inline bool operator== (const Concentration &l, const Concentration &r) {
+    return (l == r);
+}
+
+inline bool operator!= (Concentration &l, Concentration &r) {
+    return !(l == r);
+}
 
 #endif
