5916f3f953201ea1c4173d7fa6eccf1fe4e3c6eb
Hawken Rives <hawkrives@gmail.com>
2013-07-06 09:28:04 -0500

    GenEd shouldn't inherit from Requirement
    They're too different.

--- gened.cpp
+++ gened.cpp
@@ -1,1 +1,67 @@
 #include "gened.hpp"
+
+bool GenEd::checkSatisfied() {
+	if (has >= needed)
+		satisfied = true;
+	else
+		satisfied = false;
+	return satisfied;
+}
+
+void GenEd::incrementHas() {
+	++has;
+	checkSatisfied();
+}
+
+void GenEd::decrementHas() {
+	--has;
+	checkSatisfied();
+}
+
+string GenEd::getName() {
+	return name;
+}
+
+int GenEd::getNeeded() {
+	return needed;
+}
+
+int GenEd::getHas() {
+	return has;
+}
+
+bool GenEd::getSatisfied() {
+	return checkSatisfied();
+}
+
+void GenEd::setName(string str) {
+	name = str;
+}
+
+void GenEd::setNeeded(int n) {
+	needed = n;
+}
+
+ostream& GenEd::getData(ostream &os) {
+	if (satisfied)
+		cout << "✅";
+	else
+		cout << "❌";
+	os << "  ";
+	os << name;
+	os << " needs " << needed;
+	if (satisfied)
+		os << ", and has ";
+	else
+		os << ", but has ";
+	os << has << ".";
+	return os;
+}
+
+ostream &operator<<(ostream &os, GenEd &item) {
+	return item.getData(os);
+}
+
+void GenEd::display() {
+	cout << *this << endl;
+}
--- gened.hpp
+++ gened.hpp
@@ -2,26 +2,56 @@
 #define __gobbldygook__gened__
 
 #include "general.hpp"
-#include "requirement.hpp"
+#include "id.hpp"
+using namespace std;
 
-class GenEd : public Requirement {
+class GenEd {
+protected:
+	string name = "";
+	int needed = 0;
+	int has = 0;
+	bool satisfied = false;
+
+	bool checkSatisfied();
+
 public:
 	GenEd() : GenEd("", 0) {}
 	GenEd(string str) : GenEd(str, 0) {}
-	GenEd(string str, int n) : Requirement(str, n) {}
+	GenEd(string str, int n) : name(str), needed(n) {}
 
 //	bool fulfillsRequirement(const ID& c);
 //	void addCourse(const ID& c);
 
-//	friend bool operator== (const GenEd &l, const GenEd&r);
-//	friend bool operator!= (const GenEd &l, const GenEd &r);
-//	friend bool operator== (const GenEd &l, const Requirement &r);
-//	friend bool operator!= (const GenEd &l, const Requirement &r);
+	void incrementHas();
+	void decrementHas();
 
-//	ostream& getData(ostream &os);
-//	void display();
+	string getName();
+	int getNeeded();
+	int getHas();
+	bool getSatisfied();
+
+	void setName(string str);
+	void setNeeded(int n);
+
+	friend bool operator== (const GenEd &l, const GenEd &r);
+	friend bool operator!= (const GenEd &l, const GenEd &r);
+
+	ostream& getData(ostream &os);
+	void display();
 };
 
-//ostream &operator<<(ostream &os, GenEd &item);
+ostream &operator<<(ostream &os, GenEd &item);
+
+inline bool operator== (const GenEd &l, const GenEd &r) {
+	bool name = (l.name == r.name);
+	bool needed = (l.needed == r.needed);
+	bool has = (l.has == r.has);
+	bool satisfied = (l.satisfied == r.satisfied);
+    return (name && needed && has && satisfied);
+}
+
+inline bool operator!= (GenEd &l, GenEd &r) {
+    return !(l == r);
+}
 
 #endif /* defined(__gobbldygook__gened__) */
