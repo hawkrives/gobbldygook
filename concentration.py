770f9dd75a223fb31a18af2fcb089398663f2065
Hawken Rives <hawkrives@gmail.com>
2013-07-13 20:49:07 -0500

    Add a getConcentrationRequirement to corrospond to getMajorRequirement

--- concentration.py
+++ concentration.py
@@ -4,6 +4,9 @@ class Concentration(Major):
 	def __init__(self, dept="NONE"):
 		super().__init__(dept, path="concentrations/")
 
+	def getConcentrationRequirement(self, string):
+		return self.requirements[string]
+
 if __name__ == '__main__':
 	tmp = [
 		Concentration(dept="Asian")
