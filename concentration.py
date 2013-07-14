30d1bc4ed93176364c5ba6c229eeece6a3e30a81
Hawken Rives <hawkrives@gmail.com>
2013-07-13 19:47:34 -0500

    Name the path argument in Concentration super().__init__

--- concentration.py
+++ concentration.py
@@ -2,7 +2,7 @@ from major import Major
 
 class Concentration(Major):
 	def __init__(self, dept="NONE"):
-		super().__init__(dept, "concentrations/")
+		super().__init__(dept, path="concentrations/")
 
 if __name__ == '__main__':
 	tmp = [
