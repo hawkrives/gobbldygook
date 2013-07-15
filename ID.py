47a233f56d06c4c224e89d0376029cd368fb9817
Hawken Rives <hawkrives@gmail.com>
2013-07-14 21:26:26 -0500

    Update the regex in ID to process sections

--- ID.py
+++ ID.py
@@ -7,15 +7,16 @@ class ID:
 			self.department = Department(dept)
 			self.number = int(num)
 		else:
-			regex = "(?i)(([a-z]*)(?=/)(/)([a-z]*)|[a-z]*) *(\d*)"
-			replace = "\g<1> \g<5>"
-			result = re.sub(regex, replace, combined)
-			sides = result.split()
-			self.department = Department(sides[0])
-			self.number = int(sides[1])
 
 		self.section = sec
 
+			regex = "(?P<dept>([a-z]*)(?=/)(/)([a-z]*)|[a-z]*) *(?P<num>[0-9]*) *(?P<sec>[a-z]*)"
+			match = re.search(regex, combined, re.I | re.S)
+			result = match.groupdict()
+			self.department = Department(result["dept"])
+			self.number = int(result["num"])
+			if not section:
+				self.section = result["sec"]
 
 	def __str__(self):
 		return str(self.department) + " " + str(self.number) + self.section
