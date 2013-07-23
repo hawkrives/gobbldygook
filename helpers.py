10334ccb7a213f149aa64f91a4f23ee49ddb3779
Hawken Rives <hawkrives@gmail.com>
2013-07-23 17:03:23 -0500

    Update get_readable_list
    There do still seem to be some problems with the outputing of extra
    newlines

--- helpers.py
+++ helpers.py
@@ -1,16 +1,28 @@
-def get_readable_list(passed_list, sep=', '):
+def get_readable_list(passed_list, sep=', ', end=''):
 	output = ""
-	for i, item in enumerate(passed_list):
-		if len(passed_list) is 1:
-			output += str(item)
-
-		else:
-			if i is not (len(passed_list) - 1):
-				output += str(item) + sep
-			else:
+	if isinstance(passed_list, list):
+		for i, item in enumerate(passed_list):
+			if len(passed_list) is 1:
 				output += str(item)
 
-	return output
+			else:
+				if i is not (len(passed_list) - 1):
+					output += str(item) + sep
+				else:
+					output += str(item)
+
+	elif isinstance(passed_list, dict):
+		for i, item in enumerate(passed_list.values()):
+			if len(passed_list) is 1:
+				output += str(item)
+
+			else:
+				if i is not (len(passed_list) - 1):
+					output += str(item) + sep
+				else:
+					output += str(item)
+
+	return output + end
 
 
 def get_list_as_english(passed_list):
