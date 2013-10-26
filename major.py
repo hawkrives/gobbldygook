6f4bcba403891927745dd5e69f0c4a20b09c0b78
Hawken Rives <hawkrives@gmail.com>
2013-10-26 14:04:37 -0500

    Switch the arg in Major.__init__ from path to folder; remove support for txt-based files.

--- major.py
+++ major.py
@@ -9,7 +9,7 @@ except ImportError:
     from yaml import Loader, Dumper
 
 class Major:
-	def __init__(self, dept="NONE", path="majors/"):
+	def __init__(self, dept="NONE", folder="majors/"):
 		self.department = Department(dept)
 		self.name = self.department.name()
 
@@ -17,15 +17,12 @@ class Major:
 		self.specialRequirements = []
 		self.setRequirements = []
 
-		filepath = (
-			path + self.department.abbr() + ".yaml",
-			path + self.department.abbr() + ".txt"
-		)
+		filepath = folder + self.department.abbr() + ".yaml"
 
-		if os.path.isfile(filepath[0]):
-			self.parseYAML(filepath[0])
+		if os.path.isfile(filepath):
+			self.parseYAML(filepath)
 		else:
-			self.parseTXT(filepath[1])
+			print("Could not find major", dept)
 
 	def __str__(self):
 		return self.name
@@ -64,153 +61,6 @@ class Major:
 					req = data['special'][requirement_title]
 					special_req = SpecialRequirement(req['description'], req['needed'], req['valid'])
 					self.specialRequirements.append(special_req)
-
-
-
-	def parseTXT(self, path):
-		currentHeading = ""
-		currentRequirement = ""
-		
-		with open(path) as infile:
-			for line in infile:
-				# Make sure that the string is uppercase
-				line = line.upper()
-				# print(line)
-
-				if not line:  # Skip the rest of this iteration of the loop because the line is blank
-					continue
-
-				elif line.startswith("#") and not line.startswith("##"):
-					currentHeading = self.parseHeading(line)
-
-				elif line.startswith("##"):
-					currentRequirement = self.parseSubHeading(line, currentHeading)
-
-				elif line.startswith("//"):
-					pass
-
-				self.parseContent(line, currentRequirement, currentHeading)
-
-
-	def addMajorRequirement(self, req):
-		if not isinstance(req, MajorRequirement):
-			req = MajorRequirement(req)
-		self.requirements.append(req)
-		# self.requirements[req.name] = req
-
-	def addSpecialRequirement(self, req):
-		if not isinstance(req, SpecialRequirement):
-			req = SpecialRequirement(req)
-		self.specialRequirements.append(req)
-
-	def addSetRequirement(self, req):
-		if not isinstance(req, MajorRequirement):
-			req = MajorRequirement(req)
-		self.setRequirements.append(req)
-
-
-	def getRequirement(self, req, passed_list):
-		if req in passed_list:
-			idx = passed_list.index(req)
-			return passed_list[idx]
-		else:
-			return None
-
-	def getMajorRequirement(self, string):
-		req = MajorRequirement(string)
-		return self.getRequirement(req, self.requirements)
-
-	def getSpecialRequirement(self, string):
-		req = SpecialRequirement(string)
-		return self.getRequirement(req, self.specialRequirements)
-
-	def getSetRequirement(self, string):
-		req = MajorRequirement(string)
-		return self.getRequirement(req, self.setRequirements)
-
-
-	def parseContent(self, string, currentRequirement, currentHeading):
-		if '=' in string:
-			sides = self.parseContentLine(string)
-			# print(sides)
-
-			if sides[0] == "NAME":
-				self.name = sides[1]
-
-
-			elif sides[0] == "DEPARTMENT":
-				self.department = Department(sides[1])
-
-
-			elif "NEEDED" in sides[0]:
-				if currentHeading == "REQUIREMENTS":
-					self.getMajorRequirement(currentRequirement).needed = int(sides[1])
-					# print(self.getMajorRequirement(currentRequirement))
-
-				elif currentHeading == "SPECIAL":
-					self.getSetRequirement(currentRequirement).needed = int(sides[1])
-					# print(self.getSetRequirement(currentRequirement))
-
-				elif currentHeading == "SETS":
-					self.getSpecialRequirement(currentRequirement).needed = int(sides[1])
-					# print(self.getSpecialRequirement(currentRequirement))
-
-
-			elif sides[0] == "VALIDCOURSES":
-				validCourseList = sides[1].split(',')
-
-				for courseID in validCourseList:
-					if currentHeading == "REQUIREMENTS":
-						self.getMajorRequirement(currentRequirement).addCourse(courseID)
-
-					elif currentHeading == "SETS":
-						self.getSetRequirement(currentRequirement).addCourse(courseID)
-
-
-			elif sides[0] == "VALIDSETS":
-				# print("validsets")
-				validSetList = sides[1].split(',')
-				specialReq = self.getSpecialRequirement(currentRequirement)
-
-				for setID in validSetList:
-					specialReq.addSet(self.setRequirements[setID])
-
-
-	def parseContentLine(self, string):
-		# print(string)
-		sides = string.split('=')
-		for side in sides:
-			side = side.strip()
-		return sides
-
-
-	def parseHeading(self, string):
-		# We can assume that, if we are inside this block, there is only one octothorpe at the beginning of the line.
-
-		string = string.strip('#')  # Remove the octothorpe
-		string = string.strip()     # and whitespace
-
-		return string
-
-
-	def parseSubHeading(self, string, currentHeading):
-		# Remove the octothorps from the beginning of the requirement title
-		string = string.strip('#')
-
-		# Remove any spaces from the beginning of the requirement title
-		requirementTitle = string.strip()
-
-		if currentHeading == "REQUIREMENTS":
-			self.addMajorRequirement(MajorRequirement(requirementTitle))
-
-		elif currentHeading == "SETS":
-			self.addSpecialRequirement(SpecialRequirement(requirementTitle))
-
-		elif currentHeading == "SPECIAL":
-			# um, why are these MajorRequirements?
-			self.addSetRequirement(MajorRequirement(requirementTitle))
-
-		return requirementTitle
 
 
 if __name__ == '__main__':
