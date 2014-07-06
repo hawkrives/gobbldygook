1590132d4c7e677bcf6cc5dba9eff4a80771655f
Hawken Rives <hawkrives@gmail.com>
2014-07-06 14:38:50 -0500

    Update structure of text in studentSummary

--- client/models/studentSummary.js
+++ client/models/studentSummary.js
@@ -30,14 +30,22 @@ var StudentSummary = React.createClass({
 		return React.DOM.article( {id: 'student-summary', className: canGraduate ? 'can-graduate' : 'cannot-graduate'},
 			React.DOM.div( {id: 'student-letter' }, this.props.name[0] ),
 			React.DOM.p(null, "Hi, ", this.props.name, "!"),
-			React.DOM.p(null, 
-				"You are planning on ", 
-				degrees, " ", degreeWord, 
+			React.DOM.p(null,
+				"You are planning on ",
+				_.size(degreeObjects === 1) ? "a " : "",
+				React.DOM.span({className: 'area-of-study-list'}, degrees),
+				" ", degreeWord,
 				", with ", majorWord, " in ",
-				majors, ", and ", concentrationWord, 
-				" in ", concentrations, "."),
-			React.DOM.p(null, canGraduate ? "It looks like you'll make it! Just follow the plan, and double-check my output with your advisor a few times." : "You haven't managed to plan everything out yet. Ask your advisor if you need help fitting everything in."),
-			React.DOM.p(null, "You have ", React.DOM.output(null, creditsTaken), " of ", React.DOM.output(null, creditsNeeded), " credits.")
+				React.DOM.span({className: 'area-of-study-list'}, majors),
+				", and ", concentrationWord,
+				" in ",
+				React.DOM.span({className: 'area-of-study-list'}, concentrations),
+				"."),
+			React.DOM.p(null, 
+				canGraduate 
+				? "It looks like you'll make it! Just follow the plan, and double-check my output with your advisor a few times." 
+				: "You haven't planned everything out yet. Ask your advisor if you need help fitting everything in."),
+			React.DOM.p(null, "You have planned for ", React.DOM.output(null, creditsTaken), " of ", React.DOM.output(null, creditsNeeded), " credits.")
 		)
 	}
 })
