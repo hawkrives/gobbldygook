35ec45e09cb7d5dd3fe0331567fca8660dd4f2a1
Hawken Rives <hawkrives@gmail.com>
2014-07-06 11:58:15 -0500

    Update the student summary object

--- client/models/studentSummary.js
+++ client/models/studentSummary.js
@@ -13,20 +13,30 @@ var StudentSummary = React.createClass({
 		var creditsTaken = countCredits(this.props.courses)
 		var creditsNeeded = this.props.creditsNeeded
 
-		var degrees = _.reduce(_.pluck(_.filter(this.props.studies, 'degrees'), 'abbr'), 
-			concatenateStringAsEnglish)
-		var majors = _.reduce(_.pluck(_.filter(this.props.studies, 'majors'), 'title'), concatenateStringAsEnglish)
-		var concentrations = _.reduce(_.pluck(_.filter(this.props.studies, 'concentrations'), 'title'), concatenateStringAsEnglish)
+		var degreeObjects = _.filter(this.props.studies, {type: 'degree'})
+		var majorObjects = _.filter(this.props.studies, {type: 'major'})
+		var concentrationObjects = _.filter(this.props.studies, {type: 'concentration'})
+
+		var degrees = humanize.oxford(_.pluck(degreeObjects, 'title'))
+		var majors = humanize.oxford(_.pluck(majorObjects, 'title'))
+		var concentrations = humanize.oxford(_.pluck(concentrationObjects, 'title'))
+
+		var degreeWord = humanize.pluralize(_.size(degreeObjects), 'degree')
+		var majorWord = humanize.pluralize(_.size(majorObjects), 'major')
+		var concentrationWord = humanize.pluralize(_.size(concentrationObjects), 'concentration')
 
 		var canGraduate = checkElegibilityForGraduation(this.props)
 
-		return React.DOM.article( {id: 'student-summary'},
+		return React.DOM.article( {id: 'student-summary', className: canGraduate ? 'can-graduate' : 'cannot-graduate'},
 			React.DOM.p(null, "Hi, ", this.props.name, "!"),
-			You are planning on a [degrees] degree, with majors in [majors], and concentrations in [concentrations].
-			It looks like you'll make it! Just follow the plan, and double-check me with your advisor a few times.
-			-- or --
-			You haven't managed to plan everything out yet. Ask your advisor if you need help fitting everything in.
-			React.DOM.p(null, "You have ", React.DOM.output(null, creditsTaken), " of ", React.DOM.output(null, creditsNeeded), " credits!")
+			React.DOM.p(null, 
+				"You are planning on ", 
+				degrees, " ", degreeWord, 
+				", with ", majorWord, " in ",
+				majors, ", and ", concentrationWord, 
+				" in ", concentrations, "."),
+			React.DOM.p(null, canGraduate ? "It looks like you'll make it! Just follow the plan, and double-check my output with your advisor a few times." : "You haven't managed to plan everything out yet. Ask your advisor if you need help fitting everything in."),
+			React.DOM.p(null, "You have ", React.DOM.output(null, creditsTaken), " of ", React.DOM.output(null, creditsNeeded), " credits.")
 		)
 	}
 })
