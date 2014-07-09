0c113c1fac0ccc77a58d25bf73f39638cc341603
Hawken Rives <hawkrives@gmail.com>
2014-07-08 23:01:50 -0500

    Go back to bluebird

--- client/helpers/read.js
+++ client/helpers/read.js
@@ -1,40 +1,40 @@
-var Q = require("q")
+var Promise = require("bluebird")
 
 function xhrSuccess(req) {
 	return (req.status === 200 || (req.status === 0 && req.responseText));
 }
 
 function read(url) {
-	var request = new XMLHttpRequest();
-	var response = Q.defer();
-
-	function onload() {
-		if (xhrSuccess(request)) {
-			response.resolve(request.responseText);
-		} else {
-			onerror();
-		}
-	}
-
-	function onerror() {
-		response.reject("Can't XHR " + JSON.stringify(url));
-	}
-
-	try {
-		request.open("GET", url, true);
-		request.onreadystatechange = function () {
-			if (request.readyState === 4) {
-				onload();
+	return new Promise(function(resolve, reject) {
+		var request = new XMLHttpRequest();
+		
+		function onload() {
+			if (xhrSuccess(request)) {
+				resolve(request.responseText);
+			} else {
+				onerror();
 			}
-		};
-		request.onload = request.load = onload;
-		request.onerror = request.error = onerror;
-	} catch (exception) {
-		response.reject(exception.message, exception);
-	}
+		}
 
-	request.send();
-	return response.promise;
-};
+		function onerror() {
+			reject("Can't XHR " + JSON.stringify(url));
+		}
+
+		try {
+			request.open("GET", url, true);
+			request.onreadystatechange = function () {
+				if (request.readyState === 4) {
+					onload();
+				}
+			};
+			request.onload = request.load = onload;
+			request.onerror = request.error = onerror;
+		} catch (exception) {
+			reject(exception.message, exception);
+		}
+
+		request.send();
+	})
+}
 
 module.exports = read
