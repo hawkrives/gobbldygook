7b7cfe6146054ffa79219a8d16981dbcebe02c87
Hawken Rives <hawkrives@gmail.com>
2014-07-11 11:26:40 -0500

    Framework for DraggableContainerMixin

---
+++ client/mixins/draggableContainer.js
@@ -0,0 +1,12 @@
+var React = require('react')
+
+var DraggableContainerMixin = {
+	getDefaultProps: function () {
+	},
+	getInitialState: function () {
+	},
+	componentDidUpdate: function (props, state) {
+	},
+}
+
+module.exports = DraggableContainerMixin
