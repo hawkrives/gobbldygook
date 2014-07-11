a016f7871681ce6ee23656a98c834c15fb874e20
Hawken Rives <hawkrives@gmail.com>
2014-07-11 11:14:53 -0500

    Prototype of a Draggable mixing for React components

---
+++ client/mixins/draggable.js
@@ -0,0 +1,99 @@
+var DraggableMixin = {
+	getDefaultProps: function () {
+		return {
+			// allow the initial position to be passed in as a prop
+			initialPos: {x: 0, y: 0}
+		}
+	},
+	getInitialState: function () {
+		return {
+			pos: this.props.initialPos,
+			dragging: false,
+			rel: null // position relative to the cursor
+		}
+	},
+
+	// we could get away with not having this (and just having the listeners on
+	// our div), but then the experience would be possibly be janky. If there's
+	// anything w/ a higher z-index that gets in the way, then you're toast,
+	// etc.
+	componentDidUpdate: function (props, state) {
+		if (this.state.dragging && !state.dragging) {
+			document.addEventListener('mousemove', this.onMouseMove)
+			document.addEventListener('mouseup', this.onMouseUp)
+		} else if (!this.state.dragging && state.dragging) {
+			document.removeEventListener('mousemove', this.onMouseMove)
+			document.removeEventListener('mouseup', this.onMouseUp)
+		}
+	},
+
+	offset: function() {
+		var rect = this.getDOMNode().getBoundingClientRect()
+
+		return {
+			top: rect.top + document.body.scrollTop,
+			left: rect.left + document.body.scrollLeft
+		}
+	},
+
+	// calculate relative position to the mouse and set dragging=true
+	onMouseDown: function (e) {
+		// only left mouse button
+		if (e.button !== 0) {
+			return;
+		}
+
+		var pos = this.offset()
+
+		this.setState({
+			dragging: true,
+			rel: {
+				x: e.pageX - pos.left,
+				y: e.pageY - pos.top
+			}
+		})
+		e.stopPropagation()
+		e.preventDefault()
+	},
+	onMouseUp: function (e) {
+		this.setState({dragging: false})
+		e.stopPropagation()
+		e.preventDefault()
+	},
+	onMouseMove: function (e) {
+		if (!this.state.dragging) return
+		this.setState({
+			pos: {
+				x: e.pageX - this.state.rel.x,
+				y: e.pageY - this.state.rel.y
+			}
+		})
+		e.stopPropagation()
+		e.preventDefault()
+	},
+}
+
+var DraggableDiv = React.createClass({
+	mixins: [DraggableMixin],
+	render: function () {
+		// transferPropsTo will merge style & other props passed into our component to also be on the child DIV.
+		return this.transferPropsTo(React.DOM.div({
+			className: this.state.dragging ? 'dragging' : '',
+			onMouseDown: this.onMouseDown,
+			style: {
+				position: 'absolute',
+				left: this.state.pos.x + 'px',
+				top: this.state.pos.y + 'px'
+			}
+		}, this.props.children))
+	}
+})
+
+// React.renderComponent(Draggable({
+// 	initialPos: {x: 50, y: 100},
+// 	className: 'my-draggable',
+// 	style: {
+// 		//border: '2px solid #aa5',
+// 		padding: '10px'
+// 	}
+// }, 'Drag Me! See how children are passed through to the div!'), document.body)
