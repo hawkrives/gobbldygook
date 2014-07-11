761083039e06759de920d7442c0c4dbb16d4166d
Hawken Rives <hawkrives@gmail.com>
2014-07-11 11:12:37 -0500

    Mock up course objects with just the CLBID

--- client/models/course.js
+++ client/models/course.js
@@ -2,9 +2,32 @@ var _ = require('lodash')
 var React = require('react')
 
 var Course = React.createClass({
+	getInitialState: function() {
+	},
+	componentDidMount: function() {
+
+	},
+	componentWillUnmount: function() {
+
+	},
 	render: function() {
-		console.log('course render')
-		return React.DOM.div(null, this.props.info.clbid);
+		return React.DOM.article({className: 'course'},
+			React.DOM.h1({className: 'title'}, this.props.info.clbid),
+			React.DOM.span({className: 'details'},
+				React.DOM.span({className: 'identifier'}, 
+					React.DOM.span({className: 'department'}, new String(this.props.info.clbid).substr(0, 3)),
+					' ',
+					React.DOM.span({className: 'number'}, new String(this.props.info.clbid).slice(-3)),
+					this.props.info.sect ? React.DOM.span({className: 'section'}, this.props.info.clbid) : ''
+				),
+				React.DOM.span({className: 'professors'}, 
+					Math.random() > 0.5 ? 'Mr.' : 'Ms.', ' ',
+					this.props.info.clbid,
+					' ',
+					this.props.info.clbid
+				)
+			)
+		)
 	}
 })
 
