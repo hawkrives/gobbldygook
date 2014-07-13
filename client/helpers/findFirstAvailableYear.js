625a73c00ffb08e5414f56956c47893444ab2980
Hawken Rives <hawkrives@gmail.com>
2014-07-13 18:22:52 -0500

    Update finFirstAvailableYear to return the current year if no schedules

--- client/helpers/findFirstAvailableYear.js
+++ client/helpers/findFirstAvailableYear.js
@@ -1,14 +1,18 @@
 var _ = require('lodash')
 var findMissingNumberBinarySearch = require('./findMissingNumberBinarySearch')
 
-// Takes a list of schedules and finds the first open year.
-// If they go [2012, 2013, 2015] findFirstAvailableYear will
-// return 2014. If it goes [2013, 2014, 2015] findFirstAvailableYear
-// will return 2016. Etc.
+// Takes a list of schedules and finds the first open year. If they go [2012,
+// 2013, 2015] findFirstAvailableYear will return 2014. If it goes [2013,
+// 2014, 2015] findFirstAvailableYear will return 2016. If schedules is empty,
+// it will return the current year.
 
 function findFirstAvailableYear(schedules) {
+	if (_.isEmpty(schedules)) {
+		return new Date().getFullYear()
+	}
+
 	var years =
-		_(schedules)
+		_.chain(schedules)
 			.sortBy('year')
 			.pluck('year')
 			.uniq()
