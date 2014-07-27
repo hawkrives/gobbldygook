147e44dec9cfc51c974dc46ea486d4f85b855bca
Hawken Rives <hawkrives@gmail.com>
2014-07-26 23:05:43 -0500

    Check for time conflicts in Semester

--- client/models/semester.js
+++ client/models/semester.js
@@ -1,10 +1,12 @@
 var _ = require('lodash')
 var React = require('react')
+var humanize = require('humanize-plus')
 
 var CourseList = require('./courseList')
 var Course = require('./course')
 
 var getCourses = require('../helpers/getCourses').getCourses
+var checkScheduleTimeConflicts = require('../helpers/time').checkScheduleTimeConflicts
 
 var isCurrentTermSchedule = _.curry(function(year, semester, schedule) {
 	return (schedule.year.val() === year && schedule.semester.val() === semester)
@@ -12,11 +14,11 @@ var isCurrentTermSchedule = _.curry(function(year, semester, schedule) {
 
 var Semester = React.createClass({
 	putActiveCoursesIntoState: function() {
-		var active = this.findActiveSchedules()
-		var clbids = _.uniq(active.val().clbids)
+		var active = this.findActiveSchedules().val()
+		var clbids = _.uniq(active.clbids)
 
 		var self = this
-		getCourses(clbids).then(function(courses) {
+		return getCourses(clbids).then(function(courses) {
 			console.log('retrieved ' + courses.length + ' courses for semester')
 			self.setState({
 				courses: courses
@@ -25,14 +27,16 @@ var Semester = React.createClass({
 	},
 	getInitialState: function() {
 		return {
-			courses: []
+			courses: [],
+			isValid: true,
+			conflicts: []
 		}
 	},
 	componentWillReceiveProps: function() {
-		this.putActiveCoursesIntoState()
+		this.putActiveCoursesIntoState().then(this.validateSchedule)
 	},
 	componentDidMount: function() {
-		this.putActiveCoursesIntoState()
+		this.putActiveCoursesIntoState().then(this.validateSchedule)
 	},
 	semesterName: function() {
 		if      (this.props.semester === 1) return 'Fall';
@@ -64,12 +68,45 @@ var Semester = React.createClass({
 		// 	}
 		// })
 	},
+	validateSchedule: function() {
+		// Checks to see if the schedule is valid
+
+		// Step one: do any times conflict?
+		var courses = this.state.courses
+		var conflicts = checkScheduleTimeConflicts(courses)
+
+		var hasConflict = _.chain(conflicts)
+			.toArray() // turn the object into an array
+			.map(_.toArray) // and each of the nested objects, too
+			.flatten() // flatten the nested arrays
+			.any() // and see if any of the resulting values are true
+			.value()
+
+		this.setState({
+			isValid: !hasConflict,
+			conflicts: conflicts
+		})
+
+		console.log('schedule conflicts', conflicts, hasConflict)
+
+		return _.any([hasConflict])
+	},
 	render: function() {
 		console.log('semester render')
 
 		return React.DOM.div( {className:"semester"},
 			React.DOM.header({className: "semester-title"},
 				React.DOM.h1(null, this.semesterName()),
+				React.DOM.ul({className: 'bar'},
+					React.DOM.li(
+						{className: 'semester-course-count'},
+						_.size(this.state.courses) + ' ' + humanize.pluralize(_.size(this.state.courses), 'course')
+					),
+					this.state.isValid ? null : React.DOM.li(
+						{className: 'semester-status'},
+						this.state.conflicts
+					)
+				),
 				React.DOM.button({
 					className: "remove-semester",
 					title: "Remove " + String(this.props.year) + ' ' + this.semesterName(),
