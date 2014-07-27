8f4b4c561f2f1d440759e9790852b34dc7727dc9
Hawken Rives <hawkrives@gmail.com>
2014-07-27 13:20:04 -0500

    Create queryCourse helpers

---
+++ client/helpers/deptNum.js
@@ -0,0 +1,43 @@
+var _ = require('lodash')
+
+var queryCourses = require('./queryCourses')
+
+function buildDeptNum(deptNumString) {
+	var combined = deptNumString.toUpperCase()
+	var regex = /(([A-Z]+)(?=\/)(?:\/)([A-Z]+)|[A-Z]+) *([0-9]+) *([A-Z]?)/gi
+	var match = regex.exec(combined)
+	// "AS/RE 230A" -> ["AS/RE 230A", "AS/RE", "AS", "RE", "230", "A"]
+
+	var result = {}
+	result.dept = {values: _.contains(match[1], '/') ? [match[2], match[3]] : [match[1]]}
+	result.num  = {values: [match[4]]}
+	if (match[5]) {
+		result.sect = {values: [match[5]]}
+	}
+
+	console.log('built deptNum', result)
+
+	return result
+}
+
+function getCourseFromDeptNum(deptNumString) {
+	var deptNum = buildDeptNum(deptNumString)
+	return queryCourses(deptNum)
+}
+
+function checkCoursesForDeptNum(deptNumString, courses) {
+	return new Promise(function(resolve, reject) {
+		console.log('checkCoursesForDeptNum', 'deptNum', deptNumString, 'courses', courses)
+		var crsidsToCheckAgainst = _.chain(courses).pluck('crsid').uniq().value()
+
+		getCourseFromDeptNum(deptNumString).then(function(crsidToCheckFor) {
+			crsidToCheckFor = _.uniq(_.pluck(courses, 'crsid'))
+			console.log('checkCoursesForDeptNum crsids', crsidToCheckFor, 'against', crsidsToCheckAgainst)
+			resolve(_.contains(crsidsToCheckAgainst, crsidToCheckFor))
+		})
+	})
+}
+
+module.exports.buildDeptNum = buildDeptNum
+module.exports.getCourseFromDeptNum = getCourseFromDeptNum
+module.exports.checkCoursesForDeptNum = checkCoursesForDeptNum
---
+++ client/helpers/queryCourses.js
@@ -0,0 +1,45 @@
+var _ = require('lodash')
+var Promise = require("bluebird")
+
+// query = {
+// 		'title': {values: ['Chinese'], flags: ['caseInsensitive', 'partialMatch']},
+// 		'year': {values: [2009, 2010], bool: 'OR'},
+// 		'level': {values: [200], flags: ['>=']},
+// 		'type': {values: ['Lab', 'Research', 'Topic'], bool: 'NOT', flags: ['caseInsensitive']}
+// }
+
+var checkSingleQuery = _.curry(function(query, valueToCheck, against) {
+	if (_.isArray(valueToCheck) || _.isString(valueToCheck)) {
+		return _.contains(valueToCheck, against)
+	} else {
+		return valueToCheck === against
+	}
+})
+
+var checkAgainstQuery = _.curry(function(queryObject, course) {
+	var results = {}
+
+	_.each(queryObject, function(singleQuery, key) {
+		if (!singleQuery.bool || singleQuery.bool === 'AND') {
+			results[key] = _.all(singleQuery.values, checkSingleQuery(singleQuery, course[key]))
+		} else if (singleQuery.bool === 'OR') {
+			results[key] = _.any(singleQuery.values, checkSingleQuery(singleQuery, course[key]))
+		} else if (singleQuery.bool === 'NOT') {
+			results[key] = !_.any(singleQuery.values, checkSingleQuery(singleQuery, course[key]))
+		}
+	})
+
+	return _.all(results)
+})
+
+function queryCourses(query) {
+	return new Promise(function(resolve, reject) {
+		window.server.courses.query()
+			.filter(checkAgainstQuery(query))
+			.execute()
+			.done(resolve)
+	})
+}
+
+window.queryCourses = queryCourses
+module.exports = queryCourses
