c5315a659612bce2bb4e7c33e0ddb3f267f36877
Hawken Rives <hawkrives@gmail.com>
2014-07-28 21:03:27 -0500

    Move the fetching functions to getCourses and create buildDeptNum

--- client/helpers/deptNum.js
+++ client/helpers/deptNum.js
@@ -16,30 +16,9 @@ function splitDeptNum(deptNumString) {
 	}
 }
 
-function getCourseFromDeptNum(deptNumString) {
-	var deptNum = buildDeptNum(deptNumString)
-	return new Promise(function(resolve, reject) {
-		window.server.courses.query('deptnum')
-			.only('deptnum', deptNumString)
-			.execute()
-			.done(resolve)
-	})
-}
-
-function checkCoursesForDeptNum(deptNumString, courses) {
-	return new Promise(function(resolve, reject) {
-		// console.log('checkCoursesForDeptNum', 'deptNum', deptNumString, 'courses', courses)
-		var crsidsToCheckAgainst = _.chain(courses).pluck('crsid').uniq().value()
-
-		getCourseFromDeptNum(deptNumString).then(function(crsidToCheckFor) {
-			crsidToCheckFor = _.uniq(_.pluck(courses, 'crsid'))
-			console.log('checkCoursesForDeptNum crsids', crsidToCheckFor, 'against', crsidsToCheckAgainst)
-			resolve(_.contains(crsidsToCheckAgainst, crsidToCheckFor))
-		})
-	})
+function buildDeptNum(course) {
+	return course.depts.join('/') + ' ' + course.num + (course.sect || '')
 }
 
 module.exports.splitDeptNum = splitDeptNum
 module.exports.buildDeptNum = buildDeptNum
-module.exports.getCourseFromDeptNum = getCourseFromDeptNum
-module.exports.checkCoursesForDeptNum = checkCoursesForDeptNum
--- client/helpers/getCourses.js
+++ client/helpers/getCourses.js
@@ -34,5 +34,40 @@ function getCourses(clbids) {
 	return Promise.all(_.map(clbids, getCourse))
 }
 
+function getCourseFromDeptNum(deptNumString) {
+	// Filter to only those with matching dept strings
+	return window.db.courses
+		.query('deptnum')
+		.only(deptNumString)
+		.filter('type', 'Research')
+		.limit(1)
+		.execute()
+		.then(function(courses) {
+			return courses[0]
+		})
+}
+
+function checkCoursesForDeptNum(deptNumString, courses) {
+	var crsidsToCheckAgainst = _.chain(courses).pluck('crsid').uniq().value()
+
+	return getCourseFromDeptNum(deptNumString)
+		.then(function(course) {
+			if (course) {
+				console.log(
+					'checkCoursesForDeptNum',
+					'checking for', course.crsid, 'in', crsidsToCheckAgainst,
+					'result', _.contains(crsidsToCheckAgainst, course.crsid))
+				return _.contains(crsidsToCheckAgainst, course.crsid)
+			}
+		}).catch(function(err) {
+			console.error('checkCoursesForDeptNum error', err.stack)
+		})
+}
+
 module.exports.getCourses = getCourses
 module.exports.getCourse = getCourse
+
+module.exports.getCourseFromDeptNum = getCourseFromDeptNum
+module.exports.checkCoursesForDeptNum = checkCoursesForDeptNum
+
+window.getCourses = module.exports
