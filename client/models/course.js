ab394d893dcbdda380c460e878c31a53322a10a1
Hawken Rives <hawkrives@gmail.com>
2014-07-28 21:06:55 -0500

    Remove an end-of-line space

--- client/models/course.js
+++ client/models/course.js
@@ -9,7 +9,7 @@ var Course = React.createClass({
 		return React.DOM.article({className: 'course'},
 			React.DOM.h1({className: 'title'}, this.props.info.title),
 			React.DOM.span({className: 'details'},
-				React.DOM.span({className: 'identifier'}, 
+				React.DOM.span({className: 'identifier'},
 					React.DOM.span({className: 'department'}, this.props.info.depts.join('/')),
 					' ',
 					React.DOM.span({className: 'number'}, this.props.info.num),
