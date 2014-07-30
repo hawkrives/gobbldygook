a41c462c2600eaa196be3aff6ea7357cfb92259d
Hawken Rives <hawkrives@gmail.com>
2014-07-29 20:58:50 -0500

    Add functions to partialTitle

--- client/helpers/partialTitle.js
+++ client/helpers/partialTitle.js
@@ -4,4 +4,14 @@ var partialTitle = _.curry(function(partial, course) {
 	return _.contains(course.title, partial)
 })
 
-module.exports = partialTitle
+var partialName = _.curry(function(partial, course) {
+	return _.contains(course.name, partial)
+})
+
+var partialNameOrTitle = _.curry(function(partial, course) {
+	return (partialTitle(partial, course) || partialName(partial, course))
+})
+
+module.exports.partialTitle = partialTitle
+module.exports.partialName = partialName
+module.exports.partialNameOrTitle = partialNameOrTitle
