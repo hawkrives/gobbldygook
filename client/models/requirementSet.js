8a7ad2c4dc8474dbb44232326109ff5a61e098ac
Hawken Rives <hawkrives@gmail.com>
2014-07-29 21:46:53 -0500

    Comment out chunks of RequirementSet for a while

--- client/models/requirementSet.js
+++ client/models/requirementSet.js
@@ -5,21 +5,24 @@ var Requirement = require('./requirement')
 
 var RequirementSet = React.createClass({
 	render: function() {
-		console.log('requirement-set render')
-		var requirements = _.map(this.props.requirements, function(req) {
-			return Requirement({key: req.title,
-				name: req.title, needs: req.needs,
-				validCourses: req.courses,
-				query: req.query,
-				courses: this.props.courses
-			})
-		}, this);
+		// console.log('requirement-set render')
+		// var requirements = _.map(this.props.requirements, function(req) {
+		// 	return Requirement({key: req.title,
+		// 		name: req.title, needs: req.needs,
+		// 		validCourses: req.courses,
+		// 		query: req.query,
+		// 		courses: this.props.courses
+		// 	})
+		// }, this);
+
+		// if (this.props.type === )
+
 		return (
 			React.DOM.div({className: 'requirement-set'},
-				React.DOM.h2(null, this.props.name, ' (', this.props.needs === true ? 'All' : this.props.needs, this.props.count, ')'),
-				React.DOM.ul({className: 'requirement-list'},
-					requirements
-				)
+				React.DOM.h2(null, this.props.title)//,
+				// React.DOM.ul({className: 'requirement-list'},
+				// 	requirements
+				// )
 			)
 		)
 	}
