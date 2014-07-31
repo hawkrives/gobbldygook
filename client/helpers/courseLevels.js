906dc81bedcdebdd6eaf836bed1d15adf58fd22a
Hawken Rives <hawkrives@gmail.com>
2014-07-30 20:34:14 -0500

    Create filtering functions for courseLevels

--- client/helpers/courseLevels.js
+++ client/helpers/courseLevels.js
@@ -1,9 +1,22 @@
 var _ = require('lodash')
 
-module.exports.coursesAtOrAboveLevel = _.curry(function(level, course) {
+var coursesAtOrAboveLevel = _.curry(function(level, course) {
 	return course.level >= level
 })
 
-module.exports.coursesAtLevel = _.curry(function(level, course) {
+var onlyCoursesAtOrAboveLevel = _.curry(function(level, courses) {
+	return _.filter(courses, coursesAtOrAboveLevel(level))
+})
+
+var coursesAtLevel = _.curry(function(level, course) {
 	return course.level === level
 })
+
+var onlyCoursesAtLevel = _.curry(function(level, courses) {
+	return _.filter(courses, coursesAtLevel(level))
+})
+
+module.exports.coursesAtOrAboveLevel = coursesAtOrAboveLevel
+module.exports.onlyCoursesAtOrAboveLevel = onlyCoursesAtOrAboveLevel
+module.exports.coursesAtLevel = coursesAtLevel
+module.exports.onlyCoursesAtLevel = onlyCoursesAtLevel
