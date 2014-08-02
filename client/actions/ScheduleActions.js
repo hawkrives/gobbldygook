252a8576eb8c0acc18ae6e104a53996720c32867
Hawken Rives <hawkrives@gmail.com>
2014-08-02 12:42:12 -0500

    Integrate ScheduleService into ScheduleActions

--- client/actions/ScheduleActions.js
+++ client/actions/ScheduleActions.js
@@ -1,12 +1,30 @@
 var Fluxy = require('fluxy')
+var uuid = require('node-uuid')
+var mori = require('mori')
+var Promise = require('bluebird')
 
 var ScheduleConstants = require('../constants/ScheduleConstants')
-var ScheduleService = require('../services/ScheduleService')
+
+function randomChar() {
+	// modified from http://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript
+	return Math.random().toString(36).slice(2, 3)
+}
 
 var ScheduleActions = Fluxy.createActions({
 	serviceActions: {
 		create: [ScheduleConstants.SCHEDULE_CREATE, function(schedule) {
-			return ScheduleService.create(schedule)
+			return new Promise(function(resolve, reject) {
+				resolve(mori.js_to_clj({
+					id: uuid.v4(),
+					year: schedule.year,
+					semester: schedule.semester,
+					title: schedule.title || 'Schedule ' + randomChar(),
+					index: schedule.index || 1,
+					clbids: mori.set(),
+					active: schedule.active || false,
+				}))
+			})
+		}],
 		}]
 	},
 	undo: function() {
