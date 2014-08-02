57eefa09434d2ee356c9ea1688a5112dccdddcec
Hawken Rives <hawkrives@gmail.com>
2014-08-02 11:35:07 -0500

    Allow students to override the default object (StudentService)

--- client/services/StudentService.js
+++ client/services/StudentService.js
@@ -4,20 +4,25 @@ var uuid = require('node-uuid')
 var _ = require('lodash')
 var mori = require('mori')
 
+var StudyStore = require('../stores/StudyStore')
+var ScheduleStore = require('../stores/ScheduleStore')
+
 var StudentService = {
 	create: function(student) {
 		return new Promise(function(resolve, reject) {
 			var s = {
-				id: uuid.v4(),
-				name: '',
-				enrollment: 0,
-				graduation: 0,
-				creditsNeeded: 0,
-				studies: StudyStore(),
-				schedules: ScheduleStore(),
+				id: student.id || uuid.v4(),
+				name: student.name || '',
+				active: student.active || false,
+				enrollment: student.enrollment || 0,
+				graduation: student.graduation || 0,
+				creditsNeeded: student.creditsNeeded || 0,
+				studies: student.studies.length > 0 ? mori.vector(student.studies) : mori.vector(),
+				schedules: student.schedules.length > 0 ? mori.vector(student.schedules) : mori.vector(),
 				// overrides: new OverrideStore(),
 				// fabrications: new FabricationStore(),
 			}
+			console.log('called StudentService.create', s)
 			resolve(mori.js_to_clj(s))
 		})
 	},
