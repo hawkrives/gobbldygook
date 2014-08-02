6a3057cc27d144b9f2e5b3644039ffdb9c6f258d
Hawken Rives <hawkrives@gmail.com>
2014-08-02 00:12:45 -0500

    Update the initial schedule created in ScheduleService

--- client/services/ScheduleService.js
+++ client/services/ScheduleService.js
@@ -2,11 +2,25 @@ var Fluxy = require('fluxy')
 var Promise = require('bluebird')
 var uuid = require('node-uuid')
 var _ = require('lodash')
+var mori = require('mori')
+
+function randomChar() {
+	// modified from http://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript
+	return Math.random().toString(36).slice(2, 3)
+}
 
 var ScheduleService = {
 	create: function(schedule) {
 		return new Promise(function(resolve, reject) {
-			resolve(_.merge({id: uuid.v4()}, schedule))
+			resolve(mori.js_to_clj({
+				id: uuid.v4(),
+				year: schedule.year,
+				semester: schedule.semester,
+				title: schedule.title || 'Schedule ' + randomChar(),
+				index: schedule.index || 1,
+				clbids: mori.set(),
+				active: schedule.active || false,
+			}))
 		})
 	}
 }
