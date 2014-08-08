dc4c32b2f07a67a30ee83b446716d57e43baa268
Hawken Rives <hawkrives@gmail.com>
2014-08-07 21:00:36 -0500

    Move where onReady is declared to shut up JSHint

--- client/helpers/document-ready.js
+++ client/helpers/document-ready.js
@@ -3,14 +3,15 @@
 var Promise = require('bluebird')
 
 module.exports = new Promise(function(resolve) {
+	function onReady() {
+		resolve()
+		document.removeEventListener('DOMContentLoaded', onReady, true)
+		window.removeEventListener('load', onReady, true)
+	}
+
 	if (document.readyState === 'complete') {
 		resolve()
 	} else {
-		function onReady() {
-			resolve()
-			document.removeEventListener('DOMContentLoaded', onReady, true)
-			window.removeEventListener('load', onReady, true)
-		}
 		document.addEventListener('DOMContentLoaded', onReady, true)
 		window.addEventListener('load', onReady, true)
 	}
