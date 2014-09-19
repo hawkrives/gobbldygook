0dcb4e6493084ae1486762a5942e64791dd512de
Hawken Rives <hawkrives@gmail.com>
2014-09-19 11:55:04 -0500

    consolidate gulp

--- gulp/tasks/watch.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('watch', ['browserSync'], function() {
-	gulp.watch('./client/app/styles/**/*.scss', ['compass'])
-	gulp.watch('./client/app/*.html', ['copy'])
-	// Note: The browserify task handles js recompiling with watchify
-})
