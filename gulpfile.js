4a3fd9f51257d733f1c67abb4e8f59896fd376ad
Hawken Rives <hawkrives@gmail.com>
2014-09-26 12:11:56 -0500

    Single quotes in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,10 @@ var gulp         = require('gulp');
 var gutil        = require('gulp-util');
 var jshint       = require('gulp-jshint');
 var opn          = require('opn');
-var path         = require("path");
+var path         = require('path');
 var plumber      = require('gulp-plumber');
 var sass         = require('gulp-ruby-sass');
-var server       = require("webpack-dev-server");
+var server       = require('webpack-dev-server');
 var size         = require('gulp-size');
 var subtree      = require('gulp-subtree');
 var symlink      = require('gulp-symlink');
@@ -46,44 +46,46 @@ gulp.task('link', function() {
 var webpackConfig = {
 	cache: true,
 	entry: {
-		app: "./app/app.js"
+		app: './app/app.js'
 	},
 	output: {
-		contentBase: path.join(__dirname, "dist/"),
-		path: path.join(__dirname, "dist/"),
-		publicPath: "dist/",
-		filename: "[name].js",
-		chunkFilename: "[chunkhash].js"
+		contentBase: path.join(__dirname, 'dist/'),
+		path: path.join(__dirname, 'dist/'),
+		publicPath: 'dist/',
+		filename: '[name].js',
+		chunkFilename: '[chunkhash].js'
 	},
 	plugins: []
 };
 
 
 // The development server (the recommended option for development)
-gulp.task("default", ["webpack-dev-server"]);
-gulp.task("serve", ["webpack-dev-server"]);
+gulp.task('default', ['webpack-dev-server']);
+gulp.task('serve', ['webpack-dev-server']);
 
 // Build and watch cycle (another option for development)
 //// Advantage: No server required, can run app from filesystem
 //// Disadvantage: Requests are not blocked until bundle is available,
 ////   can serve an old app on refresh
-gulp.task("build-dev", ["webpack:build-dev"], function() {
-	gulp.watch(["app/**/*"], ["webpack:build-dev"]);
+gulp.task('build-dev', ['webpack:build-dev'], function() {
+	gulp.watch(['app/**/*'], ['webpack:build-dev']);
 });
 
 // Production build
-gulp.task("build", ["webpack:build"]);
+gulp.task('build', ['webpack:build']);
 
-gulp.task("prepare-build", ['html', 'styles', 'link'])
+gulp.task('prepare-build', ['html', 'styles', 'link'])
 
-gulp.task("webpack:build", ['prepare-build'], function(callback) {
+gulp.task('webpack:build', ['prepare-build'], function(callback) {
 	// modify some webpack config options
 	var myConfig = Object.create(webpackConfig);
+	myConfig.devtool = 'source-map';
+
 	myConfig.plugins = myConfig.plugins.concat(
 		new webpack.DefinePlugin({
-			"process.env": {
+			'process.env': {
 				// This has effect on the react lib size
-				"NODE_ENV": JSON.stringify("production")
+				'NODE_ENV': JSON.stringify('production')
 			}
 		}),
 		new webpack.optimize.DedupePlugin(),
@@ -92,8 +94,8 @@ gulp.task("webpack:build", ['prepare-build'], function(callback) {
 
 	// run webpack
 	webpack(myConfig, function(err, stats) {
-		if(err) throw new gutil.PluginError("webpack:build", err);
-		gutil.log("[webpack:build]", stats.toString({
+		if(err) throw new gutil.PluginError('webpack:build', err);
+		gutil.log('[webpack:build]', stats.toString({
 			colors: true
 		}));
 		callback();
@@ -108,37 +110,41 @@ myDevConfig.debug = true;
 // create a single instance of the compiler to allow caching
 var devCompiler = webpack(myDevConfig);
 
-gulp.task("webpack:build-dev", ['prepare-build'], function(callback) {
+gulp.task('webpack:build-dev', ['prepare-build'], function(callback) {
 	// run webpack
 	devCompiler.run(function(err, stats) {
-		if(err) throw new gutil.PluginError("webpack:build-dev", err);
-		gutil.log("[webpack:build-dev]", stats.toString({
+		if (err) {
+			throw new gutil.PluginError('webpack:build-dev', err);
+		}
+
+		gutil.log('[webpack:build-dev]', stats.toString({
 			colors: true
 		}));
 		callback();
 	});
 });
 
-gulp.task("webpack-dev-server", ['prepare-build'], function(callback) {
+gulp.task('webpack-dev-server', ['prepare-build'], function(callback) {
 	gulp.watch('app/styles/**/*.scss', ['styles']);
 
 	// modify some webpack config options
 	var myConfig = Object.create(webpackConfig);
-	myConfig.devtool = "source-map";
+	myConfig.devtool = 'eval';
 	myConfig.debug = true;
+	var pack = webpack(myConfig);
 
 	// Start a webpack-dev-server
-	new server(webpack(myConfig), {
+	new server(pack, {
 		contentBase: myConfig.output.contentBase,
 		path: myConfig.output.path,
-		publicPath: "/" + myConfig.output.publicPath,
+		publicPath: '/' + myConfig.output.publicPath,
 		stats: {
 			colors: true
 		}
-	}).listen(8080, "localhost", function(err) {
-		if(err) throw new gutil.PluginError("webpack-dev-server", err);
-		var url = "http://localhost:8080/webpack-dev-server/index.html";
-		gutil.log("[webpack-dev-server]", url);
+	}).listen(8080, 'localhost', function(err) {
+		if(err) throw new gutil.PluginError('webpack-dev-server', err);
+		var url = 'http://localhost:8080/webpack-dev-server/index.html';
+		gutil.log('[webpack-dev-server]', url);
 		opn(url);
 	});
 });
