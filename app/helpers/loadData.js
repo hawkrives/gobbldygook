51f81f5d4a1629a21379fc7c90b6b2e76baf7580
Hawken Rives <hawkrives@gmail.com>
2014-09-28 08:50:32 -0500

    ... except that logDataLoading appears to need to be defined in some way.

--- app/helpers/loadData.js
+++ app/helpers/loadData.js
@@ -8,6 +8,7 @@ var db = require('./db')
 var buildDeptNum = require('./deptNum').buildDeptNum
 var deptNumToCrsidCache = require('./getCourses').deptNumToCrsidCache
 
+var logDataLoading = false
 // var logDataLoading = true
 
 function storeCourses(item) {
