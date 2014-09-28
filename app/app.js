611158c530287d263de44d1de0f202a884c3f4ca
Hawken Rives <hawkrives@gmail.com>
2014-09-27 22:51:07 -0500

    Clean up app.js

--- app/app.js
+++ app/app.js
@@ -21,7 +21,7 @@ window.lodash = _
 window.React = React
 
 // Handy debugging function
-window.log = function(thing){console.log(_.isUndefined(thing) ? arguments : thing)}
+window.log = function(thing) { console.log(_.isUndefined(thing) ? arguments : thing) }
 
 // Initialize some library options.
 Promise.longStackTraces()
@@ -31,40 +31,39 @@ function addDemoStudent() {
 	return db.store('students').put(demoStudent);
 }
 
+function setupStudents() {
+	return db.store('students').all()
+		.then(function(results) {
+			if (results.length > 0) {
+				console.log('results!', results)
+			} else {
+				console.log('no results!')
+				addDemoStudent()
+			}
+
+			var students = (results.length > 0) ? results : [demoStudent]
+			var stores = {StudentStore: new StudentStore({students: students})}
+			return new Fluxxor.Flux(stores, actions)
+		})
+}
+
 module.exports = {
-	flux: function() {
-		return db.store('students').all()
-			.then(function(results) {
-				var students = (results.length > 0) ? results : [demoStudent]
-
-				if (results.length > 0) {
-					console.log('results!', students)
-				} else {
-					console.log('no results!', students)
-					addDemoStudent()
-				}
-
-				var stores = {StudentStore: new StudentStore({students: students})}
-				var flux = new Fluxxor.Flux(stores, actions)
-
-				// window.flux = flux
-			})
-	},
 	blastoff: function() {
 		// Wait for document.ready and the database.
-		Promise.all([db, document.ready]).bind(this).then(function() {
+		Promise.all([db, document.ready]).then(function() {
 			console.log('3. 2.. 1... Blastoff!')
 
 			// Load data into the database
 			loadData()
 
 			// Set up Fluxxor
-			return this.flux()
-		}).then(function() {
+			return setupStudents()
+		}).then(function(fluxxor) {
 			// Render the app
 			var studentComponent = React.renderComponent(
-				Gobbldygook({flux: window.flux}),
-				document.body)
+				Gobbldygook({flux: fluxxor}),
+				document.body
+			)
 		}).done()
 	},
 }
