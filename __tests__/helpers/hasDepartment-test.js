7f8eb5181aaf85349a078ec9c4e20c787ee73028
Hawken Rives <hawkrives@gmail.com>
2014-10-17 18:31:54 -0500

    Test hasDepartment

---
+++ __tests__/helpers/hasDepartment-test.js
@@ -0,0 +1,22 @@
+// __tests__/hasDepartment-test.js
+jest.dontMock('../../app/helpers/hasDepartment');
+jest.dontMock('lodash');
+
+describe('hasDepartment', function() {
+	var _ = require('lodash');
+
+	it('checks if a course is in a department', function() {
+		var hasDepartment = require('../../app/helpers/hasDepartment');
+		var courses = [
+			{depts: ['ASIAN', 'REL'], num: 230},
+			{depts: ['BIO', 'CHEM'], num: 125},
+			{depts: ['CHEM', 'BIO'], num: 125},
+			{depts: ['ASIAN'], num: 175},
+		];
+
+		expect(hasDepartment('ASIAN', courses[0])).toEqual(true);
+		expect(hasDepartment('BIO', courses[1])).toEqual(true);
+		expect(hasDepartment('CHEM', courses[2])).toEqual(true);
+		expect(hasDepartment('REL', courses[3])).toEqual(false);
+	});
+});
