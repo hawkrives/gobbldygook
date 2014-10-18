b3c45ed134525ba4ab3cfd0ffa5db566ae9d6f26
Hawken Rives <hawkrives@gmail.com>
2014-10-17 19:02:06 -0500

    Add basic test for UUID

---
+++ __tests__/helpers/uuid-test.js
@@ -0,0 +1,11 @@
+// __tests__/uuid-test.js
+jest.dontMock('../../app/helpers/uuid');
+
+describe('uuid', function() {
+	it('generates a uuid', function() {
+		var uuid = require('../../app/helpers/uuid');
+
+		expect(uuid().length).toBe(36);
+		expect(uuid().length).toBe(36);
+	});
+});
