5c58fce70dde53c271a45af27a6d86b11ff0461d
Hawken Rives <hawkrives@gmail.com>
2014-10-20 21:51:24 -0500

    ES6-ify and de-fluxify app.js

--- app/app.js
+++ app/app.js
@@ -1,66 +1,37 @@
 'use strict';
 
 import * as _ from 'lodash'
-import * as Promise from 'bluebird'
 import * as React from 'react'
-import * as Reflux from 'fluxxor'
-import db from './helpers/db'
+import * as Promise from 'bluebird'
+
+import {db} from './helpers/db'
 import documentReady from './helpers/document-ready'
 
 import Gobbldygook from './models/gobbldygookApp'
 import loadData from './helpers/loadData'
-import * as demoStudent from '../mockups/demo_student.json'
 
 // Just for use in the browser console, I swear.
 window.lodash = _
 
-// Stick React where I (and the Chrome devtools) can see it.
+// Stick React where I (and the Chrome devtools)
+// [ok, mostly for the devtools] can see it.
 window.React = React
 
 // Handy debugging function
 window.log = (thing) => console.log(_.isUndefined(thing) ? arguments : thing)
 
-// Initialize some library options.
-Promise.longStackTraces()
 React.initializeTouchEvents(true)
+Promise.longStackTraces()
 
-function addDemoStudent() {
-	return db.store('students').put(demoStudent);
-}
+let render = () => React.renderComponent(Gobbldygook(), document.body)
 
-// function setupStudents() {
-// 	return db.store('students').all()
-// 		.then(function(results) {
-// 			if (results.length > 0) {
-// 				console.log('results!', results)
-// 			} else {
-// 				console.log('no results!')
-// 				addDemoStudent()
-// 			}
-
-// 			var students = (results.length > 0) ? results : [demoStudent]
-// 			var stores = {StudentStore: new StudentStore({students: students})}
-// 			return new Fluxxor.Flux(stores, actions)
-// 		})
-// }
-
-function blastoff() {
+let blastoff = () => {
 	// Wait for document.ready and the database.
-	Promise.all([db, document.ready]).then(function() {
+	Promise.all([db, documentReady]).then(function() {
 		console.log('3. 2.. 1... Blastoff!')
 
-		// Load data into the database
-		// loadData()
-
-		return Promise.resolve(true);
-		// Set up Fluxxor
-		// return setupStudents()
-	}).then(function() {
-		// Render the app
-		var studentComponent = React.renderComponent(
-			Gobbldygook(),
-			document.body
-		)
+		 // Load data into the database, then render the app
+		loadData().then(render)
 	}).done()
 }
 
