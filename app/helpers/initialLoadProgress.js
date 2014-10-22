92b4b44bdb3de434d6c3b9a97a4413e0b626893d
Hawken Rives <hawkrives@gmail.com>
2014-10-22 14:16:56 -0500

    Remove some dead files

--- app/helpers/initialLoadProgress.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import * as Progress from 'progress-svg'
-import documentReady from './document-ready'
-
-var initialLoadProgress = new Progress()
-documentReady.then(() => document.body.appendChild(initialLoadProgress.el))
-
-export default initialLoadProgress
--- app/mixins/draggableContainer.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var React = require('react')
-
-var DraggableContainerMixin = {
-}
-
-module.exports = DraggableContainerMixin
