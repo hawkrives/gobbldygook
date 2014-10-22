4b2298e1eb7988798b0ab60406dc256b7ff34d38
Hawken Rives <hawkrives@gmail.com>
2014-10-22 09:21:28 -0500

    Make CourseList accept dropped Courses Logs the clbid of the dropped course

--- app/models/courseList.js
+++ app/models/courseList.js
@@ -5,11 +5,27 @@ import * as React from 'react'
 
 import Course from './course'
 
+import {DragDropMixin} from '../../node_modules/react-dnd/dist/ReactDND.min'
+import itemTypes from '../objects/itemTypes'
+
 var CourseList = React.createClass({
+	mixins: [DragDropMixin],
+	
+	configureDragDrop(registerType) {
+		registerType(itemTypes.COURSE, {
+			dropTarget: {
+				acceptDrop(clbid) {
+					console.log('dropped clbid', clbid)
+				}
+			}
+		})
+	},
+	
 	render() {
 		// console.log('courses', courseElements)
 
-		return React.DOM.div({className: 'course-list'},
+		return React.DOM.div(
+			Object.assign({className: 'course-list'}, this.dropTargetFor(itemTypes.COURSE)),
 			_.map(this.props.courses,
 				course => Course({key: course.clbid, info: course}))
 		)
