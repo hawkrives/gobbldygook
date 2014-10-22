d1119bfceeed19e7267546bade8ba12558dbe23f
Hawken Rives <hawkrives@gmail.com>
2014-10-22 12:07:19 -0500

    deepFreeze, from MDN https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze

--- app/helpers/loadData.js
+++ app/helpers/loadData.js
@@ -15,6 +15,21 @@ import {convertTimeStringsToOfferings} from './time'
 var logDataLoading = false
 // var logDataLoading = true
 
+function deepFreeze(o) {
+  var prop, propKey;
+  Object.freeze(o); // First freeze the object.
+  for (propKey in o) {
+    prop = o[propKey];
+    if (!o.hasOwnProperty(propKey) || !(typeof prop === 'object') || Object.isFrozen(prop)) {
+      // If the object is on the prototype, not an object, or is already frozen,
+      // skip it. Note that this might leave an unfrozen reference somewhere in the
+      // object if there is an already frozen object containing an unfrozen object.
+      continue;
+    }
+
+    deepFreeze(prop); // Recursively call deepFreeze.
+  }
+}
 
 function primeCourseCache() {
 	let recentYears = discoverRecentYears()
