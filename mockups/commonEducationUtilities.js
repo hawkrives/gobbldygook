ae5e91f2153311131118a529b499f6c032048ec6
Hawken Rives <hawkrives@gmail.com>
2014-10-24 18:14:33 -0500

    Make/export function to check if a course is an intercollegiate sport

--- mockups/commonEducationUtilities.js
+++ mockups/commonEducationUtilities.js
@@ -50,6 +50,15 @@ function checkThatNCoursesSpanTwoDepartments(courses, geneds, genedToCheck, n=2)
 	])
 }
 
+function isIntercollegiateSport(course) {
+	// Only one SPM course credit may be earned by students as a result of
+	// participation in an approved intercollegiate sport. This credit must be
+	// entered as Exercise Science 171-194 at the registration preceding the
+	// sport/participation term.
+	let result = hasDeptNumBetween({dept: 'ESTH', start: 171, end: 194}, course)
+	return result
+}
+
 export {
 	onlyQuarterCreditCoursesCanBePassFail,
 	hasGenEd,
@@ -58,4 +67,5 @@ export {
 	getDepartments,
 	acrossAtLeastTwoDepartments,
 	checkThatNCoursesSpanTwoDepartments
+	isIntercollegiateSport
 }
