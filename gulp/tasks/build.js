7a7e4c7f2de7258b23811831f98c7c99e94dcc78
Hawken Rives <hawkrives@gmail.com>
2014-11-16 16:38:08 -0600

    Reorder parallel tasks in build.js

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('build', function() {
-	return runSequence('clean', 'link', ['browserify', 'sass', 'fonts', 'markup'])
+	return runSequence('clean', 'link', ['fonts', 'markup', 'sass', 'browserify'])
 });
