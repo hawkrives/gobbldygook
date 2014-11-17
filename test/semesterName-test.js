cf419d02a6b55b4efe61fffcb65e173ba56e91e6
Hawken Rives <hawkrives@gmail.com>
2014-11-17 14:42:39 -0600

    Write/update test for expandYear

--- test/semesterName-test.js
+++ test/semesterName-test.js
@@ -18,11 +18,24 @@ describe('toPrettyTerm', function() {
 	it('converts a term id to a year and semester', function() {
 		var toPrettyTerm = require('../app/helpers/semesterName.es6').toPrettyTerm;
 
-		assert.equal(toPrettyTerm(20141), 'Fall 2014-15');
-		assert.equal(toPrettyTerm(20103), 'Spring 2010-11');
-		assert.equal(toPrettyTerm(20135), 'Late Summer 2013-14');
-		assert.equal(toPrettyTerm(20111), 'Fall 2011-12');
-		assert.equal(toPrettyTerm(20316), 'Unknown (6) 2031-32');
-		assert.equal(toPrettyTerm(20134), 'Early Summer 2013-14');
+		assert.equal(toPrettyTerm(20141), 'Fall 2014—2015');
+		assert.equal(toPrettyTerm(20103), 'Spring 2010—2011');
+		assert.equal(toPrettyTerm(20135), 'Late Summer 2013—2014');
+		assert.equal(toPrettyTerm(20111), 'Fall 2011—2012');
+		assert.equal(toPrettyTerm(20316), 'Unknown (6) 2031—2032');
+		assert.equal(toPrettyTerm(20134), 'Early Summer 2013—2014');
+	});
+});
+
+describe('expandYear', function() {
+	it('expands a year to year-(year+1)', function() {
+		var expandYear = require('../app/helpers/semesterName.es6').expandYear;
+
+		assert.equal(expandYear(2014), '2014—2015');
+		assert.equal(expandYear(2010), '2010—2011');
+		assert.equal(expandYear(2013), '2013—2014');
+		assert.equal(expandYear(2011), '2011—2012');
+		assert.equal(expandYear(2031), '2031—2032');
+		assert.equal(expandYear(2013), '2013—2014');
 	});
 });
