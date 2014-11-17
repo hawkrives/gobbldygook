bcf23161eeb36f08c064cc00623552973e0eb1ac
Hawken Rives <hawkrives@gmail.com>
2014-11-16 18:55:50 -0600

    Remove gulp-{sourcemaps,filter,changed}

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,22 +1,16 @@
 var autoprefixer = require('gulp-autoprefixer');
-var changed = require('gulp-changed');
-var filter = require('gulp-filter');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var handleErrors = require('../util/handleErrors');
-var sourcemaps = require('gulp-sourcemaps');
 var size = require('gulp-size');
 var config = require('../config').sass;
 
 gulp.task('sass', ['fonts'], function () {
 	return gulp.src(config.src)
-		.pipe(sourcemaps.init())
 		.pipe(sass())
-		.pipe(sourcemaps.write())
 		.on('error', handleErrors)
 		.pipe(autoprefixer({browsers: config.AUTOPREFIXER_BROWSERS}))
 		.pipe(gulp.dest(config.dest))
-		.pipe(filter('**/*.css'))
 		.pipe(size({title: 'sass'}))
 });
 
