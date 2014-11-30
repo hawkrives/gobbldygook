1818613a67ec8426ffb0e6db8a2cb0ee2d147bb6
Hawken Rives <hawkrives@gmail.com>
2014-11-30 16:48:26 -0600

    Use csswring and autoprefixer in the sass build
    We use them by way of gulp-postcss. This commit also recreates the
    sass sourcemaps.
    
    autoprefixer prefixes css; csswring shrinks it.

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,15 +1,26 @@
-var autoprefixer = require('gulp-autoprefixer');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var handleErrors = require('../util/handleErrors');
 var size = require('gulp-size');
 var config = require('../config').sass;
 
-gulp.task('sass', ['fonts'], function () {
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var csswring = require('csswring');
+
+var processors = [
+    autoprefixer({browsers: config.AUTOPREFIXER_BROWSERS}),
+    csswring
+]
+
+gulp.task('sass', ['fonts'], function() {
 	return gulp.src(config.src)
 		.pipe(sass())
 		.on('error', handleErrors)
-		.pipe(autoprefixer({browsers: config.AUTOPREFIXER_BROWSERS}))
+		.pipe(sourcemaps.init())
+		.pipe(postcss(processors))
+		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest(config.dest))
 		.pipe(size({title: 'sass'}))
 });
