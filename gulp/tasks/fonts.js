ac56e3c30db220a2560bf07d2544ed6faeb3e8d6
Hawken Rives <hawkrives@gmail.com>
2014-12-01 22:33:31 -0600

    Remove extra space inside function declaration of gulp fonts

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var size = require('gulp-size');
 var config = require('../config').fonts;
 
-gulp.task('fonts', function () {
+gulp.task('fonts', function() {
 	return gulp.src(config.src)
 		.pipe(gulp.dest(config.dest))
 		.pipe(size({title: 'fonts'}));
