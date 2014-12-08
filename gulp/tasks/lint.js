7b57016de0f2f0efdb7cf58b34b8a46dff498168
Hawken Rives <hawkrives@gmail.com>
2014-12-08 12:37:13 -0600

    Pull jscs config out of package.json

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -3,9 +3,6 @@ var jscs = require('gulp-jscs');
 var config = require('../config').lint;
 
 gulp.task('lint', function() {
-	// When we update to JSCS 1.8, add these rules back in:
-	// "requireSpaceBeforeKeywords": true,
-	// "requireTrailingComma": {"ignoreSingleValue": true,"ignoreSingleLine": true},
 	return gulp.src(config.src)
-		.pipe(jscs({configPath: './package.json'}));
+		.pipe(jscs());
 });
