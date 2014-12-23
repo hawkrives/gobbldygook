6ea9ff0f711b364477f708ee20ea4c0068ae137c
Hawken Rives <hawkrives@gmail.com>
2014-12-22 22:38:32 -0600

    Update markup for studentList

--- app/elements/studentList.js
+++ app/elements/studentList.js
@@ -2,17 +2,19 @@ import * as React from 'react'
 import {Link} from 'react-router'
 
 var StudentList = React.createClass({
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.students !== this.props.students
+	},
+
 	render() {
 		let students = this.props.students.map((student) =>
-			React.createElement(Link,
-				{to: 'student', params: {id: student.id}},
-				React.createElement('li', null, student.name)))
+			React.createElement('li', {key: student.id, className: 'student-list--student'},
+				React.createElement(Link,
+					{to: 'student', params: {id: student.id}},
+					student.name)))
 
-		return React.createElement('div',
-			{className: 'student-list'},
-			React.createElement('ul', null, students.toJS())
-		)
-	}
+		return React.createElement('ul', {className: 'student-list'}, students.toJS())
+	},
 })
 
 export default StudentList
