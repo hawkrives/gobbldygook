88a7f604394cb35cde7d47766fe471741001f1cd
Hawken Rives <hawkrives@gmail.com>
2015-01-05 15:11:01 -0600

    Explicitly import two bits of lodash in courseLevels

--- app/helpers/courseLevels.js
+++ app/helpers/courseLevels.js
@@ -1,27 +1,27 @@
-import * as _ from 'lodash'
+import {curry, filter} from 'lodash'
 
-let coursesAtOrAboveLevel = _.curry((level, course) => {
+let coursesAtOrAboveLevel = curry((level, course) => {
 	return course.level >= level
 })
 
-let onlyCoursesAtOrAboveLevel = _.curry((level, courses) => {
-	return _.filter(courses, coursesAtOrAboveLevel(level))
+let onlyCoursesAtOrAboveLevel = curry((level, courses) => {
+	return filter(courses, coursesAtOrAboveLevel(level))
 })
 
-let coursesAtLevel = _.curry((level, course) => {
+let coursesAtLevel = curry((level, course) => {
 	return course.level === level
 })
 
-let onlyCoursesAtLevel = _.curry((level, courses) => {
-	return _.filter(courses, coursesAtLevel(level))
+let onlyCoursesAtLevel = curry((level, courses) => {
+	return filter(courses, coursesAtLevel(level))
 })
 
-let coursesAboveNumber = _.curry((number, course) => {
+let coursesAboveNumber = curry((number, course) => {
 	return course.num > number
 })
 
-let onlyCoursesAboveNumber = _.curry((level, courses) => {
-	return _.filter(courses, coursesAboveNumber(level))
+let onlyCoursesAboveNumber = curry((level, courses) => {
+	return filter(courses, coursesAboveNumber(level))
 })
 
 export {
