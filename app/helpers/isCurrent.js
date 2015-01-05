dcb8451dbd65a949653c888b77480ab779737792
Hawken Rives <hawkrives@gmail.com>
2015-01-05 11:17:04 -0600

    Add space before typeof operator

--- app/helpers/isCurrent.js
+++ app/helpers/isCurrent.js
@@ -1,15 +1,15 @@
 import {curry} from 'lodash'
 
 let isCurrentSemester = curry((year, semester, schedule) => {
-	if (typeof year === 'string')
+	if ( typeof year === 'string')
 		year = parseInt(year, 10)
-	if (typeof semester === 'string')
+	if ( typeof semester === 'string')
 		semester = parseInt(semester, 10)
 	return schedule.year === year && schedule.semester === semester
 })
 
 let isCurrentYear = curry((year, schedule) => {
-	if (typeof year === 'string')
+	if ( typeof year === 'string')
 		year = parseInt(year, 10)
 	return schedule.year === year
 })
