5ab479fb1af9a40fcf665e3656a8975fbb325b2d
Hawken Rives <hawkrives@gmail.com>
2015-01-06 16:39:26 -0600

    Pull isIntercollegiateSport into a helper file

--- mockups/lib/commonEducationUtilities.js
+++ mockups/lib/commonEducationUtilities.js
@@ -7,15 +7,7 @@ import countGeneds from './countGeneds'
 import getDepartments from './getDepartments'
 import acrossAtLeastTwoDepartments from './acrossAtLeastTwoDepartments'
 import checkThatCoursesSpanDepartmentsAndGeneds from './checkThatCoursesSpanDepartmentsAndGeneds'
-
-function isIntercollegiateSport(course) {
-	// Only one SPM course credit may be earned by students as a result of
-	// participation in an approved intercollegiate sport. This credit must be
-	// entered as Exercise Science 171-194 at the registration preceding the
-	// sport/participation term.
-	const result = hasDeptNumBetween({dept: 'ESTH', start: 171, end: 194}, course)
-	return result
-}
+import isIntercollegiateSport from './isIntercollegiateSport'
 
 export {
 	onlyQuarterCreditCoursesCanBePassFail,
