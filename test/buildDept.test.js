3cd166007194cbda0d2662785d7b9cd58da566f4
Hawken Rives <hawkrives@gmail.com>
2015-01-07 16:20:17 -0600

    Break the deptNum test into five tests

---
+++ test/buildDept.test.js
@@ -0,0 +1,25 @@
+// test/buildDept.test.js
+import 'should'
+
+import buildDept from 'app/helpers/buildDept'
+describe('buildDept', () => {
+	it('builds a department string from a single-dept course', () => {
+		let ASIAN = {depts: ['ASIAN']}
+
+		buildDept(ASIAN).should.equal('ASIAN')
+	})
+
+	it('builds a department string from a multi-department course', () => {
+		let ASRE = {depts: ['ASIAN', 'REL']}
+
+		buildDept(ASRE).should.equal('ASIAN/REL')
+	})
+
+	it('maintains the order of the departments array', () => {
+		let BICH = {depts: ['BIO', 'CHEM']}
+		let CHBI = {depts: ['CHEM', 'BIO']}
+
+		buildDept(BICH).should.equal('BIO/CHEM')
+		buildDept(CHBI).should.equal('CHEM/BIO')
+	})
+})
