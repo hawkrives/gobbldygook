eb97585e3fc22f0fab950208c4de7eee3d15515e
Hawken Rives <hawkrives@gmail.com>
2015-01-08 00:29:48 -0600

    Require jshint

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,9 +1,12 @@
 import gulp from 'gulp'
 import cache from 'gulp-cached'
 import jscs from 'gulp-jscs'
+// import jshint from 'gulp-jshint'
 import {lint as config} from '../config'
 
 gulp.task('lint', () =>
 	gulp.src(config)
 		.pipe(cache('linting'))
 		.pipe(jscs()))
+		// .pipe(jshint())
+		// .pipe(jshint.reporter('jshint-stylish')))
