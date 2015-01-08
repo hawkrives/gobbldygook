5c546333a56dea0d11248de3fa128c9ce91214f7
Drew Volz <drew@drewvolz.com>
2015-01-08 11:32:04 -0600

    Added hasDepartment helper to isAsianCon and fixed local name of course from c to course

--- sto-areas/lib/isAsianCon.js
+++ sto-areas/lib/isAsianCon.js
@@ -1,9 +1,10 @@
 import {all, contains} from 'lodash'
+import hasDepartment from 'app/helpers/hasDepartment'
 
 function isAsianCon(course) {
 	return all([
-		hasDepartment('ASIAN', c),
-		contains([210, 215, 216, 220], c.num), // these are the asiancon course numbers
+		hasDepartment('ASIAN', course),
+		contains([210, 215, 216, 220], course.num), // these are the asiancon course numbers
 	])
 }
 
