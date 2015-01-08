6f2ebba012fce9350c0496e2e1f44796a9273e87
Hawken Rives <hawkrives@gmail.com>
2015-01-08 11:19:37 -0600

    Make an initial pass at allowing searching of areas on metadata

--- sto-areas/index.js
+++ sto-areas/index.js
@@ -3,4 +3,19 @@ import concentration from './concentration'
 import emphasis from './emphasis'
 import degree from './degree'
 
-export {major, concentration, emphasis, degree}
+import _ from 'lodash'
+let areas = [major, concentration, emphasis, degree]
+let allAreas = _(areas).map(_.toArray).flatten().value()
+// console.log(allAreas)
+
+/**
+ * Finds an area of study.
+ *
+ * @param {Object} args - a lodash filter object.
+ * @returns {Object} - An area of study.
+ */
+let find = (args={}) => {
+	return _.find(allAreas, args)
+}
+
+export {major, concentration, emphasis, degree, find}
