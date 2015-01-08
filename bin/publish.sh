343dd7caf437e62b4499be7002771d01bdfce441
Hawken Rives <hawkrives@gmail.com>
2015-01-08 17:04:27 -0600

    Update bin/publish.sh

--- bin/publish.sh
+++ bin/publish.sh
@@ -4,10 +4,11 @@ git pull --rebase
 
 npm run build
 
-grep "version" package.json
+node ./version.js
 
-read -p "Version: "  version; \
-npm version $version --message "v%s"
-npm publish
+read -p "New Version [Major|Minor|Patch]: " version
+npm version $version
 
-git push --follow-tags
+# npm publish
+
+# git push --follow-tags
