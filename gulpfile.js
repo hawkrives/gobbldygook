4a5c7f86d76c1ed0118625281463966155c5e147
Hawken Rives <hawkrives@gmail.com>
2015-01-08 00:19:21 -0600

    Edit the comment in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 /*
   gulpfile.js
   ===========
-  Rather than manage one giant configuration file responsible
-  for creating multiple tasks, each task has been broken out into
-  its own file in gulp/tasks. Any files in that directory get
-  automatically required below.
+
+  Rather than manage one giant configuration file responsible for
+  creating multiple tasks, each task has been broken out into its own
+  file in gulp/tasks. Any files in that directory get automatically
+  required below.
+
   To add a new task, simply add a new task file that directory.
   gulp/tasks/default.js specifies the default set of tasks to run
   when you run `gulp`.
 
-  Copied from <https://github.com/greypants/gulp-starter>.
+  Derived from <https://github.com/greypants/gulp-starter>.
 */
 
 require('6to5/register')
