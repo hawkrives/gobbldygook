4a2c7461b97a80e94a0ea7537eb372f41be8813d
Hawken Rives <hawkrives@gmail.com>
2015-01-09 12:42:44 -0600

    Remove a bunch of helpers; pull them into sto-helpers

--- app/helpers/add.js
+++
@@ -1,7 +0,0 @@
-/**
- * Takes two numbers and adds them together.
- * @param {Number} a
- * @param {Number} b
- * @returns {Number} - the sum of `a` and `b`
- */
-export default (a, b) => a + b
--- app/helpers/randomChar.js
+++
@@ -1,2 +0,0 @@
-// modified from http://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript
-export default () => Math.random().toString(36).slice(2, 3)
