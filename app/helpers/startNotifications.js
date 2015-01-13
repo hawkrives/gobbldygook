3b55c83d8b2d83137fc6499c387ba0e7a3c24cb4
Hawken Rives <hawkrives@gmail.com>
2015-01-12 23:07:34 -0600

    Update the call to testing progress bars

--- app/helpers/startNotifications.js
+++ app/helpers/startNotifications.js
@@ -1,6 +1,6 @@
 import notificationActions from '../flux/notificationActions'
 window.addEventListener('error', notificationActions.logError)
-notificationActions.startProgress(1, 'Testing Progress Bars', 1, 4)
+notificationActions.startProgress(1, 'Testing Progress Bars', {max: 4, value: 1}, true)
 notificationActions.logMessage(0, 'Testing Messages')
 notificationActions.logError({message: 'Testing Errors'})
 
