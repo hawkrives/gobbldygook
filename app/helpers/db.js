80881a722bd341069605dee40105d9e430aa391d
Hawken Rives <hawkrives@gmail.com>
2015-01-13 21:58:59 -0600

    Clean up the db indexes

--- app/helpers/db.js
+++ app/helpers/db.js
@@ -2,23 +2,14 @@ import treo from 'treo'
 
 let schema = treo.schema()
 	.version(1)
-		.addStore('courses', { key: 'clbid' })
-			.addIndex('profs',      'profs',      {multi: true})
-			.addIndex('depts',      'depts',      {multi: true})
-			.addIndex('places',     'places',     {multi: true})
-			.addIndex('times',      'times',      {multi: true})
-			.addIndex('gereqs',     'gereqs',     {multi: true})
+		.addStore('courses', { key: 'id' })
 			.addIndex('sourcePath', 'sourcePath', {multi: false})
-			.addIndex('crsid',      'crsid',      {multi: false})
 			.addIndex('clbid',      'clbid',      {multi: false})
-			.addIndex('deptnum',    'deptnum',    {multi: false})
-			.addIndex('year',       'year',       {multi: false})
+			.addIndex('term',       'term',       {multi: false})
 		.addStore('areas', { key: 'sourcePath' })
 			.addIndex('type',       'type',       {multi: true})
 			.addIndex('sourcePath', 'sourcePath', {multi: false})
-	.version(2)
 		.addStore('students', { key: 'id' })
-			.addIndex('active', 'active', {multi: false, unique: true})
 
 
 import treoPromise from 'treo/plugins/treo-promise'
@@ -26,6 +17,7 @@ import queryTreoDatabase from './queryTreoDatabase'
 let db = treo('gobbldygook', schema)
 	.use(queryTreoDatabase())
 	.use(treoPromise())
+
 
 window.eraseDatabase = () => {
 	window.database.drop().then(() => {
