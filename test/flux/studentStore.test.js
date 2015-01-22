6dc49b795f0187d1fc7d2516ef8f38ae1f476e9a
Hawken Rives <hawkrives@gmail.com>
2015-01-21 22:47:29 -0600

    Add more test descriptions to studentStore

--- test/flux/studentStore.test.js
+++ test/flux/studentStore.test.js
@@ -12,4 +12,18 @@ describe('studentStore', () => {
 		expect(studentStore.history).toBeDefined()
 		expect(studentStore.future).toBeDefined()
 	})
+
+	xit('supports undoing things')
+	xit('supports redoing things')
+	xit('will not let you go back to a previous future')
+
+	xit('can reset a student to a demo state')
+	xit('reloads all currently known students from storage')
+
+	xit('loads students from storage')
+
+	xit('creates new students')
+
+	xit('passes methods that change students')
+	xit('passes methods that alter things inside of students')
 })
