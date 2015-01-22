c12fe38a1e43883300b71d481eb56b3856e64780
Hawken Rives <hawkrives@gmail.com>
2015-01-22 16:27:54 -0600

    Log query objects when they're created

--- app/helpers/courses.js
+++ app/helpers/courses.js
@@ -46,6 +46,7 @@ function getCourses(clbids) {
 
 function queryCourseDatabase(queryString) {
 	let queryObject = buildQueryFromString(queryString)
+	console.log('query object', queryObject)
 	let start = performance.now()
 	return db
 		.store('courses')
