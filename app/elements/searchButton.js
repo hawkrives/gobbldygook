ba99bf7cded454a3cd256a332898a6cb745cc9d6
Hawken Rives <hawkrives@gmail.com>
2015-01-23 13:22:47 -0600

    Break the query funciton into "search" and "process"

--- app/elements/searchButton.js
+++ app/elements/searchButton.js
@@ -25,74 +25,90 @@ let SearchButton = React.createClass({
 
 	getInitialState() {
 		return {
-			courseObjects: null,
-			query: '',
+			isQuerying: false,
+			hasQueried: false,
+			results: [],
+			queryString: '',
+			query: null,
 		}
 	},
 
 	onSubmit() {
-		this.query(this.state.query)
+		this.query(this.state.queryString)
 	},
 
 	onChange(evt) {
-		this.setState({query: evt.target.value})
+		this.setState({queryString: evt.target.value})
 	},
 
 	onKeyDown(evt) {
 		if (evt.keyCode == 13) {
-			return this.onSubmit()
+			this.onSubmit()
 		}
+	},
+
+	processQueryResults(resultsAndStartQueryTime) {
+		let [results, startQueryTime] = resultsAndStartQueryTime
+		console.log('results', results)
+
+		let searchResults = chain(results)
+			.sortBy(c => `${c.deptnum}${c.sect || ''}`) // Sort the results
+			.groupBy('term') // Group them by term
+			.pairs() // Turn the object into an array of pairs
+			// Sort the result arrays by the first element, the term,
+			// because object keys don't have an implicit sort.
+			.sortBy(group => group[0])
+			.reverse() // reverse it, so the most recent is at the top
+			.flatten() // then flatten so that it's all one flat list
+			.value()
+
+		console.log('search results', searchResults)
+		let endQueryTime = performance.now()
+		console.info(`query took ${(endQueryTime - startQueryTime)}ms.`)
+
+		let startTime = performance.now()
+		let courseObjects = map(searchResults, (courseOrTerm) => {
+			if (!isObject(courseOrTerm)) {
+				let prettyTerm = toPrettyTerm(courseOrTerm)
+
+				return React.createElement('li',
+					{key: prettyTerm, className: 'course-group'},
+					prettyTerm)
+			}
+			return React.createElement('li',
+				{key: courseOrTerm.clbid},
+				React.createElement(Course, {info: courseOrTerm}))
+		})
+
+		let endTime = performance.now()
+		console.info(`element creation took an additional ${(endTime - startTime)}ms.`)
+
+		this.setState({results: courseObjects, hasQueried: true, query: null})
 	},
 
 	query(searchQuery) {
 		if (searchQuery.length === 0 || this.state.query)
 			return
-
+		this.setState({results: [], hasQueried: false})
 		let startQueryTime = performance.now()
-		queryCourseDatabase(searchQuery).then(results => {
-			console.log('results', results)
 
-			let searchResults = chain(results)
-				.sortBy(c => `${c.deptnum}${c.sect || ''}`) // Sort the results
-				.groupBy('term') // Group them by term
-				.pairs() // Turn the object into an array of pairs
-				// Sort the result arrays by the first element, the term,
-				// because object keys don't have an implicit sort.
-				.sortBy(group => group[0])
-				.reverse() // reverse it, so the most recent is at the top
-				.flatten() // then flatten so that it's all one flat list
-				.value()
+		let query = queryCourseDatabase(searchQuery)
+			.then(results => [results, startQueryTime])
+			.then(this.processQueryResults)
 
-			console.log('search results', searchResults)
-			let endQueryTime = performance.now()
-			console.info('query took ' + (endQueryTime - startQueryTime) + 'ms.')
-
-			let startTime = performance.now()
-			let courseObjects = map(searchResults, (courseOrTerm) => {
-				if (!isObject(courseOrTerm)) {
-					let prettyTerm = toPrettyTerm(courseOrTerm)
-					return React.createElement('li',
-						{key: prettyTerm, className: 'course-group'},
-						prettyTerm)
-				}
-				return React.createElement('li',
-					{key: courseOrTerm.clbid},
-					React.createElement(Course, {info: courseOrTerm}))
-			})
-
-			let endTime = performance.now()
-			console.info('react element creation took an additional ' + (endTime - startTime) + 'ms.')
-
-			this.setState({courseObjects})
-		})
+		this.setState({query})
 	},
 
 	render() {
+
 		return React.createElement('div', {className: 'search-sidebar'},
 			React.createElement('header', {className: 'sidebar-heading'},
 				React.createElement('h1', null, 'Search for Courses'),
-				React.createElement('button',
-					{className: 'close-sidebar', title: 'Close Sidebar', onClick: this.props.toggle})),
+				React.createElement('button', {
+					className: 'close-sidebar',
+					title: 'Close Sidebar',
+					onClick: this.props.toggle,
+				})),
 			React.createElement('input', {
 				type: 'search',
 				placeholder: 'Search Courses',
