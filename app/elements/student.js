7c1e61900ea3773575687017ac2b9652ff550d2c
Hawken Rives <hawkrives@gmail.com>
2015-01-23 12:21:08 -0600

    Use the mixin on the student loading spinner

--- app/elements/student.js
+++ app/elements/student.js
@@ -43,7 +43,7 @@ let Student = React.createClass({
 		console.info('list of students in Student', this.props.students.toJS())
 
 		if (!this.state.student)
-			return React.createElement('figure', {className: 'loading'},
+			return React.createElement('figure', {className: 'loading-screen'},
 				React.createElement('div', {className: 'loading-spinner'}, React.createElement('div', null), 'Loading Students&hellip;'),
 				React.createElement('figcaption', {className: 'loading-message ' + this.state.messageClass}, this.state.message))
 
