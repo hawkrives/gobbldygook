e7abae5671043031aa79751f3986fd7bbb71e6aa
Hawken Rives <hawkrives@gmail.com>
2015-01-28 00:31:46 -0600

    Write test for cleanTimeStringSegment

--- test/cleanTimeStringSegment.test.js
+++ test/cleanTimeStringSegment.test.js
@@ -1,2 +1,12 @@
 // test/cleanTimeStringSegment.test.js
 import cleanTimeStringSegment from '../lib/cleanTimeStringSegment'
+
+describe('cleanTimeStringSegment', () => {
+	it('trims up a timestring segment', () => {
+		expect(cleanTimeStringSegment('    800AM   ')).to.equal('800AM')
+	})
+
+	it('capitalizes a timestring segment', () => {
+		expect(cleanTimeStringSegment('800am')).to.equal('800AM')
+	})
+})
