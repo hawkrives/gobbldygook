b1665453c9436eced1de746e9946595903b898a6
Hawken Rives <hawkrives@gmail.com>
2015-01-28 15:58:34 -0600

    Pass words into buildQueryFromString

--- app/helpers/queryCourseDatabase.js
+++ app/helpers/queryCourseDatabase.js
@@ -2,13 +2,13 @@ import db from './db'
 import {buildQueryFromString} from 'sto-helpers'
 
 function queryCourseDatabase(queryString) {
-	let queryObject = buildQueryFromString(queryString)
+	let queryObject = buildQueryFromString(queryString, {words: true})
 	console.log('query object', queryObject)
 
 	return db
 		.store('courses')
 		.query(queryObject)
-		.catch(err => new Error('course query failed on', queryString, err))
+		.catch(err => new Error(`course query failed on "${queryString}" with error "${err}"`))
 }
 
 export default queryCourseDatabase
