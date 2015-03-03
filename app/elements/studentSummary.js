5d65c4499d2b4ee4d6f9db9f5816f6291f40e054
Hawken Rives <hawkrives@gmail.com>
2015-03-03 12:58:40 -0600

    Fix studentSummary

--- app/elements/studentSummary.js
+++ app/elements/studentSummary.js
@@ -1,6 +1,6 @@
 import _ from 'lodash'
 import React from 'react'
-import humanize from 'humanize-plus'
+import {oxford, pluralize} from 'humanize-plus'
 
 import ContentEditable from './contentEditable'
 
@@ -21,72 +21,56 @@ let StudentSummary = React.createClass({
 	},
 
 	render() {
+		let canGraduate = this.props.graduatability
 		let student = this.props.student
 		let studies = student.studies
 
 		let name = student.name
-		let nameEl = <ContentEditable content={name} onChange={this.updateStudentName} />
+		let NameEl = <ContentEditable content={name} onChange={this.updateStudentName} />
 
 		let has = studies
 			.groupBy(s => s.type)
 			.map(s => s.size)
 			.toObject()
 
-		let objects = {
-			degree: studies.filter(s => s.type === 'degree'),
-			major: studies.filter(s => s.type === 'major'),
-			concentration: studies.filter(s => s.type === 'concentration'),
-			emphasis: studies.filter(s => s.type === 'emphasis'),
-		}
+		let degrees = studies.filter(s => s.type === 'degree')
+		let majors = studies.filter(s => s.type === 'major')
+		let concentrations = studies.filter(s => s.type === 'concentration')
+		let emphases = studies.filter(s => s.type === 'emphasis')
 
-		let titles = {
-			degree: humanize.oxford(objects.degree.map(s => s.title).toArray()),
-			major: humanize.oxford(objects.major.map(s => s.title).toArray()),
-			concentration: humanize.oxford(objects.concentration.map(s => s.title).toArray()),
-			emphasis: humanize.oxford(objects.emphasis.map(s => s.title).toArray()),
-		}
+		let degreeTitles = oxford(degrees.map(s => s.title).toArray())
+		let majorTitles = oxford(majors.map(s => s.title).toArray())
+		let concentrationTitles = oxford(concentrations.map(s => s.title).toArray())
+		let emphasisTitles = oxford(emphases.map(s => s.title).toArray())
 
-		let words = {
-			degree: humanize.pluralize(objects.degree.size, 'degree'),
-			major: humanize.pluralize(objects.major.size, 'major'),
-			concentration: humanize.pluralize(objects.concentration.size, 'concentration'),
-			emphasis: humanize.pluralize(objects.emphasis.size, 'emphasis', 'emphases'),
-		}
-
-		let canGraduate = this.props.graduatability
-
-		// let phrases = {
-		// 	degree: React.createElement('span', {className: 'area-of-study-list', key:'degree'}, titles.degree),
-		// 	major: React.createElement('span', {className: 'area-of-study-list', key:'major'}, titles.major),
-		// 	concentration: React.createElement('span', {className: 'area-of-study-list', key:'concentration'}, titles.concentration),
-		// 	emphasis: React.createElement('span', {className: 'area-of-study-list', key:'emphasis'}, titles.emphasis),
-		// }
-		let phrases = {
-			degree: <span className='area-of-study-list' key='degree'>{titles.degree}</span>,
-			major: <span className='area-of-study-list' key='major'>{titles.major}</span>,
-			concentration: <span className='area-of-study-list' key='concentration'>{titles.concentration}</span>,
-			emphasis: <span className='area-of-study-list' key='emphasis'>{titles.emphasis}</span>,
-		}
+		let degreeWords = pluralize(degrees.size, 'degree')
+		let majorWords = pluralize(majors.size, 'major')
+		let concentrationWords = pluralize(concentrations.size, 'concentration')
+		let emphasisWords = pluralize(emphases.size, 'emphasis', 'emphases')
 
 		let degreeEmphasizer = has.degree === 1 ? 'a ' : ''
 		let majorEmphasizer = has.major === 1 ? 'a ' : ''
 		let concentrationEmphasizer = has.concentration === 1 ? 'a ' : ''
 		let emphasisEmphasizer = has.emphasis === 1 ? 'an ' : ''
 
+		let degreeEl = <span className='area-of-study-list' key='degrees'>{degreeTitles}</span>
+		let majorEl = <span className='area-of-study-list' key='majors'>{majorTitles}</span>
+		let concentrationEl = <span className='area-of-study-list' key='concentrations'>{concentrationTitles}</span>
+		let emphasisEl = <span className='area-of-study-list' key='emphases'>{emphasisTitles}</span>
+
 		return <article id='student-summary' className={canGraduate ? 'can-graduate' : 'cannot-graduate'}>
 			<header>
-				<div key='letter' id='student-letter'>{name.length ? name[0] : ''}</div>
-				<p key='hi'>{`Hi, ${nameEl}!`}</p>
+				<div id='student-letter'>{name.length ? name[0] : ''}</div>
+				<p>Hi, {NameEl}</p>
 			</header>
 			<div className='content'>
-				<p key='overview'>{
-					'You are planning on ', degreeEmphasizer,
-					phrases.degree, ' ', words.degree, ', with ', majorEmphasizer, words.major, ' in ', phrases.major,
-					(has.concentration > 0) ? [', and ' + concentrationEmphasizer, words.concentration, ' in ', phrases.concentration] : '',
-					(has.emphasis > 0) ? [', not to mention ', emphasisEmphasizer, words.emphasis, ' in ', phrases.emphasis] : '',
-					'.'
-				}</p>
-				<p key='message' className='graduation-message'>
+				<p>
+					You are planning on {degreeEmphasizer} {degreeEl} {degreeWords}
+					{(has.major > 0) ? [', with ', majorEmphasizer, majorWords, ' in ', majorEl] : null}
+					{(has.concentration > 0) ? [', and ' + concentrationEmphasizer, concentrationWords, ' in ', concentrationEl] : null}
+					{(has.emphasis > 0) ? [', not to mention ', emphasisEmphasizer, emphasisWords, ' in ', emphasisEl] : null}.
+				</p>
+				<p className='graduation-message'>
 					{canGraduate ? goodGraduationMessage : badGraduationMessage}
 				</p>
 			</div>
