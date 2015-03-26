6908b9d154507949b95158a80d63ac5d791a3378
Hawken Rives <hawkrives@gmail.com>
2015-03-26 14:26:24 -0500

    turn off notification of errors

--- app/startThings/startNotifications.js
+++ app/startThings/startNotifications.js
@@ -1,5 +1,5 @@
-import notificationActions from '../flux/notificationActions'
-window.addEventListener('error', notificationActions.logError)
+// import notificationActions from '../flux/notificationActions'
+// window.addEventListener('error', notificationActions.logError)
 // notificationActions.startProgress(1, 'Testing Progress Bars', {max: 4, value: 1}, true)
 // notificationActions.logMessage(0, 'Testing Messages')
 // notificationActions.logError({message: 'Testing Errors'})
