0e2c32ef9c93901135a5a0384463cc7fa97346a1
Hawken Rives <hawkrives@gmail.com>
2015-07-25 13:16:54 +0900

    import benchmarks from area-data

---
+++ benchmarks/students-benchmark.js
@@ -0,0 +1,76 @@
+import fs from 'graceful-fs'
+import info from '../package.json'
+
+import evaluate from '../src/lib/evaluate'
+import enhanceHanson from '../src/lib/enhance-hanson'
+import pluralizeArea from '../src/lib/pluralize-area'
+
+import includes from 'lodash/collection/includes'
+import junk from 'junk'
+import kebabCase from 'lodash/string/kebabCase'
+import meow from 'meow'
+import ms from 'pretty-ms'
+import range from 'lodash/utility/range'
+import size from 'lodash/collection/size'
+import sparkly from 'sparkly'
+import sum from 'lodash/collection/sum'
+import yaml from 'js-yaml'
+import {join} from 'path'
+import loadArea from '../bin/load-area'
+
+function now(other=[]) {
+    let time = process.hrtime(other)
+    return time[0] * 1e3 + time[1] / 1e6
+}
+
+function loadStudents(dir) {
+    return fs.readdirSync(dir)
+        .filter(name => !includes(name, '.ip'))
+        .filter(junk.not)
+        .map(path => dir + path)
+        .map(path => fs.readFileSync(path, 'utf-8'))
+        .map(JSON.parse)
+}
+
+function benchmark({runs, graph}) {
+    loadStudents('./test/example-students/')
+        .forEach(({courses=[], overrides={}, areas=[]}) => {
+            areas.forEach(areaInfo => {
+                console.log(`the '${areaInfo.name}' ${areaInfo.type} (${areaInfo.revision})`)
+                let areaData = loadArea(areaInfo)
+                let times = range(runs).map(() => {
+                    const start = process.hrtime()
+                    evaluate({courses, overrides}, areaData)
+                    return now(start)
+                })
+
+                const avg = sum(times) / size(times)
+                if (graph) {
+                    console.log(`  ${sparkly(times)}\n`)
+                }
+                console.log(`  average time: ${ms(avg)} (over ${runs} runs)\n`)
+            })
+        })
+}
+
+function cli() {
+    const args = meow({
+        help: `
+            Usage
+              node students-benchmark [-r, --runs <number>] [--(no-)graph]
+        `,
+        pkg: info,
+    })
+
+    if (args.flags.args) {
+        console.log(args.flags)
+    }
+
+    const runs = args.flags.r || args.flags.runs || 50
+    const graph = args.flags.graph
+    benchmark({runs, graph})
+}
+
+if (require.main === module) {
+    cli()
+}
