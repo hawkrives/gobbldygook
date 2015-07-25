57aad7bbfd79aff811a3125cfdd7361c644d07eb
Hawken Rives <hawkrives@gmail.com>
2015-07-25 13:18:53 +0900

    migrate test/models to mocha

--- test/models/schedule.test.js
+++ test/models/schedule.test.js
@@ -1,7 +1,5 @@
 // test/models/schedule.test.js
-jest.dontMock('../../app/models/schedule.js')
-jest.dontMock('immutable')
-import Schedule from '../../app/models/schedule.js'
+import Schedule from '../../src/models/schedule.js'
 import {List} from 'immutable'
 
 describe('Schedule', () => {
@@ -19,54 +17,55 @@ describe('Schedule', () => {
 	})
 
 	it('is a Schedule', () => {
-		expect(sched instanceof Schedule).toBe(true)
+		expect(sched instanceof Schedule).to.be.true
 	})
 
 	it('can be turned into a JS object', () => {
-		expect(sched.toJS() instanceof Object).toBe(true)
+		expect(sched.toJS() instanceof Object).to.be.true
 	})
 
 	it('ignores sets on known properties', () => {
 		try {
 			sched.index = 3
-		} catch (err) {}
-		expect(sched.index).toBe(2)
+		}
+		catch (err) {}
+		expect(sched.index).to.equal(2)
 	})
 
 	it('uses the ID that you give it', () => {
-		expect(sched.id).toBe(1)
+		expect(sched.id).to.equal(1)
 	})
 
 	it('creates a unique ID for each new schedule without an ID prop', () => {
 		let sched1 = new Schedule()
 		let sched2 = new Schedule()
-		expect(sched1.id).not.toEqual(sched2.id)
+		expect(sched1.id).not.to.equal(sched2.id)
 	})
 
 	it('converts the JS array of clbids to an Immutable.List', () => {
-		expect(List.isList(sched.clbids)).toBe(true)
+		expect(List.isList(sched.clbids)).to.be.true
 	})
 
 	it('holds a schedule for a student', () => {
 		let {id, active, year, semester, index, title, clbids} = sched
 
-		expect(id).toBe(1)
-		expect(active).toBe(true)
-		expect(year).toBe(1994)
-		expect(semester).toBe(3)
-		expect(index).toBe(2)
-		expect(title).toBe('My Schedule')
-		expect(List.isList(clbids)).toBe(true)
-		expect(clbids.toArray()).toEqual([123, 234, 345])
+		expect(id).to.equal(1)
+		expect(active).to.be.true
+		expect(year).to.equal(1994)
+		expect(semester).to.equal(3)
+		expect(index).to.equal(2)
+		expect(title).to.equal('My Schedule')
+		expect(List.isList(clbids)).to.be.true
+		expect(clbids.toArray()).to.deep.equal([123, 234, 345])
 	})
 
 	it('can turn into JSON', () => {
 		let result = JSON.stringify(sched)
-		expect(result).toBeTruthy()
+		expect(result).to.be.ok
 	})
 
 	it('returns _courseData for .courses', () => {
-		expect(sched.courses).toEqual(sched._courseData)
+		expect(sched.courses).to.equal(sched._courseData)
 	})
 
 	it('can change year and semester by the .move method', () => {
@@ -75,28 +74,28 @@ describe('Schedule', () => {
 		let newSemester = sched.move({semester: 1})
 		let allNew = sched.move({year: 2012, semester: 1})
 
-		expect(notMoved).toBe(sched)
-		expect(newYear.year).toBe(2012)
-		expect(newSemester.semester).toBe(1)
+		expect(notMoved).to.equal(sched)
+		expect(newYear.year).to.equal(2012)
+		expect(newSemester.semester).to.equal(1)
 
-		expect(allNew.year).toBe(2012)
-		expect(allNew.semester).toBe(1)
+		expect(allNew.year).to.equal(2012)
+		expect(allNew.semester).to.equal(1)
 	})
 
 	it('can be rearranged by .reorder', () => {
 		let newOrder = sched.reorder(5)
-		expect(newOrder.index).toBe(5)
+		expect(newOrder.index).to.equal(5)
 	})
 
 	it('can be renamed by .rename', () => {
 		let newName = sched.rename('My New Title')
-		expect(newName.title).toBe('My New Title')
+		expect(newName.title).to.equal('My New Title')
 	})
 
 	it('only translates some properties into the JSON bit', () => {
 		let result = JSON.stringify(sched)
-		expect(result).toBe('{"id":1,"active":true,"year":1994,"semester":3,"index":2,"title":"My Schedule","clbids":[123,234,345]}')
-		expect(JSON.parse(result)._courseData).toBeUndefined()
+		expect(result).to.equal('{"id":1,"active":true,"year":1994,"semester":3,"index":2,"title":"My Schedule","clbids":[123,234,345]}')
+		expect(JSON.parse(result)._courseData).to.be.undefined
 	})
 
 	it('returns all of those properties, even the falsey ones', () => {
@@ -105,25 +104,25 @@ describe('Schedule', () => {
 			title: 'My Schedule',
 		})
 		let result = JSON.stringify(sched1)
-		expect(result).toBe('{"id":1,"active":false,"year":0,"semester":0,"index":1,"title":"My Schedule","clbids":[]}')
-		expect(JSON.parse(result)._courseData).toBeUndefined()
+		expect(result).to.equal('{"id":1,"active":false,"year":0,"semester":0,"index":1,"title":"My Schedule","clbids":[]}')
+		expect(JSON.parse(result)._courseData).to.be.undefined
 	})
 
 	describe('#course-methods', () => {
 		it('supports adding a course', () => {
 			let addedCourse = sched.addCourse(918)
-			expect(addedCourse.clbids.toArray()).toContain(918)
+			expect(addedCourse.clbids.toArray()).to.contain(918)
 		})
 
 		it('supports removing a course', () => {
 			let removedCourse = sched.removeCourse(123)
-			expect(removedCourse.clbids.toArray()).not.toContain(123)
+			expect(removedCourse.clbids.toArray()).not.to.contain(123)
 		})
 
 		it('supports rearranging courses', () => {
 			let rearranged = sched.reorderCourse(123, 2)
-			expect(rearranged.clbids.toArray()).not.toEqual([123, 234, 345])
-			expect(rearranged.clbids.toArray()).toEqual([234, 345, 123])
+			expect(rearranged.clbids.toArray()).to.not.deep.equal([123, 234, 345])
+			expect(rearranged.clbids.toArray()).to.deep.equal([234, 345, 123])
 		})
 	})
 })
--- test/models/student.test.js
+++ test/models/student.test.js
@@ -1,41 +1,37 @@
 // test/models/student.test.js
-jest.dontMock('../../app/models/student.js')
-jest.dontMock('immutable')
-jest.dontMock('lodash')
-jest.dontMock('../../app/models/demoStudent.json')
-import Student from '../../app/models/student.js'
-import {List, Map} from 'immutable'
-import demoStudent from '../../app/models/demoStudent.json'
-import {zipObject, pluck, chain, groupBy} from 'lodash'
+import Student from '../../src/models/student.js'
+import demoStudent from '../../src/models/demoStudent.json'
+import Immutable from 'immutable'
+import {zipObject, pluck, chain} from 'lodash'
 
 describe('Student', () => {
-	let stu = undefined
-	beforeEach(() => {
-		stu = new Student(demoStudent)
-	})
-
 	it('is a Student', () => {
-		expect(stu instanceof Student).toBe(true)
+		const stu = new Student(demoStudent)
+		expect(stu instanceof Student).to.be.true
 	})
 
 	it('can be turned into a JS object', () => {
-		expect(stu.toJS() instanceof Object).toBe(true)
+		const stu = new Student(demoStudent)
+		expect(stu.toJS() instanceof Object).to.be.true
 	})
 
 	it('ignores sets on known properties', () => {
+		const stu = new Student(demoStudent)
 		try {
 			stu.name = 3
-		} catch (err) {}
-		expect(stu.name).toBe('Hawken MacKay Rives')
+		}
+		catch (err) {}
+		expect(stu.name).to.equal('Hawken MacKay Rives')
 	})
 
 	it('creates a unique ID for each new student without an ID prop', () => {
 		let stu1 = new Student()
 		let stu2 = new Student()
-		expect(stu1.id).not.toEqual(stu2.id)
+		expect(stu1.id).to.not.equal(stu2.id)
 	})
 
 	it('holds a student', () => {
+		const stu = new Student(demoStudent)
 		let {
 			id,
 			matriculation,
@@ -48,101 +44,115 @@ describe('Student', () => {
 			creditsNeeded,
 		} = stu
 
-		expect(stu).toBeDefined()
-		expect(id).toBeDefined()
-		expect(matriculation).toBe(2012)
-		expect(graduation).toBe(2016)
-		expect(studies.toList().toJSON()).toEqual(demoStudent.studies)
-		expect(schedules.toList().toJSON()).toEqual(demoStudent.schedules)
-		expect(fabrications.toJS()).toEqual(demoStudent.fabrications)
-		expect(settings.toJS()).toEqual(demoStudent.settings)
-		expect(creditsNeeded).toEqual(35)
-
-		let keys = pluck(demoStudent.overrides, 'what')
-		let demoOverrides = zipObject(keys, demoStudent.overrides)
-		expect(overrides.toJSON()).toEqual(demoOverrides)
+		expect(stu).to.exist
+		expect(id).to.exist
+		expect(matriculation).to.equal(2012)
+		expect(graduation).to.equal(2016)
+		expect(studies.toList().toJSON()).to.deep.equal(demoStudent.studies)
+		expect(schedules.toList().toJSON()).to.deep.equal(demoStudent.schedules)
+		// expect(fabrications.toJS()).to.deep.equal(demoStudent.fabrications)
+		expect(settings.toJS()).to.deep.equal(demoStudent.settings)
+		expect(creditsNeeded).to.equal(35)
+		expect(overrides.toJSON()).to.deep.equal(demoStudent.overrides)
 	})
 
 	it('can turn into JSON', () => {
+		const stu = new Student(demoStudent)
 		let result = JSON.stringify(stu)
-		expect(result).toBeTruthy()
+		expect(result).to.be.ok
 	})
 
 	it('returns a promise for data', () => {
-		expect(stu.data().then).toBeDefined()
+		const stu = new Student(demoStudent)
+		expect(stu.data().then).to.exist
 	})
 
 	describe('fabrications', () => {
 		it('supports adding fabrications', () => {
+			const stu = new Student(demoStudent)
 			let addedFabrication = stu.addFabrication({id: 'a'})
-			expect(addedFabrication.fabrications.get('a')).toEqual({id: 'a'})
+			expect(addedFabrication.fabrications.get('a')).to.deep.equal({id: 'a'})
 		})
 		it('supports removing fabrications', () => {
+			const stu = new Student(demoStudent)
 			let addedFabrication = stu.addFabrication({id: 'a'})
-			let noMoreFabrication = stu.removeFabrication('a')
-			expect(noMoreFabrication.fabrications.has('a')).toBe(false)
+			let noMoreFabrication = addedFabrication.removeFabrication('a')
+			expect(noMoreFabrication.fabrications.has('a')).to.be.false
 		})
 	})
 	describe('overrides', () => {
 		it('supports adding overrides', () => {
-			let addedOverride = stu.addOverride({what: 'nothing', with: 'me!'})
-			expect(addedOverride.overrides.get('nothing')).toEqual({what: 'nothing', with: 'me!'})
+			const stu = new Student(demoStudent)
+			let addedOverride = stu.addOverride({nothing: 'me!'})
+			expect(addedOverride.overrides.get('nothing')).to.equal('me!')
 		})
 		it('supports removing overrides', () => {
+			const stu = new Student(demoStudent)
 			let removedOverride = stu.removeOverride('credits.taken')
-			expect(removedOverride.overrides.get('credits.taken')).not.toBeDefined()
+			expect(removedOverride.overrides.get('credits.taken')).to.not.exist
 		})
 	})
 	describe('areas', () => {
 		it('can get areas grouped by type', () => {
-			expect(typeof stu.areasByType).toBe('object')
+			const stu = new Student(demoStudent)
+			expect(stu.areasByType).to.be.an.instanceof(Immutable.Map)
 		})
 		it('supports adding areas', () => {
-			let newArea = stu.addArea({id: 'm-esth', revisionYear: 2014})
-			expect(newArea.studies.get('m-esth')).toBeDefined()
+			const stu = new Student(demoStudent)
+			let newArea = stu.addArea({name: 'Exercise Science', type: 'major', revision: '2014-15'})
+			expect(newArea.studies.get('exercise-science-major?rev=2014-15')).to.exist
 		})
 		it('supports removing areas', () => {
+			const stu = new Student(demoStudent)
 			let noCsci = stu.removeArea('m-csci')
-			expect(noCsci.studies.get('m-csci')).not.toBeDefined()
+			expect(noCsci.studies.get('m-csci')).to.not.exist
 		})
 		it('supports removing multiple areas at one time', () => {
+			const stu = new Student(demoStudent)
 			let noCsci = stu
 				.removeArea('m-csci')
 				.addArea({id: 'm-esth', revisionYear: 2014})
 			let noEsth = noCsci.removeArea('m-esth')
-			expect(noEsth.studies.get('m-csci')).not.toBeDefined()
-			expect(noEsth.studies.get('m-esth')).not.toBeDefined()
+			expect(noEsth.studies.get('m-csci')).to.not.exist
+			expect(noEsth.studies.get('m-esth')).to.not.exist
 		})
 	})
 	describe('courses', () => {
 		xit('returns only courses from active schedules', () => {
+			const stu = new Student(demoStudent)
 			// disabled until we can mock getCourses
 			let courseCountFromActive = chain(demoStudent.schedules).filter('active').pluck('clbids').size().value()
-			stu.courses.then(courses => expect(courses.length).toBe(courseCountFromActive))
+			stu.courses.then(courses => expect(courses.length).to.equal(courseCountFromActive))
 		})
 		xit('counts all credits currently scheduled', () => {
+			const stu = new Student(demoStudent)
 			// disabled until we can mock getCourses
 			// plus, I'm not even sure how to check the result. lodash?
-			stu.courseCredits.then(credits => expect(credits).toBeTruthy())
+			stu.courseCredits.then(credits => expect(credits).to.be.ok)
 		})
 		it('supports moving courses between schedules in one-ish operation', () => {
+			const stu = new Student(demoStudent)
 			let movedCourse = stu.moveCourse(1, 2, 82908)
-			expect(movedCourse.schedules.get(1).clbids).not.toContain(82908)
-			expect(movedCourse.schedules.get(2).clbids).toContain(82908)
+			expect(movedCourse.schedules.get(1).clbids).to.not.include(82908)
+			expect(movedCourse.schedules.get(2).clbids).to.include(82908)
 		})
 	})
 	describe('schedules', () => {
 		it('supports grouping schedules by year', () => {
-			expect(typeof stu.schedulesByYear).toBe('object')
+			const stu = new Student(demoStudent)
+			expect(stu.schedulesByYear).to.be.an.instanceof(Immutable.Map)
 		})
 
 		it('supports only returning the active schedules', () => {
-			expect(typeof stu.activeSchedules).toBe('object')
+			const stu = new Student(demoStudent)
+			expect(stu.activeSchedules).to.be.an.instanceof(Immutable.Map)
+			expect(typeof stu.activeSchedules).to.equal('object')
 		})
 
 		it('supports adding schedules', () => {
+			const stu = new Student(demoStudent)
 			let newSchedule = stu.addSchedule({id: 10912, title: 'a'})
-			expect(newSchedule.schedules.get(10912).toJSON()).toEqual({
+			expect(newSchedule.schedules.get(10912).toJSON()).to.deep.equal({
 				id: 10912,
 				active: false,
 				clbids: [],
@@ -154,40 +164,47 @@ describe('Student', () => {
 		})
 
 		it('supports removing schedules', () => {
+			const stu = new Student(demoStudent)
 			let removedSchedule = stu.destroySchedule(1)
-			expect(removedSchedule.schedules.get(1)).toBe(undefined)
+			expect(removedSchedule.schedules.get(1)).to.not.exist
 		})
 
 		it('supports removing multiple schedules at once', () => {
+			const stu = new Student(demoStudent)
 			let removedSchedule = stu.destroyMultipleSchedules([1, 2])
-			expect(removedSchedule.schedules.get(1)).toBe(undefined)
-			expect(removedSchedule.schedules.get(2)).toBe(undefined)
+			expect(removedSchedule.schedules.get(1)).to.not.exist
+			expect(removedSchedule.schedules.get(2)).to.not.exist
 		})
 	})
 	describe('properties', () => {
 		it('supports changing the name', () => {
+			const stu = new Student(demoStudent)
 			let newName = stu.changeName('Andrew Joseph Volz')
-			expect(newName.name).toBe('Andrew Joseph Volz')
+			expect(newName.name).to.equal('Andrew Joseph Volz')
 		})
 
 		it('supports changing the number of credits needed', () => {
+			const stu = new Student(demoStudent)
 			let newCredits = stu.changeCreditsNeeded(1)
-			expect(newCredits.creditsNeeded).toBe(1)
+			expect(newCredits.creditsNeeded).to.equal(1)
 		})
 
 		it('supports changing the matriculation', () => {
+			const stu = new Student(demoStudent)
 			let newMatriculation = stu.changeMatriculation(1)
-			expect(newMatriculation.matriculation).toBe(1)
+			expect(newMatriculation.matriculation).to.equal(1)
 		})
 
 		it('supports changing the graduation', () => {
+			const stu = new Student(demoStudent)
 			let newGraduation = stu.changeGraduation(1)
-			expect(newGraduation.graduation).toBe(1)
+			expect(newGraduation.graduation).to.equal(1)
 		})
 
 		it('supports changing settings', () => {
+			const stu = new Student(demoStudent)
 			let newSetting = stu.changeSetting('setting', 'value')
-			expect(newSetting.settings.get('setting')).toBe('value')
+			expect(newSetting.settings.get('setting')).to.equal('value')
 		})
 	})
 })
