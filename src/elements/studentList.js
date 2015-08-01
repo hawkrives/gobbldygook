4f6c885b42115df993e09633bba1df1651d63abb
Hawken Rives <hawkrives@gmail.com>
2015-08-01 13:12:43 +0900

    poke at StudentList

--- src/elements/studentList.js
+++ src/elements/studentList.js
@@ -4,55 +4,70 @@ import {Link} from 'react-router'
 import studentActions from '../flux/studentActions'
 import fuzzysearch from 'fuzzysearch'
 import any from 'lodash/collection/any'
+import Button from './button'
+import List from './list'
+import Icon from './icon'
+import identity from 'lodash/utility/identity'
 
 export default class StudentList extends Component {
 	static propTypes = {
+		filter: PropTypes.string,
 		isEditing: PropTypes.bool,
 		students: PropTypes.instanceOf(Immutable.Map).isRequired,
 	}
 
 	static defaultProps = {
 		isEditing: false,
-	}
-
-	constructor() {
-		super()
-
-		// since we are starting off without any data, there is no initial value
-		this.state = {
-			studentFilter: '',
-		}
+		filter: '',
 	}
 
 	render() {
 		// console.log('StudentList#render')
 		let studentObjects = this.props.students
 			.toList()
-			.filter(s =>
-				fuzzysearch(this.state.studentFilter, s.name.toLowerCase()) ||
-				any(s.studies.map(st => st.name.toLowerCase()).toArray(), name => fuzzysearch(this.state.studentFilter, name)))
+			.filter(s => fuzzysearch(this.props.filter, s.name.toLowerCase()))
 			.sortBy(s => s.dateLastModified)
-			.map(student =>
-				<li key={student.id}>
+			.map(student => {
+				const groupedStudies = student.studies.groupBy(s => s.type)
+				return (<span key={student.id}>
 					<Link className='student-list-item' to='student' params={{id: student.id}}>
-						{
-							this.props.isEditing
-								? <span className='delete' onClick={(ev) => {
-									ev.preventDefault()
-									studentActions.destroyStudent(student.id)
-								}}>×</span>
-								: null
-						}
-						<span className='name'>{student.name || ''}</span>
-						<span className='areas'>{student.studies.map(st => st.name).toArray().join(', ')}</span>
+						<span className='student-list-item-info'>
+							<div className='name'>{student.name || ''}</div>
+							<div className='areas'>
+								{['degree', 'major', 'concentration', 'emphasis']
+									.map(type => groupedStudies.get(type))
+									.filter(identity)
+									.map(group =>
+										group.toArray()
+											.map(s => s.name)
+											.join(' · '))
+									.map((group, i, coll) =>
+										<span className='area-type' key={i}>
+											{group}{i < coll.length - 1 ? ' / ' : null}
+										</span>)}
+							</div>
+						</span>
+						<span className='student-list-item-actions'>
+							{
+								this.props.isEditing
+									? <Button className='delete' type='raised'
+										onClick={(ev) => {
+											ev.preventDefault()
+											studentActions.destroyStudent(student.id)}
+										}>Delete</Button>
+									: null
+							}
+						</span>
+						<Icon className='student-list-item--go' name='ionicon-ios-arrow-forward' />
 					</Link>
-				</li>)
+				</span>)
+			})
 			.toArray()
 
 		return (
-			<ol className='student-list'>
+			<List className='student-list' type='plain' canSelect>
 				{studentObjects}
-			</ol>
+			</List>
 		)
 	}
 }
