aa550c9c09782b107a26ff683daac9eb45cb7277
Hawken Rives <hawkrives@gmail.com>
2015-08-03 10:45:21 +0900

    migrate LoadingScreen to ES6

--- src/elements/loadingScreen.js
+++ src/elements/loadingScreen.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, {Component, PropTypes} from 'react'
+import cx from 'classnames'
 
-class LoadingScreen extends React.Component {
+export default class LoadingScreen extends Component {
+	static propTypes = {
+		className: PropTypes.string,
+		message: PropTypes.string.isRequired,
+	}
+
 	render() {
-		// console.log('LoadingScreen#render')
 		return (<figure className='loading-screen'>
 			<div className='loading-spinner'><div /></div>
-			<figcaption className={`loading-message ${this.props.className}`}>{this.props.message}</figcaption>
+			<figcaption className={cx('loading-message', this.props.className)}>
+				{this.props.message}
+			</figcaption>
 		</figure>)
 	}
 }
-LoadingScreen.propTypes = {
-	className: React.PropTypes.string,
-	message: React.PropTypes.string.isRequired,
-}
-
-export default LoadingScreen
