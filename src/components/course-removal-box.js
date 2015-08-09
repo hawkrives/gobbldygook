f0873ea45deff3a38034f10388dba69834532503
Hawken Rives <hawkrives@gmail.com>
2015-08-09 18:32:59 -0500

    add an icon to <CourseRemovalBox/>

--- src/components/course-removal-box.js
+++ src/components/course-removal-box.js
@@ -44,6 +44,7 @@ class CourseRemovalBox extends Component {
 
 		return this.props.connectDropTarget(
 			<div className={className}>
+				<Icon name='ionicon-ios-trash-outline' type='block' style={{fontSize: '3em', textAlign: 'center'}} />
 				Drop a course here to remove it.
 			</div>
 		)
