7a6e8746152ed07409dd4546f2da24b2f0e72347
Hawken Rives <hawkrives@gmail.com>
2015-08-08 21:21:09 -0700

    change default overrides value from [] to {}

--- src/lib/evaluate.js
+++ src/lib/evaluate.js
@@ -1,7 +1,7 @@
 import assertKeys from './assert-keys'
 import compute from './compute'
 
-export default function evaluate({courses=[], overrides=[]}, area) {
+export default function evaluate({courses=[], overrides={}}, area) {
 	assertKeys(area, 'name', 'result', 'type', 'revision')
 	const {name, type} = area
 	return compute(area, {path: [type, name], courses, overrides})
