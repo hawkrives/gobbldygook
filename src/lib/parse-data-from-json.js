4543d932a4aa2e9448adf5e85b8af32ea1db4492
Hawken Rives <hawkrives@gmail.com>
2015-08-11 22:43:25 -0500

    allow parseDataFromJson to accept data as an argument, instead of using a global

--- src/lib/parse-data-from-json.js
+++ src/lib/parse-data-from-json.js
@@ -1,8 +1,7 @@
-import SISData from './sis-degreeaudit.json'
 import parseSIS from './parse-sis'
 
-export default function parseDataFromJSON() {
+export default function parseDataFromJSON(jsonData) {
 	let parser = new DOMParser()
-	let html = parser.parseFromString(SISData.sis, 'text/html')
+	let html = parser.parseFromString(jsonData.sis, 'text/html')
 	return parseSIS(html)
 }
