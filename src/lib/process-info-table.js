548ae1093da77b5012e630d4274fbf47043d6110
Hawken Rives <hawkrives@gmail.com>
2015-08-11 22:41:32 -0500

    reduce in processInfoTable, instead of forEaching

--- src/lib/process-info-table.js
+++ src/lib/process-info-table.js
@@ -1,11 +1,11 @@
-import forEach from 'lodash/collection/forEach'
+import reduce from 'lodash/collection/reduce'
 
 export default function processInfoTable(infoTable) {
-	let info = {}
-	forEach(infoTable.rows, row => {
-		let rowName = row.cells[0].textContent.replace(/:$/, '').toLowerCase()
-		info[rowName] = row.cells[1].textContent
-	})
+	let info = reduce(infoTable.rows, (obj, row) => {
+		const rowName = row.cells[0].textContent.replace(/:$/, '').toLowerCase()
+		obj[rowName] = row.cells[1].textContent
+		return obj
+	}, {})
 
 	info.graduation = parseInt(info['class year'])
 	delete info['class year']
