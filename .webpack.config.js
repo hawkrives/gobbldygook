35e9c277eb94cf37168ee744f648618d606d4881
Hawken Rives <hawkrives@gmail.com>
2015-08-12 19:14:21 -0500

    make webpack output relative paths
    https://github.com/webpack/file-loader/issues/3#issuecomment-52463600

--- .webpack.config.js
+++ .webpack.config.js
@@ -8,7 +8,10 @@ module.exports = getConfig({
 
   // Name or full path of output directory commonly named `www` or `public`.
   // This is where your fully static site should end up for simple deployment.
-  out: 'build',
+  out: 'build/',
+  output: {
+    publicPath: '',
+  },
 
   // This will destroy and re-create your `out` folder before building so you
   // always get a fresh folder. Usually you want this but since it's
