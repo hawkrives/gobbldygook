606408c4bdef3b603e0218a156b6b7d065af723e
Hawken Rives <hawkrives@gmail.com>
2015-08-13 13:04:08 -0500

    add google analytics

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ React.initializeTouchEvents(true)
 import './index.scss'
 
 import './start-things/bind-keys'
+import './start-things/analytics'
 import './start-things/start-notifications'
 import './start-things/start-global-pollution'
 import './start-things/start-data-loading'
---
+++ src/start-things/analytics.js
@@ -0,0 +1,15 @@
+export function isogram() {
+	window.GoogleAnalyticsObject = 'ga'
+	window.ga.q = [['create', 'UA-10662325-7', 'auto'], ['send', 'pageview']]
+	window.ga.l = Number(new Date())
+
+	let script = document.createElement('script')
+	script.async = true
+	script.src = '//www.google-analytics.com/analytics.js'
+	document.body.appendChild(script)
+}
+
+if (process.env.NODE_ENV === 'production') {
+	console.log('Initializing analytics 📊')
+	isogram()
+}
