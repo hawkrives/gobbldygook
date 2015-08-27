139c82bbed9c61cf8d81a0b70b2ae74b5dbae0e0
Hawken Rives <hawkrives@gmail.com>
2015-08-26 20:00:53 -0500

    allow counting credits from Immutable Lists

--- src/lib/count-credits.js
+++ src/lib/count-credits.js
@@ -1,5 +1,7 @@
 import sum from 'lodash/math/sum'
 import pluck from 'lodash/collection/pluck'
+import add from 'lodash/math/add'
+import {List} from 'immutable'
 
 /**
  * Sums up the number of credits offered by a set of courses
@@ -8,5 +10,8 @@ import pluck from 'lodash/collection/pluck'
  * @returns {number} - the sum of the 'credits' properties
  */
 export default function countCredits(courses) {
+	if (List.isList(courses)) {
+		return courses.map(c => c.credits).reduce(add, 0)
+	}
 	return sum(pluck(courses, 'credits'))
 }
