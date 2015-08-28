bd7af1fed16593da64e5622965d2e5f92887aa52
Hawken Rives <hawkrives@gmail.com>
2015-08-28 13:38:10 -0500

    add test for computeReference

---
+++ test/areas/compute-reference.test.js
@@ -0,0 +1,30 @@
+import {computeReference} from '../../src/lib/compute-chunk'
+
+describe('computeReference', () => {
+	it('returns the result of the referenced requirement', () => {
+		const expr = {$requirement: 'Req'}
+		const ctx = {Req: {computed: true}}
+		expect(computeReference({expr, ctx})).to.have.property('computedResult', true)
+	})
+
+	it('supports spaces in the requirement name', () => {
+		const expr = {$requirement: 'Req Name'}
+		const ctx = {'Req Name': {computed: true}}
+		expect(computeReference({expr, ctx})).to.have.property('computedResult', true)
+	})
+
+	xit('returns the list of matches, if present', () => {
+		const expr = {$requirement: 'Req Name'}
+		const ctx = {'Req Name': {computed: true, matches: ['Match']}}
+		expect(computeReference({expr, ctx})).to.deep.equal({
+			computedResult: true,
+			matches: ['Match'],
+		})
+	})
+
+	it('throws a ReferenceError if the referenced requirement doesn\'t exist', () => {
+		const expr = {$requirement: 'A'}
+		const ctx = {ONLY: {}}
+		expect(() => computeReference({expr, ctx})).to.throw(ReferenceError)
+	})
+})
