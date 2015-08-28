e8b2fe984dbe56ecb86082912bdd58247d8a7b3b
Hawken Rives <hawkrives@gmail.com>
2015-08-28 13:33:28 -0500

    add coverage for counters

--- test/areas/parse-hanson-string.test.js
+++ test/areas/parse-hanson-string.test.js
@@ -448,9 +448,32 @@ describe('parse hanson-string', () => {
         })
     })
 
-    xdescribe('counters', () => {
-        xit('n may be in english from "zero" to "twenty"', () => {})
-        xit('n may be a number from 0 to (at least) 999', () => {})
+    describe('counters', () => {
+        it('n may be in english from "zero" to "ten"', () => {
+            expect(() => parse('zero occurrences of AAAA 101')).to.not.throw()
+            expect(() => parse('one occurrence of AAAA 101')).to.not.throw()
+            expect(() => parse('two occurrences of AAAA 101')).to.not.throw()
+            expect(() => parse('three occurrences of AAAA 101')).to.not.throw()
+            expect(() => parse('four occurrences of AAAA 101')).to.not.throw()
+            expect(() => parse('five occurrences of AAAA 101')).to.not.throw()
+            expect(() => parse('six occurrences of AAAA 101')).to.not.throw()
+            expect(() => parse('seven occurrences of AAAA 101')).to.not.throw()
+            expect(() => parse('eight occurrences of AAAA 101')).to.not.throw()
+            expect(() => parse('nine occurrences of AAAA 101')).to.not.throw()
+            expect(() => parse('ten occurrences of AAAA 101')).to.not.throw()
+        })
+        it('may be prefixed by "at most" (≤)', () => {
+            expect(parse('at most ten occurrences of AAAA 101'))
+                .to.have.property('$count').deep.equal({$num: 10, $operator: '$lte'})
+        })
+        it('may be prefixed by "exactly" (==)', () => {
+            expect(parse('exactly ten occurrences of AAAA 101'))
+                .to.have.property('$count').deep.equal({$num: 10, $operator: '$eq'})
+        })
+        it('with no prefix are assumed to be ≥', () => {
+            expect(parse('ten occurrences of AAAA 101'))
+                .to.have.property('$count').deep.equal({$num: 10, $operator: '$gte'})
+        })
     })
 
     describe('of-statements', () => {
