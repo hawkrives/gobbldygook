64f54f01e093706921137f2cc5543ea49f95e9d6
Hawken Rives <hawkrives@gmail.com>
2015-08-28 13:38:19 -0500

    add test for computeChunk

--- test/areas/compute-chunk.test.js
+++ test/areas/compute-chunk.test.js
@@ -1,38 +1,11 @@
-// it('doesn\'t mark courses as used unless the requirements are met', () => {
-//     const clause = {
-//         $type: 'boolean',
-//         $and: [
-//             {$type: 'course', $course: {department: ['CHEM'], number: 121}},
-//             {$type: 'course', $course: {department: ['CHEM'], number: 123}},
-//             {$type: 'course', $course: {department: ['CHEM'], number: 125}},
-//         ],
-//     }
-//     const requirement = {result: clause}
-//     const courses = [
-//         {department: ['CHEM'], name: 'Gen Chemistry/Lab', number: 121, lab: true},
-//         {department: ['CHEM'], name: 'Atom/Molec Structure', number: 123},
-//     ]
-//
-//     const result = computeBoolean({expr: clause, ctx: requirement, courses, dirty: new Set()})
-//     expect(clause).to.deep.equal({
-//         $type: 'boolean',
-//         $and: [
-//             {
-//                 $type: 'course',
-//                 department: ['CHEM'],
-//                 number: 121,
-//             },
-//             {
-//                 $type: 'course',
-//                 department: ['CHEM'],
-//                 number: 123,
-//             },
-//             {
-//                 $type: 'course',
-//                 department: ['CHEM'],
-//                 number: 125,
-//             },
-//         ],
-//     })
-//     expect(result).to.have.property('computedResult', false)
-// })
+import computeChunk from '../../src/lib/compute-chunk'
+
+describe('computeChunk', () => {
+	it('requires that the expression be an object', () => {
+		expect(() => computeChunk({expr: 'string'})).to.throw(TypeError)
+	})
+
+	it('throws when encountering an unknown type', () => {
+		expect(() => computeChunk({expr: {$type: 'invalid'}})).to.throw(TypeError)
+	})
+})
