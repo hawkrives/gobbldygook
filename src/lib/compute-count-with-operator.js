81563dfe5ec822d51565057116546a6710dca12f
Hawken Rives <hawkrives@gmail.com>
2015-08-30 21:30:44 -0500

    make $eq to the first branch in computeCountWIthOperator

--- src/lib/compute-count-with-operator.js
+++ src/lib/compute-count-with-operator.js
@@ -1,13 +1,13 @@
 export default function computeCountWithOperator({comparator, has, needs}) {
 	// compute the result
-	if (comparator === '$lte') {
+	if (comparator === '$eq') {
+		return has === needs
+	}
+	else if (comparator === '$lte') {
 		return has <= needs
 	}
 	else if (comparator === '$gte') {
 		return has >= needs
-	}
-	else if (comparator === '$eq') {
-		return has === needs
 	}
 
 	throw new TypeError(`computeModifier(): "${comparator}" must be one of $eq, $lte, or $gte.`)
