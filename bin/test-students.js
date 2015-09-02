219dd5fecf1404bd861befe8d5aedfa2058fc047
Hawken Rives <hawkrives@gmail.com>
2015-09-02 11:43:13 -0500

    prepare to be able to test a single student at a time

--- bin/test-students.js
+++ bin/test-students.js
@@ -7,11 +7,15 @@ import path from 'path'
 
 import {describe, it} from 'mocha'
 import {expect} from 'chai'
+import yaml from 'js-yaml'
 
 const studentDir = './test/example-students/'
 
 function loadStudent(filename) {
-	return JSON.parse(readFileSync(filename, {encoding: 'utf-8'}))
+	const data = yaml.safeLoad(readFileSync(filename, {encoding: 'utf-8'}))
+	data.areas = data.areas.map(loadArea)
+	data.filename = filename
+	return data
 }
 
 function getStudentNames() {
@@ -20,22 +24,21 @@ function getStudentNames() {
 		.map(filename => path.resolve(studentDir + filename))
 }
 
-export function cli() {
-	getStudentNames()
-		.map(filename => {
-			const s = loadStudent(filename)
-			s.areas = s.areas.map(loadArea)
-			return {...s, filename}
-		})
-		.forEach(({courses, overrides, areas, filename, expectation=true, pending=false}) => {
-			const func = pending ? describe.skip : describe
-			func(path.basename(filename), () => {
-				areas.forEach(data => {
-					it(`${expectation ? 'should' : 'should not'} pass ${data.name}`, () => {
-						const result = evaluate({courses, overrides}, data)
-						expect(result).to.have.property('computed', expectation)
-					})
-				})
+export function testStudent(studentFileName) {
+	const {courses, overrides, areas, filename, expectation=true, pending=false} = loadStudent(studentFileName)
+
+	const func = pending ? describe.skip : describe
+
+	func(path.basename(filename), () => {
+		areas.forEach(data => {
+			it(`${expectation ? 'should' : 'should not'} pass ${data.name}`, () => {
+				const result = evaluate({courses, overrides}, data)
+				expect(result).to.have.property('computed', expectation)
 			})
 		})
+	})
+}
+
+export function cli() {
+	getStudentNames().forEach(testStudent)
 }
