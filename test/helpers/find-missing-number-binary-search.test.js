dc29c4f58d53b5147c84ac2ff7bd126efd3cdd83
Hawken Rives <hawkrives@gmail.com>
2015-09-02 09:20:32 -0500

    broke apart the findMissingNumBinSearch tests

--- test/helpers/find-missing-number-binary-search.test.js
+++ test/helpers/find-missing-number-binary-search.test.js
@@ -2,19 +2,17 @@ import findMissingNumberBinarySearch from '../../src/helpers/find-missing-number
 
 describe('findMissingNumberBinarySearch', () => {
 	it('takes a list of numbers and finds the first gap', () => {
-		let nums = [
-			[1, 2, 3],
-			[0, 1, 2],
-			[0, 3, 4],
-		]
+		expect(findMissingNumberBinarySearch([1, 2, 3])).to.equal(null)
+		expect(findMissingNumberBinarySearch([1, 2, 3])).to.not.equal(4)
+	})
 
-		expect(findMissingNumberBinarySearch(nums[0])).to.equal(null)
-		expect(findMissingNumberBinarySearch(nums[0])).to.not.equal(4)
+	it('returns null if there is no gap', () => {
+		expect(findMissingNumberBinarySearch([0, 1, 2])).to.equal(null)
+	})
 
-		expect(findMissingNumberBinarySearch(nums[1])).to.equal(null)
-
+	it('returns the high-end of a multi-numer gap', () => {
 		// I mean, ideally this would return 1, but for now...
-		expect(findMissingNumberBinarySearch(nums[2])).to.equal(2)
-		expect(findMissingNumberBinarySearch(nums[2])).to.not.equal(1)
+		expect(findMissingNumberBinarySearch([0, 3, 4])).to.equal(2)
+		expect(findMissingNumberBinarySearch([0, 3, 4])).to.not.equal(1)
 	})
 })
