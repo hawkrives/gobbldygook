2fd5f3c9de6d5900259068f717d058826a76372a
Hawken Rives <hawkrives@gmail.com>
2015-09-03 13:33:11 -0500

    port compile-student to nomnom

--- bin/compile-student.js
+++ bin/compile-student.js
@@ -1,25 +1,19 @@
-import meow from 'meow'
-import pkg from '../package.json'
+import nom from 'nomnom'
 import fs from 'graceful-fs'
 import yaml from 'js-yaml'
 import enhanceHanson from '../src/lib/enhance-hanson'
 
 export function cli() {
-	const args = meow({
-		pkg,
-		help: `Usage:
-			compile-student areaFile`,
-	})
+	const args = nom()
+		.script('compile-student')
+		.option('filename', {
+			position: 0,
+			required: true,
+			help: 'the file to process',
+		})
 
-	const [filename] = args.input
-
-	if (filename) {
-		const data = fs.readFileSync(filename, {encoding: 'utf-8'})
-		const obj = yaml.safeLoad(data)
-		const enhanced = enhanceHanson(obj, {topLevel: true})
-		console.log(JSON.stringify(enhanced, null, 2))
-	}
-	else {
-		args.showHelp()
-	}
+	const data = fs.readFileSync(args.filename, {encoding: 'utf-8'})
+	const obj = yaml.safeLoad(data)
+	const enhanced = enhanceHanson(obj, {topLevel: true})
+	console.log(JSON.stringify(enhanced, null, 2))
 }
