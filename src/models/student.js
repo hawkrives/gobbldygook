844188cf09d13cfb260e5a954d2b1de4b4849da4
Hawken Rives <hawkrives@gmail.com>
2015-09-09 18:06:52 -0500

    add some commentary to Student()

--- src/models/student.js
+++ src/models/student.js
@@ -217,13 +217,20 @@ export default class Student extends StudentRecord {
 	// getters
 
 	get courses() {
-		const allCourses = this.schedules
-			.filter(sched => sched.active)
-			.map(schedule => schedule.courses)
-			.toArray()
+		// - At it's core, this method just needs to get the list of courses that a student has chosen.
+		// - Each schedule has a list of courses that are a part of that schedule.
+		// - Additionally, we only care about the schedules that are marked as "active".
+		// - Keep in mind that each list of courses is actually a *promise* for the courses.
+		// - We also need to make sure to de-duplicate the final list of courses, so that each `clbid` only appears once.
+		// - Finally, remember that a given `clbid` might not exist in the database, in which case we get back 'undefined'.
+		//   In this case, we need to know where the `clbid` came from, so that we can render an error in the correct location.
 
-		const scheduleCoursePromises = Promise.all(allCourses)
-		return scheduleCoursePromises.then(scheduleCourses => uniq(flatten(scheduleCourses), course => course.clbid))
+		const activeSchedules = this.schedules.filter(s => s.active)
+		const promisesForCourses = activeSchedules.map(s => s.courses).toArray()
+
+		return Promise.all(promisesForCourses)
+			.then(courses => uniq(flatten(courses), course => course.clbid))
+			.catch(err => console.log(err))
 	}
 
 
