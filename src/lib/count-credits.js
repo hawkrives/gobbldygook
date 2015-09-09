ec4c63c94a9d7c898ef18365eebf60ae2ddcf294
Hawken Rives <hawkrives@gmail.com>
2015-09-09 18:09:17 -0500

    stop using lodash in countCredits

--- src/lib/count-credits.js
+++ src/lib/count-credits.js
@@ -1,7 +1,4 @@
-import sum from 'lodash/math/sum'
-import pluck from 'lodash/collection/pluck'
 import add from 'lodash/math/add'
-import {List} from 'immutable'
 
 /**
  * Sums up the number of credits offered by a set of courses
@@ -10,8 +7,5 @@ import {List} from 'immutable'
  * @returns {number} - the sum of the 'credits' properties
  */
 export default function countCredits(courses) {
-	if (List.isList(courses)) {
-		return courses.map(c => c.credits).reduce(add, 0)
-	}
-	return sum(pluck(courses, 'credits'))
+	return courses.map(c => c.credits).reduce(add, 0)
 }
