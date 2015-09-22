61994de4729b0477629bcff62542bccd591300a9
Hawken Rives <hawkrives@gmail.com>
2015-09-22 09:10:21 -0500

    blah

--- src/components/area-of-study.js
+++ src/components/area-of-study.js
@@ -1,5 +1,5 @@
 import React, {Component, PropTypes} from 'react'
-import {Link} from 'react-router'
+// import {Link} from 'react-router'
 import cx from 'classnames'
 
 import Button from './button'
