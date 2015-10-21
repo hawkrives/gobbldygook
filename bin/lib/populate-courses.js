a091f90470602b827ea001c47aaa902bf7725b08
Hawken Rives <hawkrives@gmail.com>
2015-10-21 00:00:44 -0500

    fix up courses for evaluate

--- bin/lib/populate-courses.js
+++ bin/lib/populate-courses.js
@@ -1,13 +1,15 @@
 import flatten from 'lodash/array/flatten'
 import includes from 'lodash/collection/includes'
 
-import updateData from './update-local-data-cache'
-
 import searchCourses from './search-for-courses'
 
 export default async function populateCourses(student) {
-	await updateData()
 	const clbids = student.clbids || flatten(student.schedules.map(s => s.clbids))
-
-	return searchCourses({riddles: course => includes(clbids, course.clbid)})
+	let courses = await searchCourses({riddles: [course => includes(clbids, course.clbid)]})
+	courses = courses.map(c => {
+		c.department = c.depts
+		c.number = c.num
+		return c
+	})
+	return courses
 }
