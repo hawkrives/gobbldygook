b62be39c8a531d274f5439720df2db107c137e33
Hawken Rives <hawkrives@gmail.com>
2015-10-21 10:05:07 -0500

    added script to group courses by gereq count

---
+++ bin/olaf-count-courses-with-ges.js
@@ -0,0 +1,31 @@
+import nomnom from 'nomnom'
+import Promise from 'bluebird'
+import sparkly from 'sparkly'
+import search from './lib/search-for-courses'
+import map from 'lodash/collection/map'
+import mapValues from 'lodash/object/mapValues'
+import zipObject from 'lodash/array/zipObject'
+import groupBy from 'lodash/collection/groupBy'
+import yaml from 'js-yaml'
+
+function findCoursesWithGes(term) {
+	return search({riddles: [{term}]})
+}
+
+export async function cli() {
+	const args = nomnom()
+		.option('term', {list: true, default: []})
+		.option('year', {list: true, default: []})
+		.parse()
+
+	args.term = args.term.concat(...args.year.map(y => [1, 2, 3, 4, 5].map(s => parseInt(`${y}${s}`))))
+	const courses = zipObject(await Promise.all(map(args.term, async term => [term, await findCoursesWithGes(term)])))
+
+	// console.log(courses)
+	// const counts = mapValues(courses, c => c.length)
+	// console.log(counts)
+
+	const groupedByGeCount = mapValues(courses, list => mapValues({...groupBy(list, c => c.gereqs && c.gereqs.length || 0), total: list}, l => l.length))
+	console.log(yaml.safeDump(groupedByGeCount))
+	// console.log(sparkly(sizes))
+}
