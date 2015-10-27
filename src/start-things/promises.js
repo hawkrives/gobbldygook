0885c9329e872fd259e0510a32c3164ad1882db0
Hawken Rives <hawkrives@gmail.com>
2015-10-27 10:29:23 -0500

    Update bluebird config for 3.0.0

--- src/start-things/promises.js
+++ src/start-things/promises.js
@@ -1,6 +1,13 @@
 // apply global overrides stuff here
-global.Promise = require('bluebird')
 require('babel-runtime/core-js/promise').default = require('bluebird')
+global.Promise = require('bluebird')
+
+if (process.env.NODE_ENV === 'development') {
+	global.Promise.config({
+		warnings: true,
+		longStackTraces: true,
+	})
+}
 
 // handle promise errors
 const throwError = e => {
