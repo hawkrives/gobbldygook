b50f2e003d22f2aab1169d822731981e71109a06
Hawken Rives <hawkrives@gmail.com>
2015-11-09 16:14:05 -0600

    manually curate the index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports.checkCoursesForTimeConflicts = require("./lib/checkCoursesForTimeConflicts")
+module.exports.checkScheduleForTimeConflicts = require("./lib/checkScheduleForTimeConflicts")
+module.exports.convertTimeStringsToOfferings = require("./lib/convertTimeStringsToOfferings")
+module.exports.findScheduleTimeConflicts = require("./lib/findScheduleTimeConflicts")
