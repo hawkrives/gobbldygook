4376940b798f7f0ea92a440ad1280a795e93d2a5
Hawken Rives <hawkrives@gmail.com>
2015-11-10 19:17:05 -0600

    disable custom unhandled rejection handling

--- src/start-things/promises.js
+++ src/start-things/promises.js
@@ -1,13 +1,3 @@
 // apply global overrides stuff here
 global.Promise = require('bluebird')
 require('babel-runtime/core-js/promise').default = require('bluebird')
-
-// handle promise errors
-const throwError = e => {
-	e.preventDefault()
-	console.warn(e)
-}
-
-if (typeof window !== 'undefined') {
-	window.addEventListener('unhandledrejection', throwError)
-}
