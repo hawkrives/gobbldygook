d5546770ec3af9c3bc9d27390e1b0ea6824c2aa4
Hawken Rives <hawkrives@gmail.com>
2015-11-14 15:42:11 -0600

    … and provide the proper path, too

--- test/mocha-harness.js
+++ test/mocha-harness.js
@@ -1,5 +1,5 @@
 global.Promise = require('bluebird')
 global.debug = require('debug')
 
-global.VERSION = JSON.stringify(require('../../package.json').version)
+global.VERSION = JSON.stringify(require('../package.json').version)
 global.DEV = false
