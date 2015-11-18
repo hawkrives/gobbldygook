b8dbd612cb31dc796df3829451b2bd6db827bb28
Hawken Rives <hawkrives@gmail.com>
2015-11-18 09:06:59 -0600

    extract loadArea

---
+++ src/models/load-area.js
@@ -0,0 +1,23 @@
+import enhanceHanson from '../lib/enhance-hanson'
+import includes from 'lodash/collection/includes'
+
+export async function loadArea(path) {
+	const db = require('../lib/db')
+
+	let data
+	try {
+		data = await db.store('areas').get(path)
+		if (!data && includes(path, '?')) {
+			data = await db.store('areas').get(path.split('?')[0])
+		}
+	}
+	catch (err) {
+		throw new Error(`Could not load area ${path}`)
+	}
+
+	if (typeof data === 'undefined') {
+		throw new Error(`Could not find any areas with the query ${path}`)
+	}
+
+	return enhanceHanson(data, {topLevel: true})
+}
