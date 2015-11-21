14fe2379cbd96666f6767cd9553e61deaba8c06a
Hawken Rives <hawkrives@gmail.com>
2015-11-21 16:25:37 -0600

    when populating courses from schedules, only use the active ones

--- bin/lib/populate-courses.js
+++ bin/lib/populate-courses.js
@@ -4,7 +4,7 @@ import includes from 'lodash/collection/includes'
 import searchCourses from './search-for-courses'
 
 export default async function populateCourses(student) {
-	const clbids = student.clbids || flatten(student.schedules.map(s => s.clbids))
+	const clbids = student.clbids || flatten(student.schedules.filter(s => s.active).map(s => s.clbids))
 	let courses = await searchCourses({riddles: [course => includes(clbids, course.clbid)]})
 	courses = courses.map(c => {
 		c.department = c.depts
