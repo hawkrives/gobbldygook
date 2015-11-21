826bc604e1d409c3e6b20ec22ea7085d2fea418c
Hawken Rives <hawkrives@gmail.com>
2015-11-21 16:25:25 -0600

    expanded the loadArea function

--- bin/lib/load-area.js
+++ bin/lib/load-area.js
@@ -4,9 +4,26 @@ import findAreaPath from '../../src/lib/find-area-path'
 import path from 'path'
 import fs from 'graceful-fs'
 
-export default function loadArea({name, type, revision}) {
-	const filepath = path.join('area-data', `${findAreaPath({name, type, revision})}.yaml`)
-	const data = fs.readFileSync(filepath, {encoding: 'utf-8'})
-	const obj = yaml.safeLoad(data)
-	return enhanceHanson(obj, {topLevel: true})
+export default function loadArea({name, type, revision, source, isCustom}) {
+	let obj = {}
+	if (isCustom) {
+		obj = yaml.safeLoad(source)
+	}
+	else {
+		const filepath = path.join('area-data', `${findAreaPath({name, type, revision})}.yaml`)
+		const data = fs.readFileSync(filepath, {encoding: 'utf-8'})
+		obj = yaml.safeLoad(data)
+	}
+
+
+	let result
+	try {
+		// console.log(obj)
+		result = enhanceHanson(obj, {topLevel: true})
+	}
+	catch (err) {
+		console.error(`Problem enhancing area "${name}"`, err)
+	}
+
+	return result
 }
