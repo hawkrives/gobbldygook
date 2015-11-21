f2242f19d64f8cfebceed8ee4d6057e613c0534a
Hawken Rives <hawkrives@gmail.com>
2015-11-21 16:42:41 -0600

    update find-area-path to handle the "latest" revision

--- src/lib/find-area-path.js
+++ src/lib/find-area-path.js
@@ -6,5 +6,5 @@ export function slugifyAreaName(name) {
 }
 
 export default function findAreaPath({name, type, revision}) {
-	return `${pluralizeArea(type)}/${slugifyAreaName(name)}-${revision}`
+	return `${pluralizeArea(type)}/${slugifyAreaName(name)}` + (revision === 'latest' ? '' : `-${revision}`)
 }
--- test/helpers/find-area-path.test.js
+++ test/helpers/find-area-path.test.js
@@ -3,7 +3,18 @@ import findAreaPath from '../../src/lib/find-area-path'
 
 describe('findAreaPath', () => {
 	it('handles apostrophes in the name', () => {
-		expect(findAreaPath({name: `Women's and Gender Studies`, type: 'concentration', revision: '2014-15'}))
-			.to.equal('concentrations/womens-and-gender-studies-2014-15')
+		expect(findAreaPath({
+			name: `Women's and Gender Studies`,
+			type: 'concentration',
+			revision: '2014-15',
+		})).to.equal('concentrations/womens-and-gender-studies-2014-15')
+	})
+
+	it('expects the latest revision to not have a revision in the title', () => {
+		expect(findAreaPath({
+			name: 'Computer Science',
+			type: 'major',
+			revision: 'latest',
+		})).to.equal('majors/computer-science')
 	})
 })
