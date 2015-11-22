1652db5a44ff258f6a14d4466ebb715f376c882d
Hawken Rives <hawkrives@gmail.com>
2015-11-21 20:12:58 -0600

    fix convert-semi-to-json

--- bin/olaf-convert-semi-to-json.js
+++ bin/olaf-convert-semi-to-json.js
@@ -8,14 +8,19 @@ const unzip = require('lodash/array/unzip')
 const first = require('lodash/array/first')
 
 const args = nomnom()
-	.option('file', {position: 0, required: true, list: false, help: 'The semicolon-separated files from the registrar to load'})
+	.option('file', {
+		position: 0,
+		required: true,
+		list: false,
+		help: 'The semicolon-separated files from the registrar to load',
+	})
 	.parse()
 
 const input = fs.readFileSync(args.file, 'utf-8')
 
-let splitten = input
-	.split(';')
-	.filter(s => s && s.trim().length)
+let splitten = input.split(';')
+
+// console.log(splitten)
 
 let indexOfFirstCourse = splitten.findIndex(item => /\d{5,6}/.test(item))
 
@@ -26,10 +31,7 @@ let onlyCoursesAndGereqs = unzip(
 
 let onlyMajors = splitten.slice(0, indexOfFirstCourse)
 
-// console.log(onlyMajors)
-// console.log(onlyCoursesAndGereqs)
-
 console.log(JSON.stringify({
 	studies: onlyMajors.map(m => ({type: 'major', name: m, revision: 'latest'})),
-	clbids: onlyCoursesAndGereqs.map(cAndGe => first(cAndGe)).map(c => Number(c)),
+	clbids: onlyCoursesAndGereqs.map(cAndGe => first(cAndGe)).filter(s => s.trim().length).map(c => Number(c)),
 }, null, 2))
