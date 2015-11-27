53d1be09814414e3897af2f23baed30c98cb2c13
Hawken Rives <hawkrives@gmail.com>
2015-11-26 19:36:52 -0600

    add a courseCache to getCourse

--- src/helpers/get-course.js
+++ src/helpers/get-course.js
@@ -1,5 +1,7 @@
 import db from './db'
 
+const courseCache = new WeakMap()
+
 /**
  * Gets a course from the database.
  *
@@ -9,13 +11,39 @@ import db from './db'
  * @fulfill {Object} - the course object.
  * @reject {Error} - a message about retrieval failing.
  */
-function getCourse(clbid) {
+export default async function getCourse(clbid) {
 	// console.log('called getCourse', clbid)
-	return db
-		.store('courses')
-		.index('clbid')
-		.get(clbid)
-		.catch(error => ({clbid, error}))
+
+	if (courseCache.has(clbid)) {
+		let value = courseCache.get(clbid)
+		if (value instanceof Promise) {
+			return await value
+		}
+		else {
+			return value
+		}
+		return courseCache.get(clbid)
+	}
+
+	let course
+	try {
+		const coursePromise = db.store('courses').index('clbid').get(clbid)
+		courseCache.set(clbid, coursePromise)
+		course = await coursePromise
+	}
+	catch (error) {
+		return {clbid, error}
+	}
+
+	courseCache.set(clbid, course)
+
+	return course
 }
 
-export default getCourse
+// export default function getCourse(clbid) {
+// 	return db
+// 		.store('courses')
+// 		.index('clbid')
+// 		.get(clbid)
+// 		.catch(error => ({clbid, error}))
+// }
