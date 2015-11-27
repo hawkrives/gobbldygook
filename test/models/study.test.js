a184417169a9f677802036fd425dde8248f3b842
Hawken Rives <hawkrives@gmail.com>
2015-11-27 13:40:18 -0600

    enable the Study tests

--- test/models/study.test.js
+++ test/models/study.test.js
@@ -1,12 +1,11 @@
 import {expect} from 'chai'
-import proxyquire from 'proxyquire'
+import mock from 'mock-require'
 import stringify from 'json-stable-stringify'
 
-describe('Study', () => {
-	const Study = proxyquire('../../src/models/study', {
-		'./load-area': () => Promise.resolve({}),
-	})
+mock('../../src/models/load-area', require('../mocks/load-area.mock'))
+const Study = require('../../src/models/study')
 
+describe('Study', () => {
 	it('holds an area of study for a student', () => {
 		let csci = new Study({type: 'major', name: 'Computer Science', revision: '2014-15'})
 
@@ -20,19 +19,6 @@ describe('Study', () => {
 	})
 
 	it('can turn into JSON', () => {
-		let csci = new Study({type: 'major', name: 'Computer Science', revision: '2014-15'})
-		let result = stringify(csci)
-
-		expect(JSON.parse(result)).to.deep.equal({
-			name: 'Computer Science',
-			path: 'majors/computer-science/2014-15',
-			revision: '2014-15',
-			type: 'major',
-			source: '',
-		})
-	})
-
-	it('only translates some properties into the JSON bit', () => {
 		let csci = new Study({type: 'major', name: 'Computer Science', revision: '2014-15'})
 		let result = stringify(csci)
 
