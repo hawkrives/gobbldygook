42886002c4dff0e4c0bfd83107664c200536d670
Hawken Rives <hawkrives@gmail.com>
2015-11-27 20:53:33 -0600

    erm, this is part of the enabling of the devtools.

--- src/ducks/reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import notifications from './reducers/notifications'
-import students from './reducers/students'
-
-export default combineReducers({
-	notifications,
-	students,
-})
