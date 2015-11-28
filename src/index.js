03b7caca82931fcd6d4cff31cdcf3598570d7b5c
Hawken Rives <hawkrives@gmail.com>
2015-11-27 20:53:45 -0600

    disable a bunch of stuff temporarily

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import './start-things/promises'
 
-import 'isomorphic-fetch'
+// import 'isomorphic-fetch'
 
 import './index.scss'
 
-import './start-things/bind-keys'
-import './start-things/analytics'
+// import './start-things/bind-keys'
+// import './start-things/analytics'
 import './start-things/start-notifications'
-import './start-things/start-global-pollution'
-import './start-things/start-data-loading'
-import routes from './start-things/start-router'
-import history from './start-things/history'
+// import './start-things/start-global-pollution'
+// import './start-things/start-data-loading'
+// import routes from './start-things/start-router'
+// import history from './start-things/history'
 
 // run it
 console.log('3. 2.. 1... Blast off! 🚀')
 
-import React from 'react'
-import {render} from 'react-dom'
-import {Router} from 'react-router'
+// import React from 'react'
+// import {render} from 'react-dom'
+// import {Router} from 'react-router'
 
-render(
-	React.createElement(Router, {history, routes}),
-	document.getElementById('app'))
+// render(
+// 	React.createElement(Router, {history, routes}),
+// 	document.getElementById('app'))
