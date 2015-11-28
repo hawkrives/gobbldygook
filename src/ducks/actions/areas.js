aa9d0e5fbdbc58c4e811d8e3f4cac56c794bf824
Hawken Rives <hawkrives@gmail.com>
2015-11-28 13:20:45 -0600

    remove message from loadAreas action

--- src/ducks/actions/areas.js
+++ src/ducks/actions/areas.js
@@ -5,7 +5,6 @@ import {
 import db from '../../helpers/db'
 
 export async function loadAreas() {
-	console.log('loadAreas')
 	let areas = await db.store('areas').all()
 	return { type: LOAD_AREAS, payload: areas }
 }
