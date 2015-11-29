8dd4ac3bff091af65457c0e346619d501186b05e
Hawken Rives <hawkrives@gmail.com>
2015-11-29 01:44:41 -0600

    begin porting <Sidebar/> to redux
    also move search state management from state to the querystring

--- src/screens/sidebar.js
+++ src/screens/sidebar.js
@@ -1,91 +1,88 @@
 import React, {Component, PropTypes} from 'react'
-import history from '../start-things/history'
-
-import studentActions from '../flux/student-actions'
-import studentStore from '../flux/student-store'
+import omit from 'lodash/object/omit'
+import history from '../history'
 
 import Button from '../components/button'
-import CourseSearcher from './course-searcher'
-import GraduationStatus from './graduation-status'
+// import CourseSearcher from './course-searcher'
+// import GraduationStatus from './graduation-status'
 import Icon from '../components/icon'
 import Toolbar from '../components/toolbar'
 import Separator from '../components/separator'
 
-import CourseRemovalBox from '../components/course-removal-box'
+// import CourseRemovalBox from '../components/course-removal-box'
 
 import './sidebar.scss'
 
 export default class Sidebar extends Component {
 	static propTypes = {
-		allAreas: PropTypes.arrayOf(PropTypes.object),
-		baseSearchQuery: PropTypes.object,
-		courses: PropTypes.arrayOf(PropTypes.object),
-		coursesLoaded: PropTypes.bool.isRequired,
-		isSearching: PropTypes.bool.isRequired,
+		actions: PropTypes.object.isRequired,
+		canRedo: PropTypes.bool.isRequired,
+		canUndo: PropTypes.bool.isRequired,
 		student: PropTypes.object.isRequired,
-		toggleSearchSidebar: PropTypes.func.isRequired,
 	}
 
 	static contextTypes = {
-		router: PropTypes.func,
+		location: PropTypes.object,
+	}
+
+	goHome = () => {
+		history.pushState(null, '/')
+	}
+
+	showShareSheet = () => {
+		console.log(this.context.location.query)
+		const query = {...this.context.location.query, share: true}
+		console.log(query)
+		history.pushState(null, this.context.location.pathname, query)
+	}
+
+	removeSearchQuery = () => {
+		const query = omit(this.context.location.query, ['partialSearch', 'searchTerm'])
+		history.pushState(null, this.context.location.pathname, query)
+	}
+
+	showSearchSidebar = () => {
+		const query = {...this.context.location.query, searchTerm: ''}
+		history.pushState(null, this.context.location.pathname, query)
 	}
 
 	render() {
+		const { actions, canUndo, canRedo, student } = this.props
+		const isSearching = 'partialSearch' in this.context.location.query || 'searchTerm' in this.context.location.query
+
 		return (
 			<aside className='sidebar'>
 				<Toolbar className='student-buttons'>
-					<Button title='Students' onClick={() => history.pushState(null, '/')}>
-							<Icon name='ios-people-outline' type='block' />
+					<Button title='Students' onClick={this.goHome}>
+						<Icon name='ios-people-outline' type='block' />
 					</Button>
-					<Button
-						title='Search'
-						onClick={this.props.toggleSearchSidebar}>
+					<Button title='Search' onClick={isSearching ? this.removeSearchQuery : this.showSearchSidebar}>
 						<Icon name='ios-search' type='block' />
 					</Button>
 
 					<Separator type='spacer' />
 
-					<Button
-						title='Undo'
-						onClick={studentActions.undo}
-						disabled={studentStore.history.size === 0}>
-						<Icon name={`ios-undo${studentStore.history.size === 0 ? '-outline' : ''}`} type='block' />
+					<Button title='Undo' onClick={actions.undo} disabled={!canUndo}>
+						<Icon name={`ios-undo${!canUndo ? '-outline' : ''}`} type='block' />
 					</Button>
-					<Button
-						title='Redo'
-						onClick={studentActions.redo}
-						disabled={studentStore.future.size === 0}>
-						<Icon name={`ios-redo${studentStore.future.size === 0 ? '-outline' : ''}`} type='block' />
+					<Button title='Redo' onClick={actions.redo} disabled={!canRedo}>
+						<Icon name={`ios-redo${!canRedo ? '-outline' : ''}`} type='block' />
 					</Button>
 
 					<Separator type='spacer' />
 
-					<Button title='Download' onClick={() => history.pushState(null, `/s/${this.props.student.id}/download/`)}>
-						<Icon name='ios-download-outline' type='block' />
-					</Button>
-					<Button
-						title='Revert to Demo'
-						onClick={() => studentActions.resetStudentToDemo(this.props.student.id)}>
-						<Icon name='ios-reload' type='block' />
+					<Button title='Share' onClick={this.showShareSheet}>
+						<Icon name='ios-upload-outline' type='block' />
 					</Button>
 				</Toolbar>
 
-				<CourseRemovalBox studentId={this.props.student.id} />
+				{/*<CourseRemovalBox studentId={student.id} />*/}
 
-				{!this.props.isSearching
-					? <GraduationStatus
-						allAreas={this.props.allAreas}
-						courses={this.props.courses}
-						coursesLoaded={this.props.coursesLoaded}
-						isHidden={this.props.isSearching}
-						student={this.props.student}
-					/>
-					: <CourseSearcher
-						isHidden={!this.props.isSearching}
-						toggle={this.props.toggleSearchSidebar}
-						student={this.props.student}
-						baseSearchQuery={this.props.baseSearchQuery}
-					/>}
+				<div>{isSearching ? 'Is searching!' : 'is not searching…'}</div>
+
+				{/*!isSearching
+					? <GraduationStatus isHidden={isSearching} />
+					: <CourseSearcher isHidden={!isSearching} />*/}
 			</aside>
 		)
 	}
