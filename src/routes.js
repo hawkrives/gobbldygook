89448570b7eb5aece8662c0377e6a4ec6e05f101
Hawken Rives <hawkrives@gmail.com>
2015-11-29 01:43:04 -0600

    enable <Student/> in routes

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import App from './screens/app'
 // import DownloadStudent from './screens/download-student'
 // import NewStudentWizard from './screens/new-student-wizard'
 // import SemesterDetail from './screens/semester-detail'
-// import Student from './screens/student'
+import Student from './screens/student'
 import Degub from './components/degub'
 import StudentPicker from './screens/student-picker'
 
@@ -22,13 +22,14 @@ export default (
 		<IndexRoute component={StudentPicker} />
 		<Route component={Degub} path='degub' />
 		<Redirect from='debug' to='degub' />
-		{/*<Route component={CreateStudent} path='create-student/' />
+		{/*<Route component={CreateStudent} path='create-student/' />*/}
 		<Route component={Student} path='s/:id/'>
-			<IndexRoute component={CourseTable} />
+			<IndexRoute component={Degub} />
+			{/*<IndexRoute component={CourseTable} />
 			{//<Route component={AreaEditor} path='edit-area' />}
 			<Route component={NewStudentWizard} path='wizard/' />
 			<Route component={SemesterDetail} path='semester/:year/:semester/' />
-			<Route component={DownloadStudent} path='download/' />
-		</Route>*/}
+			<Route component={DownloadStudent} path='download/' />*/}
+		</Route>
 	</Route>
 )
