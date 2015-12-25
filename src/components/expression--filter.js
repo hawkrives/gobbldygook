6935af308f67057288da236bf5a868375c8ab2e5
Hawken Rives <hawkrives@gmail.com>
2015-12-25 16:26:12 -0600

    remove some unused code

--- src/components/expression--filter.js
+++ src/components/expression--filter.js
@@ -1,13 +1,5 @@
 import React, {Component, PropTypes} from 'react'
 
-class FilterWhere extends Component {
-
-}
-
-class FilterOf extends Component {
-
-}
-
 export default class Filter extends Component {
 	static propTypes = {
 		filter: PropTypes.object,
