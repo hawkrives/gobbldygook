455c44b3c9e36aed5a927fd26a4da9631a4ceeb8
Hawken Rives <hawkrives@gmail.com>
2015-12-25 18:57:04 -0600

    pass a description of the action across postMessage
    since we can't transfer promises, we'll just transfer the info that needs
    to be transferred, and let the main thread actually create the action.

--- src/helpers/load-data.js
+++ src/helpers/load-data.js
@@ -2,9 +2,24 @@ import map from 'lodash/collection/map'
 import {status, text} from './fetch-helpers'
 import uniqueId from 'lodash/utility/uniqueId'
 
+import * as notificationActions from '../ducks/actions/notifications'
+import * as courseActions from '../ducks/actions/courses'
+import * as areaActions from '../ducks/actions/areas'
+const actions = {
+	notifications: notificationActions,
+	courses: courseActions,
+	areas: areaActions,
+}
+
 import Worker from './load-data.worker.js'
 const worker = new Worker()
 worker.onerror = msg => console.warn('[main] received error from load-data worker:', msg)
+worker.onmessage = ({data: [resultId, type, actionInfo]}) => {
+	if (resultId === null && type === 'dispatch') {
+		const action = actions[actionInfo.type][actionInfo.action](...actionInfo.args)
+		typeof window !== 'undefined' && window.dispatch && window.dispatch(action)
+	}
+}
 
 function loadDataFile(url) {
 	return new Promise((resolve, reject) => {
@@ -13,10 +28,6 @@ function loadDataFile(url) {
 
 		// This is inside of the function so that it doesn't get unregistered too early
 		function onMessage({data: [resultId, type, contents]}) {
-			if (type === 'dispatch') {
-				typeof window !== 'undefined' && window.dispatch && window.dispatch(contents)
-			}
-
 			if (resultId === sourceId) {
 				worker.removeEventListener('message', onMessage)
 
