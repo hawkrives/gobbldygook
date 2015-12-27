c933f32c7c67a3709783a5f031e1bf091fef611f
Hawken Rives <hawkrives@gmail.com>
2015-12-27 13:29:13 -0600

    make loadAreas not an async function

--- src/ducks/actions/areas.js
+++ src/ducks/actions/areas.js
@@ -1,10 +1,10 @@
+import db from '../../helpers/db'
+
 import {
 	LOAD_AREAS,
 } from '../constants/areas'
 
-import db from '../../helpers/db'
-
-export async function loadAreas() {
-	let areas = await db.store('areas').getAll()
-	return { type: LOAD_AREAS, payload: areas }
+export function loadAreas() {
+	let areasPromise = db.store('areas').getAll()
+	return { type: LOAD_AREAS, payload: areasPromise }
 }
