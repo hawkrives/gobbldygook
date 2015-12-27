898119e4c4bf4658222e040c49974266bcf00ad6
Hawken Rives <hawkrives@gmail.com>
2015-12-27 14:45:57 -0600

    create a CourseSearcherModal

---
+++ src/screens/course-searcher-sheet.js
@@ -0,0 +1,36 @@
+import React, {PropTypes} from 'react'
+
+import omit from 'lodash/object/omit'
+import history from '../history'
+import Modal from '../components/modal'
+import CourseSearcher from './course-searcher'
+
+function closeModal(location) {
+	const query = omit(location.query, ['search-overlay'])
+	history.pushState(null, location.pathname, query)
+}
+
+export default function CourseSearcherSheet(props, context) {
+	const { actions } = props
+	const boundCloseModal = closeModal.bind(null, context.location)
+
+	return <Modal
+		backdropClassName='modal-backdrop'
+		modalClassName='course-searcher--modal'
+		onClose={boundCloseModal}
+	>
+		<CourseSearcher
+			actions={actions}
+			closeSearcher={boundCloseModal}
+			isHidden={false}
+		/>
+	</Modal>
+}
+
+CourseSearcherSheet.propTypes = {
+	actions: PropTypes.objectOf(PropTypes.func).isRequired,
+}
+
+CourseSearcherSheet.contextTypes = {
+	location: PropTypes.object,
+}
