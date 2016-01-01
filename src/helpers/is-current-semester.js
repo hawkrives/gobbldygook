6295626c99f95d6c2d31793c3ec1fa236725ac41
Hawken Rives <hawkrives@gmail.com>
2016-01-01 11:52:39 -0600

    simplify isCurrentSemester

--- src/helpers/is-current-semester.js
+++ src/helpers/is-current-semester.js
@@ -1,19 +1,16 @@
-import curry from 'lodash/function/curry'
 import parseInt from 'lodash/string/parseInt'
 
 /**
  * Checks if a schedule is in a certain year and semester.
  *
- * @param {Number} year
- * @param {Number} semester
- * @param {Schedule} schedule
- * @returns {Boolean}
+ * @param {Number} year - a year
+ * @param {Number} semester - a semester
+ * @param {Schedule} schedule - a schedule
+ * @returns {Boolean} - is the schedule part of the current semester
  */
-let isCurrentSemester = curry((year=0, semester=0, schedule) => {
+export default function isCurrentSemester(year, semester) {
 	year = parseInt(year)
 	semester = parseInt(semester)
 
-	return (schedule.year === year) && (schedule.semester === semester)
-})
-
-export default isCurrentSemester
+	return schedule => (schedule.year === year) && (schedule.semester === semester)
+}
