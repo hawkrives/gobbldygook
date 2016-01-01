6359a84b2b8c881570ff6b6a40648c1a19f7c0fe
Hawken Rives <hawkrives@gmail.com>
2016-01-01 11:52:00 -0600

    add functions for converting imported students

---
+++ src/helpers/convert-imported-student.js
@@ -0,0 +1,60 @@
+// import db from './db'
+import Student from '../models/student'
+import Schedule from '../models/schedule'
+import groupBy from 'lodash/collection/groupBy'
+import map from 'lodash/collection/map'
+import pluck from 'lodash/collection/pluck'
+import forEach from 'lodash/collection/forEach'
+import uniq from 'lodash/array/uniq'
+import zipObject from 'lodash/array/zipObject'
+
+export default function convertStudent(partialStudent) {
+	let schedules = groupBy(partialStudent.courses, 'term')
+	schedules = map(schedules, (courses, term) => {
+		term = String(term)
+		return Schedule({
+			active: true,
+			clbids: pluck(courses, 'clbid'),
+			year: parseInt(term.substr(0, 4), 10),
+			semester: parseInt(term.substr(4, 1), 10),
+		})
+	})
+	schedules = zipObject(map(schedules, s => [s.id, s]))
+
+	let thereShouldOnlyBeOne = {
+		names: pluck(partialStudent.degrees, 'name'),
+		advisors: pluck(partialStudent.degrees, 'advisor'),
+		matriculations: pluck(partialStudent.degrees, 'matriculation'),
+		graduations: pluck(partialStudent.degrees, 'graduation'),
+	}
+
+	forEach(thereShouldOnlyBeOne, (group, name) => {
+		if (uniq(group).length !== 1) {
+			throw new Error(`convertStudent: The student has more than one ${name}: ${JSON.stringify(group)}`)
+		}
+	})
+
+	let name = thereShouldOnlyBeOne.names[0]
+	let advisor = thereShouldOnlyBeOne.advisors[0]
+	let matriculation = parseInt(thereShouldOnlyBeOne.matriculations[0], 10)
+	let graduation = parseInt(thereShouldOnlyBeOne.graduations[0], 10)
+	let studies = []
+
+	for (let {concentrations, emphases, majors, degree} of partialStudent.degrees) {
+		studies.push({name: degree, type: 'degree', revision: 'latest'})
+		studies = studies.concat(majors.map(name =>         ({name, type: 'major', revision: 'latest'})))
+		studies = studies.concat(concentrations.map(name => ({name, type: 'concentration', revision: 'latest'})))
+		studies = studies.concat(emphases.map(name =>       ({name, type: 'emphasis', revision: 'latest'})))
+	}
+
+	let newStudent = Student({
+		name,
+		advisor,
+		matriculation,
+		graduation,
+		schedules,
+		studies,
+	})
+
+	return newStudent
+}
