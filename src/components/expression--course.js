aa7ec5a8cf5c634c07be4a64fb59480a2793bd7b
Hawken Rives <hawkrives@gmail.com>
2016-01-13 09:21:00 -0600

    make CourseExpression a pure function

--- src/components/expression--course.js
+++ src/components/expression--course.js
@@ -1,4 +1,4 @@
-import React, {Component, PropTypes} from 'react'
+import React, {PropTypes} from 'react'
 import cx from 'classnames'
 import semesterName from '../helpers/semester-name'
 import {shrinkDept} from '../area-tools/convert-department'
@@ -6,62 +6,60 @@ import map from 'lodash/collection/map'
 
 import './expression--course.scss'
 
-export default class CourseExpression extends Component {
-	static propTypes = {
-		_result: PropTypes.bool,
-		department: PropTypes.arrayOf(PropTypes.string).isRequired,
-		international: PropTypes.bool,
-		lab: PropTypes.bool,
-		level: PropTypes.number,
-		number: PropTypes.number,
-		section: PropTypes.string,
-		semester: PropTypes.number,
-		style: PropTypes.object,
-		year: PropTypes.number,
-	};
+export default function CourseExpression(props) {
+	const department = map(props.department, shrinkDept).join('/')
 
-	render() {
-		const department = map(this.props.department, shrinkDept).join('/')
+	const international = props.international &&
+		<span className='course--international'>I</span>
+	const lab = props.lab &&
+		<span className='course--lab'>L</span>
 
-		const international = this.props.international &&
-			<span className='course--international'>I</span>
-		const lab = this.props.lab &&
-			<span className='course--lab'>L</span>
+	const section = props.section && props.section !== '*' &&
+		<span className='course--section'>[{props.section}]</span>
 
-		const section = this.props.section && this.props.section !== '*' &&
-			<span className='course--section'>[{this.props.section}]</span>
+	const year = props.year &&
+		<span className='course--year'>{props.year}</span>
+	const semester = props.semester &&
+		<span className='course--semester'>
+			{props.semester === '*'
+				? 'ANY'
+				: semesterName(props.semester).toUpperCase()}
+		</span>
 
-		const year = this.props.year &&
-			<span className='course--year'>{this.props.year}</span>
-		const semester = this.props.semester &&
-			<span className='course--semester'>
-				{this.props.semester === '*'
-					? 'ANY'
-					: semesterName(this.props.semester).toUpperCase()}
-			</span>
+	/////
 
-		/////
+	const temporalIdentifiers = (semester || year) &&
+		(<div className='temporal'>
+			{semester}
+			{year}
+		</div>)
 
-		const temporalIdentifiers = (semester || year) &&
-			(<div className='temporal'>
-				{semester}
-				{year}
-			</div>)
+	return (
+		<span className={cx('course', {matched: props._result})} style={props.style}>
+			<div className='basic-identifiers'>
+				<span className='course--department'>{department}</span>
+				<span>
+					<span className='course--number'>{props.number || String(props.level)[0] + 'XX'}</span>
+					{international}
+					{lab ? 'L' : null}
+					{' '}
+					{section}
+				</span>
+			</div>
+			{temporalIdentifiers}
+		</span>
+	)
+}
 
-		return (
-			<span className={cx('course', {matched: this.props._result})} style={this.props.style}>
-				<div className='basic-identifiers'>
-					<span className='course--department'>{department}</span>
-					<span>
-						<span className='course--number'>{this.props.number || String(this.props.level)[0] + 'XX'}</span>
-						{international}
-						{lab ? 'L' : null}
-						{' '}
-						{section}
-					</span>
-				</div>
-				{temporalIdentifiers}
-			</span>
-		)
-	}
+CourseExpression.propTypes = {
+	_result: PropTypes.bool,
+	department: PropTypes.arrayOf(PropTypes.string).isRequired,
+	international: PropTypes.bool,
+	lab: PropTypes.bool,
+	level: PropTypes.number,
+	number: PropTypes.number,
+	section: PropTypes.string,
+	semester: PropTypes.number,
+	style: PropTypes.object,
+	year: PropTypes.number,
 }
