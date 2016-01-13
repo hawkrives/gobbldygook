51ac8edb5cd68887e9f614f17f1d3b373079278f
Hawken Rives <hawkrives@gmail.com>
2016-01-13 00:08:23 -0600

    make modal a pure function

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,4 +1,4 @@
-import React, {Component, PropTypes} from 'react'
+import React, {PropTypes} from 'react'
 import cx from 'classnames'
 import ReactModal2 from 'react-modal2'
 import ReactGateway from 'react-gateway'
@@ -9,25 +9,23 @@ if (typeof window !== 'undefined') {
 	ReactModal2.setApplicationElement(document.getElementById('app'))
 }
 
-export default class Modal extends Component {
-	static propTypes = {
-		backdropClassName: PropTypes.string,
-		children: PropTypes.node.isRequired,
-		modalClassName: PropTypes.string,
-		onClose: PropTypes.func.isRequired,
-	};
+export default function Modal(props) {
+	return (
+		<ReactGateway>
+			<ReactModal2
+				onClose={props.onClose}
+				backdropClassName={cx('modal--backdrop', props.backdropClassName)}
+				modalClassName={cx('modal--content', props.modalClassName)}
+			>
+				{props.children}
+			</ReactModal2>
+		</ReactGateway>
+	)
+}
 
-	render() {
-		return (
-			<ReactGateway>
-				<ReactModal2
-					onClose={this.props.onClose}
-					backdropClassName={cx('modal--backdrop', this.props.backdropClassName)}
-					modalClassName={cx('modal--content', this.props.modalClassName)}
-				>
-					{this.props.children}
-				</ReactModal2>
-			</ReactGateway>
-		)
-	}
+Modal.propTypes = {
+	backdropClassName: PropTypes.string,
+	children: PropTypes.node.isRequired,
+	modalClassName: PropTypes.string,
+	onClose: PropTypes.func.isRequired,
 }
