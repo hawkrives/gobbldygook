9f7fd5b5016370b24142e834eaee6e3d9c09c96d
Hawken Rives <hawkrives@gmail.com>
2016-01-18 09:54:19 -0600

    allow getStudentData to go fetch data from the db

--- src/helpers/get-student-data.js
+++ src/helpers/get-student-data.js
@@ -2,8 +2,8 @@ import embedActiveStudentCourses from './embed-active-student-courses'
 import getStudentStudies from './get-student-studies'
 
 export default function getStudentData(student, {areas, courses}) {
-	const promisedAreas = getStudentStudies(student, {cache: areas, cacheOnly: true})
-	const promisedSchedules = embedActiveStudentCourses(student, {cache: courses, cacheOnly: true})
+	const promisedAreas = getStudentStudies(student, {cache: areas})
+	const promisedSchedules = embedActiveStudentCourses(student, {cache: courses})
 
 	return Promise.all([promisedAreas, promisedSchedules])
 		.then(([loadedAreas, loadedSchedules]) => ({
