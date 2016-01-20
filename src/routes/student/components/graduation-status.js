0e815d58a4af35e52328df971b5ae1b8ff297965
Hawken Rives <hawkrives@gmail.com>
2016-01-19 20:20:43 -0600

    split GraduationStatus into component/container

---
+++ src/routes/student/components/graduation-status.js
@@ -0,0 +1,51 @@
+import React, {PropTypes} from 'react'
+
+import AreaOfStudySidebar from './area-of-study-sidebar'
+import StudentSummary from './student-summary'
+
+import './graduation-status.scss'
+
+export default function GraduationStatus(props) {
+	if (!props.student) {
+		return null
+	}
+
+	return (
+		<section className='graduation-status'>
+			<StudentSummary
+				onChangeGraduation={props.onChangeGraduation}
+				onChangeMatriculation={props.onChangeMatriculation}
+				onChangeName={props.onChangeName}
+				student={props.student}
+			/>
+			<AreaOfStudySidebar
+				allAreas={props.allAreas}
+				showAreaPickerFor={props.showAreaPickerFor}
+				onAddArea={props.onAddArea}
+				onAddOverride={props.onAddOverride}
+				onEndAddArea={props.onEndAddArea}
+				onInitiateAddArea={props.onInitiateAddArea}
+				onRemoveArea={props.onRemoveArea}
+				onRemoveOverride={props.onRemoveOverride}
+				onToggleOverride={props.onToggleOverride}
+				student={props.student}
+			/>
+		</section>
+	)
+}
+
+GraduationStatus.propTypes = {
+	allAreas: PropTypes.arrayOf(PropTypes.object).isRequired,
+	onAddArea: PropTypes.func.isRequired,
+	onAddOverride: PropTypes.func.isRequired,
+	onChangeGraduation: PropTypes.func.isRequired,
+	onChangeMatriculation: PropTypes.func.isRequired,
+	onChangeName: PropTypes.func.isRequired,
+	onEndAddArea: PropTypes.func.isRequired,
+	onInitiateAddArea: PropTypes.func.isRequired,
+	onRemoveArea: PropTypes.func.isRequired,
+	onRemoveOverride: PropTypes.func.isRequired,
+	onToggleOverride: PropTypes.func.isRequired,
+	showAreaPickerFor: PropTypes.object.isRequired,
+	student: PropTypes.object.isRequired,
+}
--- src/routes/student/containers/graduation-status.js
+++ src/routes/student/containers/graduation-status.js
@@ -1,274 +1,115 @@
 import React, {Component, PropTypes} from 'react'
-import cx from 'classnames'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
-import includes from 'lodash/collection/includes'
-import difference from 'lodash/array/difference'
-import union from 'lodash/array/union'
-import values from 'lodash/object/values'
-import find from 'lodash/collection/find'
-import map from 'lodash/collection/map'
-import mapValues from 'lodash/object/mapValues'
-import groupBy from 'lodash/collection/groupBy'
 import has from 'lodash/object/has'
-import uniq from 'lodash/array/uniq'
-import pluck from 'lodash/collection/pluck'
-import keys from 'lodash/object/keys'
-import pick from 'lodash/object/pick'
+import pathToOverride from '../../../area-tools/path-to-override'
 
-import compareProps from '../helpers/compare-props'
-import pathToOverride from '../area-tools/path-to-override'
-import checkStudentGraduatability from '../helpers/check-student-graduatability'
-import AreaOfStudyGroup from '../components/area-of-study-group'
-import Button from '../components/button'
-import StudentSummary from '../components/student-summary'
-import * as areaTypeConstants from '../models/area-types'
-
-import './graduation-status.scss'
-
-
-class GraduationStatus extends Component {
-	static propTypes = {
-		actions: PropTypes.object.isRequired,
-		addAreaToStudent: PropTypes.func.isRequired,
-		addOverrideToStudent: PropTypes.func.isRequired,
-		areaDetails: PropTypes.arrayOf(PropTypes.object).isRequired,
-		areas: PropTypes.arrayOf(PropTypes.object).isRequired,
-		canGraduate: PropTypes.bool.isRequired,
-		courses: PropTypes.arrayOf(PropTypes.object).isRequired,
-		endAddArea: PropTypes.func.isRequired,
-		initiateAddArea: PropTypes.func.isRequired,
-		isHidden: PropTypes.bool.isRequired,
-		removeAreaFromStudent: PropTypes.func.isRequired,
-		removeOverrideFromStudent: PropTypes.func.isRequired,
-		showAreaPickerFor: PropTypes.object.isRequired,
-		student: PropTypes.object.isRequired,
-		toggleOverrideOnStudent: PropTypes.func.isRequired,
-	};
-
-	static defaultProps = {
-		areaDetails: [],
-		canGraduate: false,
-	};
-
-	shouldComponentUpdate(nextProps) {
-		return compareProps(this.props, nextProps)
-	}
-
-	render() {
-		const {
-			addAreaToStudent,
-			addOverrideToStudent,
-			areaDetails,
-			areas,
-			canGraduate,
-			courses,
-			endAddArea,
-			initiateAddArea,
-			isHidden,
-			removeAreaFromStudent,
-			removeOverrideFromStudent,
-			showAreaPickerFor,
-			student,
-			toggleOverrideOnStudent,
-		} = this.props
-
-		if (!student) {
-			return null
-		}
-
-		const allAreasGrouped = groupBy(areas, 'type')
-
-		// group the studies by their type
-		const groupedStudies = groupBy(student.studies, study => study.type.toLowerCase())
-
-		// pull out the results
-		const studyResults = mapValues(groupedStudies, group =>
-			map(group, area =>
-				find(areaDetails, pick(area, ['name', 'type', 'revision'])) || area))
-
-		// and then render them
-		const sections = map(studyResults, (areas, areaType) =>
-			<AreaOfStudyGroup key={areaType}
-				addArea={addAreaToStudent}
-				addOverride={addOverrideToStudent}
-				allAreasOfType={allAreasGrouped[areaType] || []}
-				areas={areas}
-				courses={courses}
-				endAddArea={endAddArea}
-				initiateAddArea={initiateAddArea}
-				removeArea={removeAreaFromStudent}
-				removeOverride={removeOverrideFromStudent}
-				showAreaPicker={showAreaPickerFor[areaType]}
-				student={student}
-				toggleOverride={toggleOverrideOnStudent}
-				type={areaType}
-			/>)
-
-		const allAreaTypes = values(areaTypeConstants)
-		const usedAreaTypes = uniq(pluck(student.studies, 'type'))
-
-		const areaTypesToShowButtonsFor = union(
-			usedAreaTypes,
-			keys(pick(showAreaPickerFor, k => k === true)))
-
-		const unusedTypes = difference(allAreaTypes, areaTypesToShowButtonsFor)
-
-		const unusedAreaTypeButtons = (
-			<section className='unused-areas-of-study'>
-				<span className='unused-areas-title'>Add: </span>
-				<span className='unused-areas-buttons'>
-					{unusedTypes.map(type => (
-						<Button key={type}
-							className='add-unused-area-of-study'
-							onClick={ev => initiateAddArea({ev, type})}
-							type='flat'
-						>
-							{type}
-						</Button>
-					))}
-				</span>
-			</section>
-		)
-
-		const unusedTypesToShow = pick(showAreaPickerFor,
-			(toShow, type) => toShow === true && !includes(usedAreaTypes, type))
-
-		const unusedTypesToShowComponents = map(unusedTypesToShow, (toShow, type) =>
-			<AreaOfStudyGroup key={type}
-				addArea={addAreaToStudent}
-				addOverride={addOverrideToStudent}
-				allAreasOfType={allAreasGrouped[type] || []}
-				areas={[]}
-				endAddArea={endAddArea}
-				initiateAddArea={initiateAddArea}
-				removeArea={removeAreaFromStudent}
-				showAreaPicker={toShow}
-				student={student}
-				toggleOverride={toggleOverrideOnStudent}
-				type={type}
-			/>)
-
-		return (
-			<section className={cx('graduation-status', {'is-hidden': isHidden})}>
-				<StudentSummary
-					actions={this.props.actions}
-					courses={courses}
-					student={student}
-					canGraduate={canGraduate}
-				/>
-
-				{sections}
-
-				{unusedTypesToShowComponents}
-
-				{unusedTypes.length && unusedAreaTypeButtons}
-			</section>
-		)
-	}
-}
-
+import {changeName, changeMatriculation, changeGraduation} from '../../../redux/students/actions/change'
+import {addArea, removeArea} from '../../../redux/students/actions/areas'
+import {setOverride, removeOverride} from '../../../redux/students/actions/overrides'
+import GraduationStatus from '../components/graduation-status'
 
 export default class GraduationStatusContainer extends Component {
 	static propTypes = {
-		actions: PropTypes.object.isRequired,
-		areas: PropTypes.array.isRequired,
-		courses: PropTypes.array.isRequired,
-		isHidden: PropTypes.bool.isRequired,
+		addArea: PropTypes.func.isRequired, // redux
+		allAreas: PropTypes.array.isRequired, // redux
+		changeGraduation: PropTypes.func.isRequired, // redux
+		changeMatriculation: PropTypes.func.isRequired, // redux
+		changeName: PropTypes.func.isRequired, // redux
+		removeArea: PropTypes.func.isRequired, // redux
+		removeOverride: PropTypes.func.isRequired, // redux
+		setOverride: PropTypes.func.isRequired, // redux
 		student: PropTypes.object.isRequired,
 	};
 
-	constructor(props) {
-		super(props)
-		this.state = {
-			canGraduate: false,
-			areaDetails: [],
-			showAreaPickerFor: {},
-		}
-	}
+	state = {
+		showAreaPickerFor: {},
+	};
 
-	componentWillMount() {
-		this.componentWillReceiveProps(this.props)
-	}
-
-	componentWillReceiveProps(nextProps) {
-		checkStudentGraduatability(nextProps.student)
-			.then(({canGraduate, details}) => {
-				this.setState({
-					canGraduate: canGraduate,
-					areaDetails: details,
-				})
-			})
-	}
-
-	initiateAddArea = ({ev, type}) => {
+	initiateAddArea = (type, ev) => {
 		ev.preventDefault()
 		this.setState(state => ({
 			showAreaPickerFor: {...state.showAreaPickerFor, [type]: true},
 		}))
 	};
 
-	endAddArea = ({ev, type}) => {
+	endAddArea = (type, ev) => {
 		ev.preventDefault()
 		this.setState(state => ({
 			showAreaPickerFor: {...state.showAreaPickerFor, [type]: false},
 		}))
 	};
 
-	addAreaToStudent = ({ev, area}) => {
+	handleAddArea = (area, ev) => {
 		ev.preventDefault()
-		this.props.actions.addArea(this.props.student.id, area)
+		this.props.addArea(this.props.student.id, area)
 	};
 
-	addOverrideToStudent = ({ev, path}) => {
-		ev.preventDefault()
-		const codifiedPath = pathToOverride(path)
-		this.props.actions.setOverride(this.props.student.id, codifiedPath, true)
-	};
-
-	removeOverrideFromStudent = ({ev, path}) => {
+	handleAddOverride = (path, ev) => {
 		ev.preventDefault()
 		const codifiedPath = pathToOverride(path)
-		this.props.actions.setOverride(this.props.student.id, codifiedPath)
+		this.props.setOverride(this.props.student.id, codifiedPath, true)
 	};
 
-	toggleOverrideOnStudent = ({ev, path}) => {
+	handleRemoveOverride = (path, ev) => {
+		ev.preventDefault()
+		const codifiedPath = pathToOverride(path)
+		this.props.setOverride(this.props.student.id, codifiedPath)
+	};
+
+	handleToggleOverride = (path, ev) => {
 		ev.preventDefault()
 		const codifiedPath = pathToOverride(path)
 
 		if (has(this.props.student.overrides, codifiedPath)) {
-			this.props.actions.removeOverride(this.props.student.id, codifiedPath)
+			this.props.removeOverride(this.props.student.id, codifiedPath)
 		}
 		else {
-			this.props.actions.setOverride(this.props.student.id, codifiedPath, true)
+			this.props.setOverride(this.props.student.id, codifiedPath, true)
 		}
 	};
 
-	removeAreaFromStudent = ({ev, areaQuery}) => {
+	handleRemoveArea = (areaQuery, ev) => {
 		ev.preventDefault()
-		this.props.actions.removeArea(this.props.student.id, areaQuery)
+		this.props.removeArea(this.props.student.id, areaQuery)
+	};
+
+	handleChangeGraduation = ev => {
+		this.props.changeGraduation(this.props.student.id, parseInt(ev.target.value) || 0)
+	};
+	handleChangeMatriculation = ev => {
+		this.props.changeMatriculation(this.props.student.id, parseInt(ev.target.value) || 0)
+	};
+	handleChangeName = ev => {
+		this.props.changeName(this.props.student.id, ev.target.value)
 	};
 
 	render() {
-		// console.log('GraduationStatus#render')
-
+		const student = this.props.student.data.present
 		return (
 			<GraduationStatus
-				actions={this.props.actions}
-				addAreaToStudent={this.addAreaToStudent}
-				addOverrideToStudent={this.addOverrideToStudent}
-				areas={this.props.areas}
-				areaDetails={this.state.areaDetails}
-				courses={this.props.courses}
-				endAddArea={this.endAddArea}
-				canGraduate={this.state.canGraduate}
-				initiateAddArea={this.initiateAddArea}
-				isHidden={this.props.isHidden}
-				removeAreaFromStudent={this.removeAreaFromStudent}
-				removeOverrideFromStudent={this.removeOverrideFromStudent}
+				allAreas={this.props.allAreas}
+				onAddArea={this.handleAddArea}
+				onAddOverride={this.handleAddOverride}
+				onChangeGraduation={this.handleChangeGraduation}
+				onChangeMatriculation={this.handleChangeMatriculation}
+				onChangeName={this.handleChangeName}
+				onEndAddArea={this.handleEndAddArea}
+				onInitiateAddArea={this.handleInitiateAddArea}
+				onRemoveArea={this.handleRemoveArea}
+				onRemoveOverride={this.handleTemoveOverride}
+				onToggleOverride={this.handleToggleOverride}
 				showAreaPickerFor={this.state.showAreaPickerFor}
-				student={this.props.student}
-				toggleOverrideOnStudent={this.toggleOverrideOnStudent}
+				student={student}
 			/>
 		)
 	}
 }
+
+const mapDispatch = dispatch =>
+	bindActionCreators({addArea, setOverride, removeOverride, removeArea, changeName, changeMatriculation, changeGraduation}, dispatch)
+
+const mapState = state => ({allAreas: state.areas})
+
+export default connect(mapState, mapDispatch)(GraduationStatusContainer)
