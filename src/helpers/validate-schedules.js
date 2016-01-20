97f3ffae66a1b1b8b928e81fe9a02d6500614c64
Hawken Rives <hawkrives@gmail.com>
2016-01-20 15:38:51 -0600

    make validateSchedules an async function
    so that the promise is rejected if a thing throws

--- src/helpers/validate-schedules.js
+++ src/helpers/validate-schedules.js
@@ -1,8 +1,8 @@
 import mapValues from 'lodash/object/mapValues'
 import validateSchedule from './validate-schedule'
 
-export default function validateSchedules(student) {
+export default async function validateSchedules(student) {
 	let { schedules } = student
 	schedules = mapValues(schedules, validateSchedule)
-	return Promise.resolve({...student, schedules})
+	return {...student, schedules}
 }
