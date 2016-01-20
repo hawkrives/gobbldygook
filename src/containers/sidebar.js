d5b4a714ab23c246dd1fa149abf59c0e89ce19be
Hawken Rives <hawkrives@gmail.com>
2016-01-19 20:22:03 -0600

    update Sidebar

--- src/containers/sidebar.js
+++ src/containers/sidebar.js
@@ -1,94 +1,82 @@
-import React, {Component, PropTypes} from 'react'
-import omit from 'lodash/object/omit'
+import React, {PropTypes} from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {Link} from 'react-router'
 
 import Button from '../components/button'
-import CourseSearcher from './course-searcher'
-import GraduationStatus from './graduation-status'
 import Icon from '../components/icon'
 import Toolbar from '../components/toolbar'
 import Separator from '../components/separator'
 
 import CourseRemovalBox from '../components/course-removal-box'
 
+import {undo, redo} from '../redux/students/actions/undo'
+import {removeCourse} from '../redux/students/actions/courses'
+
 import './sidebar.scss'
 
-export default class Sidebar extends Component {
-	static propTypes = {
-		actions: PropTypes.object.isRequired,
-		areas: PropTypes.array.isRequired,
-		canRedo: PropTypes.bool.isRequired,
-		canUndo: PropTypes.bool.isRequired,
-		courses: PropTypes.array.isRequired,
-		student: PropTypes.object.isRequired,
-	};
+export function Sidebar(props) {
+	const { undo, redo } = props
+	const studentId = props.student.data.present.id
+	const canUndo = props.student.data.past.length
+	const canRedo = props.student.data.future.length
 
-	static contextTypes = {
-		location: PropTypes.object.isRequired,
-		router: PropTypes.object.isRequired,
-	};
-
-	goHome = () => {
-		this.context.router.push('/')
-	};
-
-	showShareSheet = () => {
-		const query = {...this.context.location.query, share: null}
-		this.context.router.push({pathname: this.context.location.pathname, query})
-	};
-
-	closeSearcher = () => {
-		const query = omit(this.context.location.query, ['partialSearch', 'search'])
-		this.context.router.push({pathname: this.context.location.pathname, query})
-	};
-
-	openSearcher = () => {
-		const query = {...this.context.location.query, search: null}
-		this.context.router.push({pathname: this.context.location.pathname, query})
-	};
-
-	render() {
-		const { actions, canUndo, canRedo, student, courses, areas } = this.props
-		const isSearching = 'partialSearch' in this.context.location.query || 'search' in this.context.location.query
-
-		return (
-			<aside className='sidebar'>
-				<Toolbar className='student-buttons'>
-					<Button title='Students' onClick={this.goHome}>
+	return (
+		<aside className='sidebar'>
+			<Toolbar className='student-buttons'>
+				<Link to='/'>
+					<Button title='Students'>
 						<Icon name='ios-people-outline' type='block' />
 					</Button>
-					<Button title='Search' onClick={isSearching ? this.closeSearcher : this.openSearcher}>
+				</Link>
+				<Link to={`/s/${studentId}/search`}>
+					<Button title='Search'>
 						<Icon name='ios-search' type='block' />
 					</Button>
+				</Link>
 
-					<Separator type='spacer' />
+				<Separator type='spacer' />
 
-					<Button title='Undo' onClick={actions.undo} disabled={!canUndo}>
-						<Icon name={`ios-undo${!canUndo ? '-outline' : ''}`} type='block' />
-					</Button>
-					<Button title='Redo' onClick={actions.redo} disabled={!canRedo}>
-						<Icon name={`ios-redo${!canRedo ? '-outline' : ''}`} type='block' />
-					</Button>
+				<Button title='Undo' onClick={() => undo(studentId)} disabled={!canUndo}>
+					<Icon name={`ios-undo${!canUndo ? '-outline' : ''}`} type='block' />
+				</Button>
+				<Button title='Redo' onClick={() => redo(studentId)} disabled={!canRedo}>
+					<Icon name={`ios-redo${!canRedo ? '-outline' : ''}`} type='block' />
+				</Button>
 
-					<Separator type='spacer' />
+				<Separator type='spacer' />
 
-					<Button title='Share' onClick={this.showShareSheet}>
+				<Link to={`/s/${studentId}/share`}>
+					<Button title='Share'>
 						<Icon name='ios-upload-outline' type='block' />
 					</Button>
-				</Toolbar>
+				</Link>
+			</Toolbar>
 
-				<CourseRemovalBox studentId={student.id} actions={actions} />
+			<CourseRemovalBox
+				onRemoveCourse={(scheduleId, clbid) => props.removeCourse(studentId, scheduleId, clbid)}
+			/>
 
-				{!isSearching
-					? <GraduationStatus
-						{...{actions, student, courses, areas}}
-						isHidden={isSearching}
-					/>
-					: <CourseSearcher
-						{...{actions, student}}
-						isHidden={!isSearching}
-						closeSearcher={this.closeSearcher}
-					/>}
-			</aside>
-		)
-	}
+			{props.children}
+		</aside>
+	)
 }
+
+Sidebar.propTypes = {
+	children: PropTypes.node.isRequired,
+	redo: PropTypes.func.isRequired,
+	removeCourse: PropTypes.func.isRequired,
+	student: PropTypes.shape({
+		data: PropTypes.shape({
+			past: PropTypes.array.isRequired,
+			present: PropTypes.object.isRequired,
+			future: PropTypes.array.isRequired,
+		}).isRequired,
+	}).isRequired,
+	undo: PropTypes.func.isRequired,
+}
+
+
+const mapDispatch = dispatch => bindActionCreators({undo, redo, removeCourse}, dispatch)
+
+export default connect(undefined, mapDispatch)(Sidebar)
