9e6721ed0a358055e9c1e8371800f1bc2fac67d0
Hawken Rives <hawkrives@gmail.com>
2016-01-21 14:01:55 -0600

    fix getActiveStudentCourses test

--- test/helpers/get-active-student-courses.test.js
+++ test/helpers/get-active-student-courses.test.js
@@ -1,7 +1,7 @@
 import {expect} from 'chai'
 
 import demoStudent from '../../src/models/demo-student.json'
-import getStudentCourses from '../../src/helpers/get-student-courses'
+import getStudentCourses from '../../src/helpers/get-active-student-courses'
 
 import mock from 'mock-require'
 mock('../../src/helpers/get-courses', require('../mocks/get-courses.mock').default)
@@ -12,7 +12,7 @@ import size from 'lodash/size'
 import filter from 'lodash/filter'
 import map from 'lodash/map'
 
-describe('getStudentCourses', () => {
+describe('getActiveStudentCourses', () => {
 	it('only returns courses from active schedules', () => {
 		const stu = Student(demoStudent)
 		let courseCountFromActive = size(map(filter(demoStudent.schedules, 'active'), 'clbids'))
