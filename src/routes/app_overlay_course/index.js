f375bbc553aa5de9c45911057a0b7846a327c4a9
Hawken Rives <hawkrives@gmail.com>
2016-01-22 22:42:08 -0600

    fix another getComponents callback

--- src/routes/app_overlay_course/index.js
+++ src/routes/app_overlay_course/index.js
@@ -1,6 +1,6 @@
 export default {
 	path: 'course/:clbid',
-	getComponents(cb) {
+	getComponents(location, cb) {
 		require.ensure([], () => {
 			cb(null, {
 				overlay: require('./containers/course-overlay').default,
