64f5c32b4897942fdfc1344454986c34de2cbd9a
Hawken Rives <hawkrives@gmail.com>
2016-01-23 09:43:28 -0600

    update queryCourseDatabase to clean up some inputs.

--- src/helpers/query-course-database.js
+++ src/helpers/query-course-database.js
@@ -1,5 +1,11 @@
 import db from './db'
 import buildQueryFromString from '../helpers/build-query-from-string'
+import compact from 'lodash/compact'
+import filter from 'lodash/filter'
+import map from 'lodash/map'
+import some from 'lodash/some'
+import toPairs from 'lodash/toPairs'
+import fromPairs from 'lodash/fromPairs'
 
 export default function queryCourseDatabase(queryString, baseQuery={}) {
 	let queryObject = buildQueryFromString(queryString, {words: true, profWords: true})
@@ -11,6 +17,21 @@ export default function queryCourseDatabase(queryString, baseQuery={}) {
 	else {
 		query = {...baseQuery, ...queryObject}
 	}
+
+	// make sure that all values are wrapped in arrays
+	query = toPairs(query)
+	query = map(query, ([key, val]) => {
+		if (!Array.isArray(val)) {
+			val = [val]
+		}
+		if (some(val, v => v === undefined)) {
+			val = compact(val)
+		}
+		return [key, val]
+	})
+	query = filter(query, ([_, val]) => val.length)
+	query = fromPairs(query)
+
 	console.log('query object', query)
 
 	return db
