114d33a0628dba61fbdeac1c4a5f81b00ab211f6
Hawken Rives <hawkrives@gmail.com>
2016-01-22 21:29:48 -0600

    shrink things
    - turn off source maps
    - manually write an index.js file

--- index.babel.js
+++
@@ -1,4 +0,0 @@
-export checkCoursesForTimeConflicts from './lib/checkCoursesForTimeConflicts'
-export checkScheduleForTimeConflicts from './lib/checkScheduleForTimeConflicts'
-export convertTimeStringsToOfferings from './lib/convertTimeStringsToOfferings'
-export findScheduleTimeConflicts from './lib/findScheduleTimeConflicts'
--- index.js
+++ index.js
@@ -1,30 +1,6 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.findScheduleTimeConflicts = exports.convertTimeStringsToOfferings = exports.checkScheduleForTimeConflicts = exports.checkCoursesForTimeConflicts = undefined;
-
-var _checkCoursesForTimeConflicts2 = require('./lib/checkCoursesForTimeConflicts');
-
-var _checkCoursesForTimeConflicts3 = _interopRequireDefault(_checkCoursesForTimeConflicts2);
-
-var _checkScheduleForTimeConflicts2 = require('./lib/checkScheduleForTimeConflicts');
-
-var _checkScheduleForTimeConflicts3 = _interopRequireDefault(_checkScheduleForTimeConflicts2);
-
-var _convertTimeStringsToOfferings2 = require('./lib/convertTimeStringsToOfferings');
-
-var _convertTimeStringsToOfferings3 = _interopRequireDefault(_convertTimeStringsToOfferings2);
-
-var _findScheduleTimeConflicts2 = require('./lib/findScheduleTimeConflicts');
-
-var _findScheduleTimeConflicts3 = _interopRequireDefault(_findScheduleTimeConflicts2);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-exports.checkCoursesForTimeConflicts = _checkCoursesForTimeConflicts3.default;
-exports.checkScheduleForTimeConflicts = _checkScheduleForTimeConflicts3.default;
-exports.convertTimeStringsToOfferings = _convertTimeStringsToOfferings3.default;
-exports.findScheduleTimeConflicts = _findScheduleTimeConflicts3.default;
-
+exports.checkCoursesForTimeConflicts = require('./lib/checkCoursesForTimeConflicts').default
+exports.checkScheduleForTimeConflicts = require('./lib/checkScheduleForTimeConflicts').default
+exports.convertTimeStringsToOfferings = require('./lib/convertTimeStringsToOfferings').default
+exports.findScheduleTimeConflicts = require('./lib/findScheduleTimeConflicts').default
