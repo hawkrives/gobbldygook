5cb38f54376778dd59de8af9eb8e1edb4524b805
Hawken Rives <hawkrives@gmail.com>
2016-01-23 09:42:33 -0600

    update course actions

--- src/redux/courses/actions/index.js
+++ src/redux/courses/actions/index.js
@@ -1,40 +1,25 @@
 import {
-	RELOAD_CACHED_COURSES,
-	LOAD_COURSES,
-	CACHE_COURSES_FROM_SCHEDULES,
+	REFRESH_COURSES,
+	// LOAD_COURSES,
 } from '../constants'
 
-import flatten from 'lodash/flatten'
-import values from 'lodash/values'
-import map from 'lodash/map'
-import getCourses, {getCoursesFromSchedules} from '../../../helpers/get-courses'
+// import flatten from 'lodash/flatten'
+// import values from 'lodash/values'
+// import map from 'lodash/map'
+// import {getCoursesFromSchedules} from '../../../helpers/get-courses'
 
 
-export function reloadCachedCourses() {
-	return (dispatch, getState) => {
-		let {courses} = getState()
-
-		courses = map(courses, ({clbid, term}) => ({clbid, term}))
-
-		let action = { type: RELOAD_CACHED_COURSES, payload: getCourses(courses) }
-		return dispatch(action)
-	}
+export function refreshCourses() {
+	return { type: REFRESH_COURSES }
 }
 
 export function loadCourses() {
-	return (dispatch, getState) => {
-		let {students, courses} = getState()
-		let schedules = flatten(map(students, student => values(student.data.present.schedules)))
-		console.log('schedules', schedules)
-		let action = { type: LOAD_COURSES, payload: getCoursesFromSchedules(schedules, courses) }
-		return dispatch(action)
-	}
-}
-
-export function cacheCoursesFromSchedules(schedules) {
-	return (dispatch, getState) => {
-		let {courses} = getState()
-		let action = { type: CACHE_COURSES_FROM_SCHEDULES, payload: getCoursesFromSchedules(schedules, courses) }
-		return dispatch(action)
-	}
+	throw new Error('This function does not work.')
+	// return (dispatch, getState) => {
+	// 	let {students, courses} = getState()
+	// 	let schedules = flatten(map(students, student => values(student.data.present.schedules)))
+	// 	console.log('schedules', schedules)
+	// 	let action = { type: LOAD_COURSES, payload: getCoursesFromSchedules(schedules, courses) }
+	// 	return dispatch(action)
+	// }
 }
