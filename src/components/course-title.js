918c4245c42c9733e6217a87b77c56f947eb2417
Hawken Rives <hawkrives@gmail.com>
2016-01-24 10:25:02 -0600

    make <CourseTitle/> a pure function

--- src/components/course-title.js
+++ src/components/course-title.js
@@ -1,45 +1,37 @@
-import React, {Component, PropTypes} from 'react'
-import compareProps from '../helpers/compare-props'
+import React, {PropTypes} from 'react'
 
 const independentRegex = /^I[RS]/
 
-export default class CourseTitle extends Component {
-	static propTypes = {
-		name: PropTypes.string.isRequired,
-		title: PropTypes.string,
-		type: PropTypes.string,
-	};
+export default function CourseTitle({name, title, type}) {
+	const isIndependent = independentRegex.test(name)
+	let courseName = title || name
+	let subtitle = undefined
 
-	shouldComponentUpdate(nextProps) {
-		return compareProps(this.props, nextProps)
+	if (isIndependent) {
+		courseName = name
+		if (courseName.length > 3) {
+			courseName = courseName.substring(3)
+		}
+	}
+	else if (type === 'Topic') {
+		courseName = `${name.replace(/top.*: */gi, '')}`
+		subtitle = title
+	}
+	else if (type === 'Seminar') {
+		courseName = title
+		subtitle = name
 	}
 
-	render() {
-		const {name, title, type} = this.props
-		const isIndependent = independentRegex.test(name)
-		let courseName = title || name
-		let subtitle = undefined
+	return (
+		<header>
+			<h1 className='title'>{courseName}</h1>
+			{subtitle && subtitle.length && <h2 className='subtitle'>{subtitle}</h2>}
+		</header>
+	)
+}
 
-		if (isIndependent) {
-			courseName = name
-			if (courseName.length > 3) {
-				courseName = courseName.substring(3)
-			}
-		}
-		else if (type === 'Topic') {
-			courseName = `${name.replace(/top.*: */gi, '')}`
-			subtitle = title
-		}
-		else if (type === 'Seminar') {
-			courseName = title
-			subtitle = name
-		}
-
-		return (
-			<header>
-				<h1 className='title'>{courseName}</h1>
-				{subtitle && subtitle.length && <h2 className='subtitle'>{subtitle}</h2>}
-			</header>
-		)
-	}
+CourseTitle.propTypes = {
+	name: PropTypes.string.isRequired,
+	title: PropTypes.string,
+	type: PropTypes.string,
 }
