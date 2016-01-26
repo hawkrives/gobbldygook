9eda001f62b708a20d3def6dc721f501a5f0e333
Hawken Rives <hawkrives@gmail.com>
2016-01-26 01:11:23 -0600

    add some nifty buttons

--- src/routes/app_content_new-student/routes/sis/sis-import.js
+++ src/routes/app_content_new-student/routes/sis/sis-import.js
@@ -1,11 +1,17 @@
 import React, {Component, PropTypes} from 'react'
 import serializeError from 'serialize-error'
-import getStudentInfo from '../../../../helpers/import-student'
+import Button from '../../../../components/button'
+import getStudentInfo, {checkIfLoggedIn} from '../../../../helpers/import-student'
 import convertStudent from '../../../../helpers/convert-imported-student'
 import DOMify from 'react-domify'
+import { push } from 'react-router-redux'
+import { initStudent } from '../../../../redux/students/actions/init-student'
+import {connect} from 'react-redux'
 
 export default class SISImportScreen extends Component {
-	static propTypes = {};
+	static propTypes = {
+		dispatch: PropTypes.func.isRequired, // redux
+	};
 
 	state = {
 		loggedIn: null,
@@ -15,17 +21,31 @@ export default class SISImportScreen extends Component {
 	};
 
 	componentWillMount() {
-		getStudentInfo()
-			.then(data => {
-				console.log(data)
-				console.log(convertStudent(data))
-				this.setState({loggedIn: true, checkingLogin: false, student: convertStudent(data)})
-			})
+		checkIfLoggedIn()
+			.then(() => this.setState({loggedIn: true, checkingLogin: false}))
 			.catch(err => {
 				console.error(err)
 				this.setState({loggedIn: false, checkingLogin: false, error: serializeError(err)})
 			})
 	}
+
+	handleImportData = () => {
+		getStudentInfo()
+			.then(data => {
+				console.log(data)
+				this.setState({student: convertStudent(data)})
+			})
+			.catch(err => {
+				console.error(err)
+				this.setState({error: serializeError(err)})
+			})
+	};
+
+	handleCreateStudent = () => {
+		let action = initStudent(this.state.student)
+		this.props.dispatch(action)
+		this.props.dispatch(push(`/s/${action.payload.id}`))
+	};
 
 	render() {
 		return <div>
@@ -43,7 +63,24 @@ export default class SISImportScreen extends Component {
 
 			{this.state.error ? <pre>{JSON.stringify(this.state.error)}</pre> : null}
 
-			<DOMify value={this.state.student} />
+			{this.state.student ? <DOMify value={this.state.student} /> : null}
+
+			<div>
+				<Button
+					disabled={!this.state.loggedIn}
+					onClick={this.handleImportData}
+				>
+					Import Student
+				</Button>
+				{this.state.student
+					? <Button onClick={this.handleCreateStudent}>Let's go!</Button>
+					: null}
+			</div>
 		</div>
 	}
 }
+
+
+let mapDispatch = dispatch => ({dispatch})
+
+export default connect(undefined, mapDispatch)(SISImportScreen)
