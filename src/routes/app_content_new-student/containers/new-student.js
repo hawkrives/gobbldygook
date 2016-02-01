e36e96c9824fd2d90885ef6038539edf89c4946c
Hawken Rives <hawkrives@gmail.com>
2016-02-01 13:02:37 -0600

    Fix up the Upload Student screen
    - Moved the Dropzone into the UploadFile screen
    - Deprecated student.actions.importStudent, esentially
    - Allow importing more than one file at once

--- src/routes/app_content_new-student/containers/new-student.js
+++ src/routes/app_content_new-student/containers/new-student.js
@@ -1,46 +1,11 @@
-import React, {Component, PropTypes, cloneElement} from 'react'
-import DropZone from 'react-dropzone'
+import React, {PropTypes} from 'react'
 
 import './new-student.scss'
 
+export default function NewStudent(props) {
+	return <div className='new-student'>{props.children}</div>
+}
 
-export default class NewStudent extends Component {
-	static propTypes = {
-		children: PropTypes.node.isRequired,
-	};
-
-	state = {
-		files: [],
-	};
-
-	componentDidMount() {
-		if (typeof window === 'undefined') {
-			return
-		}
-
-		window.addEventListener('dragover', e => e.preventDefault())
-		window.addEventListener('drop', e => e.preventDefault())
-	}
-
-	handleFileDrop = files => {
-		this.setState({files})
-	};
-
-	render() {
-		return (
-			<DropZone
-				ref={c => (this._dropzone = c)}
-				className='dropzone'
-				activeClassName='dropzone-active'
-				accept='application/json'
-				onDrop={this.handleFileDrop}
-				multiple
-				disableClick
-			>
-			<div className='new-student'>
-				{cloneElement(this.props.children, {files: this.state.files})}
-			</div>
-			</DropZone>
-		)
-	}
+NewStudent.propTypes = {
+	children: PropTypes.node.isRequired,
 }
--- src/routes/app_content_new-student/routes/upload/upload-file.js
+++ src/routes/app_content_new-student/routes/upload/upload-file.js
@@ -1,31 +1,118 @@
 import React, {Component, PropTypes} from 'react'
-import ScreenToolbar from '../../components/screen-toolbar'
+import {findDOMNode} from 'react-dom'
+import DropZone from 'react-dropzone'
 import map from 'lodash/map'
 import Button from '../../../../components/button'
+import List from '../../../../components/list'
+import StudentSummary from '../../../app_content_student/components/student-summary'
+import { initStudent } from '../../../../redux/students/actions/init-student'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+
+import styles from './upload-file.scss'
 
 export default class UploadFileScreen extends Component {
 	static propTypes = {
-		files: PropTypes.array,
+		dispatch: PropTypes.func.isRequired,
+	};
+
+	state = {
+		files: [],
+		students: [],
+	};
+
+	handleFileDrop = files => {
+		console.log(files)
+		files = files.map(f => ({
+			name: f.name,
+			size: f.size,
+			data: new Promise((resolve, reject) => {
+				let reader = new FileReader()
+				reader.onload = ev => resolve(ev.target.result)
+				reader.onerror = reader.onabort = reject
+				reader.readAsText(f)
+			}),
+		}))
+		this.setState({files})
+		this.convertFilesToStudents(files)
+	};
+
+	handleOpenPicker = () => {
+		this.dropzone.open()
+	};
+
+	convertOneFile = file => {
+		file.data.then(data => {
+			let parsed
+			try {
+				parsed = JSON.parse(data)
+			}
+			catch (err) {
+				return {name: file.name, error: `could not parse "${data}" because "${err.message}"`}
+			}
+
+			let converted
+			try {
+				converted = initStudent(parsed)
+			}
+			catch (err) {
+				return {name: file.name, error: err.message}
+			}
+
+			return converted
+		}).then(student => {
+			this.setState({students: this.state.students.concat(student)})
+		})
+	};
+
+	convertFilesToStudents = files => {
+		this.setState({students: []})
+		files.forEach(this.convertOneFile)
+	};
+
+	handleImportStudents = () => {
+		this.state.students.forEach(this.props.dispatch)
+		this.props.dispatch(push('/'))
 	};
 
 	render() {
-		let {files} = this.props
-		return <div>
-			<header className='header'>
-				<h1>Upload a File</h1>
-			</header>
+		let {students} = this.state
+		let files = this.state.files.slice(students.length)
 
-			<p>
-				Either go ahead and drop a file here, or
-				<Button type='raised'>pick one</Button>
-				from your computer.
-			</p>
+		return (
+			<div>
+				<header className='header'>
+					<h1>Upload a File</h1>
+				</header>
 
-			<ul>
-				{map(files, file => <li>{JSON.stringify(file)}</li>)}
-			</ul>
+				<DropZone
+					ref={el => (this.dropzone = el)}
+					accept='.gbstudent,.json,.gb-student'
+					onDrop={this.handleFileDrop}
+					multiple
+					disablePreview
+					className={styles.dropzone}
+					activeClassName={styles.canDrop}
+					rejectClassName={styles.canDrop} // HTML doesn't give us filenames until we drop, so it can't tell if it'll be accepted until the drop happens
+				>
+					<p>
+						Just drop some students here, or click to select some to upload.
+					</p>
+				</DropZone>
 
-			<ScreenToolbar />
-		</div>
+				<List type='plain' className={styles.results}>
+					{map(students, stu => stu.payload
+						? <li key={stu.payload.id}><StudentSummary student={stu.payload} showMessage={false} showAvatar={false} randomizeHello /></li>
+						: <li key={stu.name}>{stu.name} returned the error "{stu.error}"</li>)}
+					{map(files, file => <li key={file.name}>{file.name}</li>)}
+				</List>
+
+				<Button onClick={this.handleImportStudents}>Import Students</Button>
+			</div>
+		)
 	}
 }
+
+let mapDispatch = dispatch => ({dispatch})
+
+export default connect(undefined, mapDispatch)(UploadFileScreen)
