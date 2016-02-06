aff89faf3a1d7ca151d8e59fea598ad2659a9387
Hawken Rives <hawkrives@gmail.com>
2016-02-06 15:49:35 -0600

    tweak alterCourse to only include some properties when passing them in

--- src/helpers/alter-course-for-evaluation.js
+++ src/helpers/alter-course-for-evaluation.js
@@ -1,13 +1,47 @@
-import mapKeys from 'lodash/mapKeys'
+// import mapKeys from 'lodash/mapKeys'
+import forEach from 'lodash/forEach'
+import toPairs from 'lodash/toPairs'
+import fromPairs from 'lodash/fromPairs'
+import filter from 'lodash/filter'
+import includes from 'lodash/includes'
 
+const whitelist = [
+	'clbid',
+	'credits',
+	'crsid',
+	'department',
+	'gereqs',
+	'groupid',
+	'level',
+	'name',
+	'number',
+	'pf',
+	'semester',
+	'type',
+	'year',
+]
+const mapping = {
+	depts: 'department',
+	num: 'number',
+}
 export default function alterCourse(course) {
-	return mapKeys(course, (value, key) => {
-		if (key === 'depts') {
-			key = 'department'
-		}
-		else if (key === 'num') {
-			key = 'number'
-		}
-		return key
+	course = {...course}
+
+	forEach(mapping, (toKey, fromKey) => {
+		course[toKey] = course[fromKey]
 	})
+
+	let pairs = toPairs(course)
+	pairs = filter(pairs, ([key]) => includes(whitelist, key))
+	return fromPairs(pairs)
+
+	// return mapKeys(course, (value, key) => {
+	// 	if (key === 'depts') {
+	// 		key = 'department'
+	// 	}
+	// 	else if (key === 'num') {
+	// 		key = 'number'
+	// 	}
+	// 	return key
+	// })
 }
