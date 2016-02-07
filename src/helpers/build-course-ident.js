1d3a26b6c8ef4940f3f1c9b3ae1fc658a7d0ff57
Hawken Rives <hawkrives@gmail.com>
2016-02-07 10:03:17 -0600

    update buildCourseIdent
    now making departments smaller if there are multiple departments

--- src/helpers/build-course-ident.js
+++ src/helpers/build-course-ident.js
@@ -1,4 +1,13 @@
-export default function courseIdent({dept, num, section='', deptnum}) {
-	deptnum = deptnum || `${dept} ${num}`
-	return `${deptnum}` + (section ? section : '')
+import {shrinkDepartment} from '../area-tools/convert-department'
+
+// Builds a course identifier ("CSCI 121", "AS/RE 230B") from a course.
+export default function courseIdent({depts, num, section='', deptnum}) {
+	if (depts) {
+		depts = depts.length > 1
+			? depts.map(shrinkDepartment).join('/')
+			: depts[0]
+	}
+
+	deptnum = deptnum || `${depts} ${num}`
+	return `${deptnum}${section}`
 }
