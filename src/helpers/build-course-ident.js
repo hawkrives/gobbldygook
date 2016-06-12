5cb0babceea9fdd7b42830f875a765b8511de0b3
Hawken Rives <hawkrives@gmail.com>
2016-06-12 12:55:20 -0500

    reduce variable reuse in buildCourseIdent

--- src/helpers/build-course-ident.js
+++ src/helpers/build-course-ident.js
@@ -1,13 +1,14 @@
 import {shrinkDepartment} from '../area-tools/convert-department'
 
 // Builds a course identifier ("CSCI 121", "AS/RE 230B") from a course.
-export default function courseIdent({depts, num, section='', deptnum}) {
+export default function courseIdent({depts, num, sect='', deptnum}) {
+	let deptString = 'UNKN'
 	if (depts) {
-		depts = depts.length > 1
+		deptString = depts.length > 1
 			? depts.map(shrinkDepartment).join('/')
 			: depts[0]
 	}
 
-	deptnum = deptnum || `${depts} ${num}`
-	return `${deptnum}${section}`
+	deptnum = deptnum || `${deptString} ${num}`
+	return `${deptnum}${sect}`
 }
