21ffd931a92a259bdf1cb6c94d8ccb5f3f07ecdd
Hawken Rives <hawkrives@gmail.com>
2016-06-12 12:54:17 -0500

    remove use of Map in semesterName

--- src/helpers/semester-name.js
+++ src/helpers/semester-name.js
@@ -1,12 +1,12 @@
-const semesters = new Map([
-	[0, 'Abroad'],
-	[1, 'Fall'],
-	[2, 'Interim'],
-	[3, 'Spring'],
-	[4, 'Summer Session 1'],
-	[5, 'Summer Session 2'],
-	[9, 'Non-St. Olaf'],
-])
+const SEMESTERS = {
+	'0': 'Abroad',
+	'1': 'Fall',
+	'2': 'Interim',
+	'3': 'Spring',
+	'4': 'Summer Session 1',
+	'5': 'Summer Session 2',
+	'9': 'Non-St. Olaf',
+}
 
 /**
  * Takes a semester number and returns the associated semester string.
@@ -15,10 +15,10 @@ const semesters = new Map([
  * @returns {String} - the nice semester name
  */
 export default function semesterName(semester) {
-	if (typeof semester === 'string') {
-		semester = parseInt(semester, 10)
+	if (typeof semester === 'number') {
+		semester = String(semester)
 	}
-	return semesters.has(semester)
-        ? semesters.get(semester)
-        : 'Unknown (' + semester + ')'
+	return SEMESTERS.hasOwnProperty(semester)
+        ? SEMESTERS[semester]
+        : `Unknown (${semester})`
 }
