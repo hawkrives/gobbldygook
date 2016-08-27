0e47d24e35269670f7548ba7b56744bc239176e0
Hawken Rives <hawkrives@gmail.com>
2016-08-27 22:53:54 +0900

    try asking react-router to work off of '/' always

--- src/history.js
+++ src/history.js
@@ -3,6 +3,6 @@ import useRouterHistory from 'react-router/lib/useRouterHistory'
 import useBasename from 'history/lib/useBasename'
 import useQueries from 'history/lib/useQueries'
 
-const history = useRouterHistory(useBasename(useQueries(createHistory)))({basename: APP_BASE})
+const history = useRouterHistory(useBasename(useQueries(createHistory)))({basename: '/'})
 
 export default history
