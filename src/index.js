d199681000fa3c6b8209cea7bcabcc2a903dcd64
Hawken Rives <hawkrives@gmail.com>
2016-08-27 10:55:42 +0900

    go ahead and finish removing react-hot-loader

--- src/index.js
+++ src/index.js
@@ -51,7 +51,6 @@ loadData()
 // Kick off the GUI
 console.log('3. 2.. 1... Blast off! 🚀')
 
-// import {AppContainer} from 'react-hot-loader'
 import {Router} from 'react-router'
 import history from './history'
 import routes from './routes'
@@ -69,20 +68,10 @@ store.dispatch(loadAllAreas())
 
 let renderFunc = Root => {
 	render(
-		//<AppContainer>
-			<Root store={store}>
-				<Router history={history} routes={routes} />
-			</Root>,
-		// </AppContainer>,
+		<Root store={store}>
+			<Router history={history} routes={routes} />
+		</Root>,
 		document.getElementById('gobbldygook'))
 }
 
 renderFunc(Root)
-
-
-// if (module.hot) {
-// 	module.hot.accept('./containers/root', () => {
-// 		let Root = require('./containers/root').default
-// 		renderFunc(Root)
-// 	})
-// }
