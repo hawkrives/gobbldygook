36c85caf07c3992d805fa01e903d3413453a6794
Hawken Rives <hawkrives@gmail.com>
2016-08-27 21:13:42 +0900

    enable base support

--- src/history.js
+++ src/history.js
@@ -1,2 +1,8 @@
-import browserHistory from 'react-router/lib/browserHistory'
-export default browserHistory
+import createHistory from 'history/lib/createBrowserHistory'
+import useRouterHistory from 'react-router/lib/useRouterHistory'
+import useBasename from 'history/lib/useBasename'
+import useQueries from 'history/lib/useQueries'
+
+const history = useRouterHistory(useBasename(useQueries(createHistory)))({basename: APP_BASE})
+
+export default history
