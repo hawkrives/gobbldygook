9e96148cf113ca9eb0193b7b73b13c5c151bfb23
Hawken Rives <hawkrives@gmail.com>
2016-08-28 16:25:04 +0900

    it appears that useRouterHistory already adds useQueries and useBasename

--- src/history.js
+++ src/history.js
@@ -1,8 +1,6 @@
 import createHistory from 'history/lib/createBrowserHistory'
 import useRouterHistory from 'react-router/lib/useRouterHistory'
-import useBasename from 'history/lib/useBasename'
-import useQueries from 'history/lib/useQueries'
 
-const history = useRouterHistory(useBasename(useQueries(createHistory)))({basename: APP_BASE})
+const history = useRouterHistory(createHistory)({basename: APP_BASE})
 
 export default history
