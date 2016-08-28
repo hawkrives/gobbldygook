2e829e0c852860b18e58fc2703d5ac990424b88d
Hawken Rives <hawkrives@gmail.com>
2016-08-28 15:53:13 +0900

    embed notfoundroute into main bundle

--- src/routes/not_found/index.js
+++ src/routes/not_found/index.js
@@ -1,11 +1,7 @@
 export default {
 	path: '*',
 
-	getComponents(location, cb) {
-		require.ensure([], require => {
-			cb(null, {
-				content: require('./not-found').default,
-			})
-		}, 'not-found.components')
+	components: {
+		content: require('./not-found').default,
 	},
 }
