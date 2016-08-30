9ffea0c63af95784afc0f6d8eea109ba63006843
Hawken Rives <hawkrives@gmail.com>
2016-08-30 12:04:38 +0900

    rename gobbldygook-schedule-builder to gb-schedule-builder

--- packages/gb-schedule-builder/combo-has-courses.js
+++ packages/gb-schedule-builder/combo-has-courses.js
@@ -1,5 +1,5 @@
 import takeWhile from 'lodash/takeWhile'
-import queryCourses from '../helpers/query-courses'
+import queryCourses from 'gb-search-queries/query-courses'
 
 export default function comboHasAllCourses(courses, combinationOfClasses) {
 	const these = takeWhile(courses, course =>
