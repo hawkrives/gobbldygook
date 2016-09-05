8920735eca879caac8e7a3288d812f06423d5c26
Hawken Rives <hawkrives@gmail.com>
2016-09-05 13:38:31 +0900

    port expandYear/friends tests to ava

--- packages/gb-school-stolaf/stolaf-courses/__tests__/expand-year.test.js
+++ packages/gb-school-stolaf/stolaf-courses/__tests__/expand-year.test.js
@@ -1,31 +1,29 @@
-import {expect} from 'chai'
-import expandYear from '../../src/helpers/expand-year'
+import test from 'ava'
+import expandYear from '../expand-year'
 
-describe('expandYear', () => {
-	it('expands a year to year-(year+1)', () => {
-		expect(expandYear(2014)).to.equal('2014—2015')
-		expect(expandYear(2010)).to.equal('2010—2011')
-		expect(expandYear(2013)).to.equal('2013—2014')
-		expect(expandYear(2011)).to.equal('2011—2012')
-		expect(expandYear(2031)).to.equal('2031—2032')
-		expect(expandYear(2013)).to.equal('2013—2014')
-	})
+test('expands a year to year-(year+1)', t => {
+	t.is(expandYear(2014), '2014—2015')
+	t.is(expandYear(2010), '2010—2011')
+	t.is(expandYear(2013), '2013—2014')
+	t.is(expandYear(2011), '2011—2012')
+	t.is(expandYear(2031), '2031—2032')
+	t.is(expandYear(2013), '2013—2014')
+})
 
-	it('can also do short expansions', () => {
-		expect(expandYear(2014, true)).to.equal('2014—15')
-		expect(expandYear(2010, true)).to.equal('2010—11')
-		expect(expandYear(2013, true)).to.equal('2013—14')
-		expect(expandYear(2011, true)).to.equal('2011—12')
-		expect(expandYear(2031, true)).to.equal('2031—32')
-		expect(expandYear(2013, true)).to.equal('2013—14')
-	})
+test('can also do short expansions', t => {
+	t.is(expandYear(2014, true), '2014—15')
+	t.is(expandYear(2010, true), '2010—11')
+	t.is(expandYear(2013, true), '2013—14')
+	t.is(expandYear(2011, true), '2011—12')
+	t.is(expandYear(2031, true), '2031—32')
+	t.is(expandYear(2013, true), '2013—14')
+})
 
-	it('can switch out the seperator char', () => {
-		expect(expandYear(2014, false, '–')).to.equal('2014–2015')
-		expect(expandYear(2010, true, '·')).to.equal('2010·11')
-		expect(expandYear(2013, false, '-')).to.equal('2013-2014')
-		expect(expandYear(2011, true, '/')).to.equal('2011/12')
-		expect(expandYear(2031, false, '+')).to.equal('2031+2032')
-		expect(expandYear(2013, true, '¿')).to.equal('2013¿14')
-	})
+test('can switch out the seperator char', t => {
+	t.is(expandYear(2014, false, '–'), '2014–2015')
+	t.is(expandYear(2010, true, '·'), '2010·11')
+	t.is(expandYear(2013, false, '-'), '2013-2014')
+	t.is(expandYear(2011, true, '/'), '2011/12')
+	t.is(expandYear(2031, false, '+'), '2031+2032')
+	t.is(expandYear(2013, true, '¿'), '2013¿14')
 })
--- packages/gb-school-stolaf/stolaf-courses/__tests__/semester-name.test.js
+++ packages/gb-school-stolaf/stolaf-courses/__tests__/semester-name.test.js
@@ -1,15 +1,13 @@
-import {expect} from 'chai'
-import semesterName from '../../src/helpers/semester-name'
+import test from 'ava'
+import semesterName from '../semester-name'
 
-describe('semesterName', () => {
-	it('converts a semester number to a semester name', () => {
-		expect(semesterName(0)).to.equal('Abroad')
-		expect(semesterName(1)).to.equal('Fall')
-		expect(semesterName(2)).to.equal('Interim')
-		expect(semesterName(3)).to.equal('Spring')
-		expect(semesterName(4)).to.equal('Summer Session 1')
-		expect(semesterName(5)).to.equal('Summer Session 2')
-		expect(semesterName(6)).to.equal('Unknown (6)')
-		expect(semesterName(9)).to.equal('Non-St. Olaf')
-	})
+test('converts a semester number to a semester name', t => {
+	t.is(semesterName(0), 'Abroad')
+	t.is(semesterName(1), 'Fall')
+	t.is(semesterName(2), 'Interim')
+	t.is(semesterName(3), 'Spring')
+	t.is(semesterName(4), 'Summer Session 1')
+	t.is(semesterName(5), 'Summer Session 2')
+	t.is(semesterName(6), 'Unknown (6)')
+	t.is(semesterName(9), 'Non-St. Olaf')
 })
