0dd455317e30603287d741e225266b5c3e1cc5a1
Hawken Rives <hawkrives@gmail.com>
2016-09-05 17:11:19 +0900

    export the other things that Gobbldygook uses from hanson-format

--- packages/hanson-format/index.js
+++ packages/hanson-format/index.js
@@ -1,2 +1,13 @@
 export {default as enhance} from './enhance'
 export {default as evaluate} from './evaluate'
+
+export {compute} from './enhance/compute'
+export {parse as parseHansonString} from './enhance/parse-hanson-string'
+
+export {countCredits} from './evaluate/count-credits'
+export {isRequirementName} from './evaluate/is-requirement-name'
+export {pathToOverride} from './evaluate/path-to-override'
+export {simplifyCourse} from './evaluate/simplify-course'
+
+export {humanizeOperator} from './unused/humanize-operator'
+export {pluralizeArea} from './unused/pluralize-area'
