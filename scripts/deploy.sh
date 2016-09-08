c3e2d779a9950f75fea2412f765a6f39ebe8a45b
Hawken Rives <hawkrives@gmail.com>
2016-09-08 17:44:41 +0900

    notify bugsnag about new revisions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,5 +28,6 @@ rmdir build/
 git add --all ./
 git commit -m "build app" --quiet
 git push -f "https://github.com/hawkrives/gobbldygook.git" $DEST_BRANCH
+curl -d "apiKey=7e393deddaeb885f5b140b4320ecef6b" -d "repository=https://github.com/hawkrives/gobbldygook" -d "revision=$(git rev-parse --verify HEAD)" 'https://notify.bugsnag.com/deploy'
 
 git checkout $SOURCE_BRANCH
