432c2cbf1db9595c533ea940c1b9d7db1f79dd37
Hawken Rives <hawkrives@gmail.com>
2016-10-02 01:07:22 +0900

    fix up the login check, I hope

--- modules/schools/stolaf/sis-parser/import-student/logged-in.js
+++ modules/schools/stolaf/sis-parser/import-student/logged-in.js
@@ -1,4 +1,3 @@
-import Bluebird from 'bluebird'
 import {AuthError} from 'modules/lib'
 import {extractStudentIds} from './student-ids'
 import {COURSES_URL} from './urls'
@@ -6,18 +5,15 @@ import {fetchHtml, getText} from './lib'
 import {selectOne} from 'css-select'
 
 export function checkIfLoggedIn() {
-	// if (typeof window !== 'undefined') {
-	// 	return Bluebird.reject(new AuthError('Wrong domain. Student import will only work under the www.stolaf.edu domain.'))
-	// }
 	return fetchHtml(COURSES_URL).then(response => {
-		// let errorMsg = selectOne('.sis-error', response)
-		// let badMsg = 'Sorry, your session has timed out; please login again.'
-		// if (errorMsg && getText(errorMsg) === badMsg) {
-		// 	throw new AuthError('Not logged in. Please log into the SIS in another tab, then try again.')
-		// }
-		// else if (errorMsg) {
-		// 	throw new Error(errorMsg)
-		// }
+		let errorMsg = selectOne('[style="text-align:center"]', response)
+		let badMsg = 'Please use your St. Olaf Google account when accessing SIS.'
+		if (errorMsg && getText(errorMsg) === badMsg) {
+			throw new AuthError('Not logged in. Please log into the SIS in another tab, then try again.')
+		}
+		else if (errorMsg) {
+			throw new Error(errorMsg)
+		}
 		return extractStudentIds(response)
 	})
 }
