466a366a98c4e2e89e1c3beac28de8f48cec46d6
Hawken Rives <hawkrives@gmail.com>
2016-10-02 00:42:52 +0900

    export the rest of the used things from import-student

--- modules/schools/stolaf/sis-parser/index.js
+++ modules/schools/stolaf/sis-parser/index.js
@@ -1,4 +1,9 @@
 // can't use export * with babel: see https://github.com/babel/babel/issues/4446
 export {convertStudent} from './convert-imported-student'
 
-export {getStudentInfo} from './import-student'
+export {
+	getStudentInfo,
+	checkIfLoggedIn,
+	ExtensionNotLoadedError,
+	ExtensionTooOldError,
+} from './import-student'
