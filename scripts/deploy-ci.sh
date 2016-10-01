cdf4714fa7fa939fbaa44f61f8ebd825c7dad68a
Hawken Rives <hawkrives@gmail.com>
2016-10-02 03:33:04 +0900

    try travis deployments again

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -11,6 +11,16 @@ mv build/* ./
 rm -f build/.DS_Store
 rmdir build/
 
+# Get the deploy key by using Travis's stored variables to decrypt config/deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K "$ENCRYPTED_KEY" -iv "$ENCRYPTED_IV" -in config/deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval "${ssh-agent -s}"
+ssh-add deploy_key
+
 # and … push
 git add --all ./
 git commit -m "build app" --quiet
