8a482ae0f7c4d66936d706392e3f611f74cbb272
Hawken Rives <hawkrives@gmail.com>
2016-10-04 19:48:15 +0900

    split expandYear into two functions

--- modules/schools/stolaf/course-info/expand-year.js
+++ modules/schools/stolaf/course-info/expand-year.js
@@ -1,13 +1,26 @@
-/* Takes a year and expands it.
- * eg. expendYear('2012') => 2012-13
- */
-export function expandYear(year, short=false, seperator='—') {
+// @flow
+export function expandYear(year: number, short=false, separator='—') {
+	if (short) {
+		return expandYearToShort(year, separator)
+	}
+	return expandYearToFull(year, separator)
+}
+
+// 2012 => 2012-2013
+export function expandYearToFull(year: number, separator='—') {
 	if (year === undefined) {
 		return '???'
 	}
 	let nextYear = parseInt(year, 10) + 1
-	if (short) {
-		nextYear = String(nextYear).substr(-2, 2)
+	return `${year}${separator}${nextYear}`
+}
+
+// 2012 => 2012-13
+export function expandYearToShort(year: number, separator='—') {
+	if (year === undefined) {
+		return '???'
 	}
-	return `${year}${seperator}${nextYear}`
+	let nextYear = parseInt(year, 10) + 1
+	nextYear = String(nextYear).substr(-2, 2)
+	return `${year}${separator}${nextYear}`
 }
