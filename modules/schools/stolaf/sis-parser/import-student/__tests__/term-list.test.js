3504d9249a79a03a10d103b4c7cfaca59808fdfe
Hawken Rives <hawkrives@gmail.com>
2016-10-08 18:08:34 +0900

    test: add tests to term-list

--- modules/schools/stolaf/sis-parser/import-student/__tests__/term-list.test.js
+++ modules/schools/stolaf/sis-parser/import-student/__tests__/term-list.test.js
@@ -1,5 +1,6 @@
 import {expect} from 'chai'
 import {loadHtml} from './support'
+import {parseHtml} from '../../parse-html'
 import {extractTermList} from '../term-list'
 
 describe('extractTermList', () => {
@@ -7,6 +8,18 @@ describe('extractTermList', () => {
 		const html = loadHtml('term-20151')
 		const actual = extractTermList(html)
 		const expected = [20153, 20152, 20151, 20143, 20142, 20141, 20133, 20132, 20131, 20123, 20122, 20121, 20119]
+		expect(actual).to.deep.equal(expected)
+	})
+
+	it('returns an empty list when no term list selectors are found', () => {
+		const actual = extractTermList(parseHtml(''))
+		const expected = []
+		expect(actual).to.deep.equal(expected)
+	})
+
+	it('returns an empty list when no term options are found', () => {
+		const actual = extractTermList(parseHtml('<select name=searchyearterm></select>'))
+		const expected = []
 		expect(actual).to.deep.equal(expected)
 	})
 })
