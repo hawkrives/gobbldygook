6e582605e8321241e00496e59c37de805e6c757f
Hawken Rives <hawkrives@gmail.com>
2016-10-08 23:27:32 +0900

    test: add more tests for expandYear

--- modules/schools/stolaf/course-info/__tests__/expand-year.test.js
+++ modules/schools/stolaf/course-info/__tests__/expand-year.test.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai'
-import {expandYear} from '../expand-year'
+import {expandYear, expandYearToFull, expandYearToShort} from '../expand-year'
 
 describe('expandYear', () => {
 	it('expands a year to year-(year+1)', () => {
@@ -9,6 +9,11 @@ describe('expandYear', () => {
 		expect(expandYear(2011)).to.equal('2011—2012')
 		expect(expandYear(2031)).to.equal('2031—2032')
 		expect(expandYear(2013)).to.equal('2013—2014')
+	})
+
+	it('returns ??? if no year is given', () => {
+		expect(expandYear()).to.equal('???')
+		expect(expandYear(undefined, true)).to.equal('???')
 	})
 
 	it('can also do short expansions', () => {
@@ -27,5 +32,10 @@ describe('expandYear', () => {
 		expect(expandYear(2011, true, '/')).to.equal('2011/12')
 		expect(expandYear(2031, false, '+')).to.equal('2031+2032')
 		expect(expandYear(2013, true, '¿')).to.equal('2013¿14')
+	})
+
+	it('calls other functions', () => {
+		expect(expandYearToShort(2031)).to.equal('2031—32')
+		expect(expandYearToFull(2031)).to.equal('2031—2032')
 	})
 })
