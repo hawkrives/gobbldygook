bb6a12121364a186a82622d64b796f7685a20bc3
Hawken Rives <hawkrives@gmail.com>
2016-10-08 12:59:24 +0900

    test: add test for stringify-error

---
+++ modules/lib/__tests__/stringify-error.test.js
@@ -0,0 +1,11 @@
+import {expect} from 'chai'
+import {stringifyError} from '../stringify-error'
+
+describe('stringifyError', () => {
+	it('should turn an error into a stringifiable object', () => {
+		let err = new Error('test message')
+		expect(() => JSON.stringify(err)).to.throw
+		expect(() => stringifyError(err)).not.to.throw
+		expect(JSON.parse(stringifyError(err)).message).to.equal('test message')
+	})
+})
