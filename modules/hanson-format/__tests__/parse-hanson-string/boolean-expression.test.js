02cd60d6b963c5859b24acd635544a5e1e0f2dde
Hawken Rives <hawkrives@gmail.com>
2017-01-29 13:50:30 -0600

    switch boolean-expr tests to snapshot tests

--- modules/hanson-format/__tests__/parse-hanson-string/boolean-expression.test.js
+++ modules/hanson-format/__tests__/parse-hanson-string/boolean-expression.test.js
@@ -1,140 +1,44 @@
-import { customParser, course } from './parse-hanson-string.support'
+import { customParser } from './parse-hanson-string.support'
 const parse = customParser({ allowedStartRules: [ 'Or' ] })
 
 describe('BooleanExpression', () => {
 	it('parses courses separated by | as being or-d', () => {
-		expect(parse('CSCI 121 | CSCI 125')).toEqual({
-			$type: 'boolean',
-			$booleanType: 'or',
-			$or: [
-				course('CSCI 121'),
-				course('CSCI 125'),
-			],
-		})
+		expect(parse('CSCI 121 | CSCI 125')).toMatchSnapshot()
 	})
 
 	it('parses courses separated by & as being and-d', () => {
-		expect(parse('CSCI 121 & CSCI 125')).toEqual({
-			$type: 'boolean',
-			$booleanType: 'and',
-			$and: [
-				course('CSCI 121'),
-				course('CSCI 125'),
-			],
-		})
+		expect(parse('CSCI 121 & CSCI 125')).toMatchSnapshot()
 	})
 
 	it('parses courses with no departments after an prior department', () => {
-		expect(parse('CSCI 121 | 125')).toEqual({
-			$type: 'boolean',
-			$booleanType: 'or',
-			$or: [
-				course('CSCI 121'),
-				course('CSCI 125'),
-			],
-		})
+		expect(parse('CSCI 121 | 125')).toMatchSnapshot()
 	})
 
 	it('changes departments when given a new one', () => {
-		expect(parse('CSCI 121 | PSCI 125')).toEqual({
-			$type: 'boolean',
-			$booleanType: 'or',
-			$or: [
-				course('CSCI 121'),
-				course('PSCI 125'),
-			],
-		})
+		expect(parse('CSCI 121 | PSCI 125')).toMatchSnapshot()
 	})
 
 	it('allows several &-d courses in a row', () => {
-		expect(parse('CSCI 121 & 125 & 126 & 123')).toEqual({
-			$type: 'boolean',
-			$booleanType: 'and',
-			$and: [
-				course('CSCI 121'),
-				course('CSCI 125'),
-				course('CSCI 126'),
-				course('CSCI 123'),
-			],
-		})
+		expect(parse('CSCI 121 & 125 & 126 & 123')).toMatchSnapshot()
 	})
 
 	it('allows several |-d courses in a row', () => {
-		expect(parse('CSCI 121 | 125 | 126 | 123')).toEqual({
-			$type: 'boolean',
-			$booleanType: 'or',
-			$or: [
-				course('CSCI 121'),
-				course('CSCI 125'),
-				course('CSCI 126'),
-				course('CSCI 123'),
-			],
-		})
+		expect(parse('CSCI 121 | 125 | 126 | 123')).toMatchSnapshot()
 	})
 
 	it('keeps duplicates in a list of courses', () => {
-		expect(parse('CSCI 121 | 121 | 125')).toEqual({
-			$type: 'boolean',
-			$booleanType: 'or',
-			$or: [
-				course('CSCI 121'),
-				course('CSCI 121'),
-				course('CSCI 125'),
-			],
-		})
+		expect(parse('CSCI 121 | 121 | 125')).toMatchSnapshot()
 	})
 
 	it('allows a & b | c – boolean logic for courses', () => {
-		expect(parse('CSCI 121 & 122 | 123')).toEqual({
-			$type: 'boolean',
-			$booleanType: 'or',
-			$or: [
-				{
-					$type: 'boolean',
-					$booleanType: 'and',
-					$and: [
-						course('CSCI 121'),
-						course('CSCI 122'),
-					],
-				},
-				course('CSCI 123'),
-			],
-		})
+		expect(parse('CSCI 121 & 122 | 123')).toMatchSnapshot()
 	})
 
 	it('allows a | b & c – boolean logic for courses', () => {
-		expect(parse('CSCI 121 | 122 & 123')).toEqual({
-			$type: 'boolean',
-			$booleanType: 'or',
-			$or: [
-				course('CSCI 121'),
-				{
-					$type: 'boolean',
-					$booleanType: 'and',
-					$and: [
-						course('CSCI 122'),
-						course('CSCI 123'),
-					],
-				},
-			],
-		})
+		expect(parse('CSCI 121 | 122 & 123')).toMatchSnapshot()
 	})
 
 	it('supports parentheses to control order-of-operations - (a | b) & c', () => {
-		expect(parse('(CSCI 121 | 122) & 123')).toEqual({
-			$type: 'boolean',
-			$booleanType: 'and',
-			$and: [
-				{
-					$type: 'boolean',
-					$booleanType: 'or',
-					$or: [
-						course('CSCI 121'),
-						course('CSCI 122'),
-					],
-				},
-				course('CSCI 123'),
-			],
-		})
+		expect(parse('(CSCI 121 | 122) & 123')).toMatchSnapshot()
 	})
 })
