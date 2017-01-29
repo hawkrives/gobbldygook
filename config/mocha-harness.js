0457d7d5acca2c25bb3b96d28b580733e9a1881c
Hawken Rives <hawkrives@gmail.com>
2017-01-28 22:52:41 -0600

    inline the localstorage mock

--- config/mocha-harness.js
+++ config/mocha-harness.js
@@ -1,24 +1,23 @@
 global.fetch = () => Promise.resolve({})
 
-let storage = {}
 global.localStorage = {
-	_storage: storage,
-	getItem: key => {
-		if (!localStorage.hasItem(key)) {
+	_storage: {},
+	getItem(key) {
+		if (!this.hasItem(key)) {
 			return null
 		}
-		return storage[key]
+		return this._storage[key]
 	},
-	setItem: (key, val) => {
-		return storage[key] = String(val)
+	setItem(key, val) {
+		this._storage[key] = String(val)
 	},
-	removeItem: key => {
-		delete storage[key]
+	removeItem(key) {
+		delete this._storage[key]
 	},
-	hasItem: key => {
-		return key in storage
+	hasItem(key) {
+		return key in this._storage
 	},
-	clear: () => {
-		storage = {}
+	clear() {
+		this._storage = {}
 	},
 }
