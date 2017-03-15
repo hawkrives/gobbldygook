5f23cbe3293cc0cb9bd4615ce46eadb4563724ba
Hawken Rives <hawkrives@gmail.com>
2017-03-15 13:05:45 -0500

    add test for AppContainer + navigation

---
+++ modules/web/__tests__/app-container.test.js
@@ -0,0 +1,38 @@
+// @flow
+
+import React from 'react'
+import { mount } from 'enzyme'
+
+import AppContainer from '../app-container'
+
+import configureStore from '../redux'
+import ReduxWrapper from '../redux-wrapper'
+import Router from 'react-router/lib/Router'
+import { createMemoryHistory } from 'history'
+import routes from '../routes'
+
+import debug from 'debug'
+debug.enable('foo')
+
+test('AppContainer renders', () => {
+    mount(
+        <ReduxWrapper store={configureStore()}>
+            <Router history={createMemoryHistory()} routes={routes} />
+        </ReduxWrapper>
+    )
+})
+
+test('AppContainer can navigate to /create', () => {
+    const store = configureStore()
+    const history = createMemoryHistory()
+
+    const app = mount(
+        <ReduxWrapper store={store}>
+            <Router history={history} routes={routes} />
+        </ReduxWrapper>
+    )
+
+    history.push('/create')
+
+    expect(app.find('h1').text()).toEqual('Hi there!')
+})
