9a631b06f045dc3a11fd02e6633c6d18a203a05a
Hawken Rives <hawkrives@gmail.com>
2017-03-15 17:16:59 -0500

    prettify modules/schedule-builder

--- modules/schedule-builder/combo-has-courses.js
+++ modules/schedule-builder/combo-has-courses.js
@@ -2,8 +2,10 @@ import takeWhile from 'lodash/takeWhile'
 import { queryCourses } from '../search-queries'
 
 export function comboHasCourses(courses, combinationOfClasses) {
-    const these = takeWhile(courses, course =>
-			queryCourses(course, combinationOfClasses).length >= 1)
+    const these = takeWhile(
+        courses,
+        course => queryCourses(course, combinationOfClasses).length >= 1
+    )
 
     return these.length === courses.length
 }
