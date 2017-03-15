eee5c721d345b694708dc8e9586d9d9b605cc20c
Hawken Rives <hawkrives@gmail.com>
2017-03-15 13:18:31 -0500

    fix lint warnings in app-container.test

--- modules/web/__tests__/app-container.test.js
+++ modules/web/__tests__/app-container.test.js
@@ -3,8 +3,6 @@
 import React from 'react'
 import { mount } from 'enzyme'
 
-import AppContainer from '../app-container'
-
 import configureStore from '../redux'
 import ReduxWrapper from '../redux-wrapper'
 import Router from 'react-router/lib/Router'
@@ -15,24 +13,24 @@ import debug from 'debug'
 debug.enable('foo')
 
 test('AppContainer renders', () => {
-    mount(
-        <ReduxWrapper store={configureStore()}>
-            <Router history={createMemoryHistory()} routes={routes} />
-        </ReduxWrapper>
-    )
+	mount(
+		<ReduxWrapper store={configureStore()}>
+			<Router history={createMemoryHistory()} routes={routes} />
+		</ReduxWrapper>
+	)
 })
 
 test('AppContainer can navigate to /create', () => {
-    const store = configureStore()
-    const history = createMemoryHistory()
+	const store = configureStore()
+	const history = createMemoryHistory()
 
-    const app = mount(
-        <ReduxWrapper store={store}>
-            <Router history={history} routes={routes} />
-        </ReduxWrapper>
-    )
+	const app = mount(
+		<ReduxWrapper store={store}>
+			<Router history={history} routes={routes} />
+		</ReduxWrapper>
+	)
 
-    history.push('/create')
+	history.push('/create')
 
-    expect(app.find('h1').text()).toEqual('Hi there!')
+	expect(app.find('h1').text()).toEqual('Hi there!')
 })
