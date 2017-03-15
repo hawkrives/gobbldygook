c7887f711173987fcb0217b85f902bdd6f0efcf0
Hawken Rives <hawkrives@gmail.com>
2017-03-15 17:13:52 -0500

    reindent modules/object-student-cli

--- modules/object-student-cli/olaf-find-student-courses.js
+++ modules/object-student-cli/olaf-find-student-courses.js
@@ -18,95 +18,95 @@ import toPairs from 'lodash/toPairs'
 import head from 'lodash/head'
 
 async function populateStudent(filename) {
-	let student
-	try {
-		student = await loadYamlFile(filename)
-	}
-	catch (err) {
-		console.error('Problem loading student', err)
-	}
+    let student
+    try {
+        student = await loadYamlFile(filename)
+    }
+    catch (err) {
+        console.error('Problem loading student', err)
+    }
 
-	try {
-		student.courses = await populateCourses(student)
-	}
-	catch (err) {
-		console.error('Problem populating courses', err)
-	}
+    try {
+        student.courses = await populateCourses(student)
+    }
+    catch (err) {
+        console.error('Problem populating courses', err)
+    }
 
-	try {
-		student.studies.push({ type: 'degree', name: 'Bachelor of Arts' })
-		student.areas = await Promise.all(student.studies.map(loadArea))
-	}
-	catch (err) {
-		console.error('Problem loading areas', err)
-	}
+    try {
+        student.studies.push({ type: 'degree', name: 'Bachelor of Arts' })
+        student.areas = await Promise.all(student.studies.map(loadArea))
+    }
+    catch (err) {
+        console.error('Problem loading areas', err)
+    }
 
-	return student
+    return student
 }
 
 function prettyCourse(c) {
-	return `${c.department.join('/')} ${c.number}${c.gereqs ? ` [${c.gereqs.join(' ')}]` : ''}`
+    return `${c.department.join('/')} ${c.number}${c.gereqs ? ` [${c.gereqs.join(' ')}]` : ''}`
 }
 
 function prettyCourseList(list) {
-	return list.map(c => `  - ${prettyCourse(c)}`).join('\n')
+    return list.map(c => `  - ${prettyCourse(c)}`).join('\n')
 }
 
 function makeHeading(str) {
-	return `${str}\n${repeat('=', str.length)}\n`
+    return `${str}\n${repeat('=', str.length)}\n`
 }
 
 function evaluateStudentAgainstEachMajor(student) {
-	const degreeEvaluation = evaluate(student, find(student.areas, a => a.type === 'degree'))
-	const countedTowardsDegree = [...degreeEvaluation.result._matches]
+    const degreeEvaluation = evaluate(student, find(student.areas, a => a.type === 'degree'))
+    const countedTowardsDegree = [...degreeEvaluation.result._matches]
 	// console.log(yaml.safeDump(degreeEvaluation))
 
-	let countedTowardsMajors = {}
-	let areas = student.areas
+    let countedTowardsMajors = {}
+    let areas = student.areas
 		.filter(a => a.type.toLowerCase() !== 'degree')
 		.map(area => evaluate(student, area))
 
-	areas.forEach(evaluated => {
-		countedTowardsMajors[evaluated.name] = evaluated.result._matches
-	})
+    areas.forEach(evaluated => {
+        countedTowardsMajors[evaluated.name] = evaluated.result._matches
+    })
 
 	///
 
-	let [name, coursesUsedInMajor] = head(toPairs(countedTowardsMajors))
+    let [name, coursesUsedInMajor] = head(toPairs(countedTowardsMajors))
 
-	let usedCourses = uniqBy([...coursesUsedInMajor, ...countedTowardsDegree], simplifyCourse)
-	let usedClbids = map(usedCourses, c => c.clbid)
+    let usedCourses = uniqBy([...coursesUsedInMajor, ...countedTowardsDegree], simplifyCourse)
+    let usedClbids = map(usedCourses, c => c.clbid)
 
-	let unusedCourses = reject(
+    let unusedCourses = reject(
 		uniqBy([...student.courses, ...flatten(countedTowardsMajors), ...countedTowardsDegree], simplifyCourse),
 		c => includes(usedClbids, c.clbid))
 
-	console.log(makeHeading(`${`Used for ${name}… (result: ${find(areas, { name }).computed})`}`))
-	console.log(prettyCourseList(usedCourses))
-	console.log()
+    console.log(makeHeading(`${`Used for ${name}… (result: ${find(areas, { name }).computed})`}`))
+    console.log(prettyCourseList(usedCourses))
+    console.log()
 
-	console.log(makeHeading('Not used:'))
-	console.log(prettyCourseList(unusedCourses))
+    console.log(makeHeading('Not used:'))
+    console.log(prettyCourseList(unusedCourses))
 }
 
 export function cli() {
-	const args = nomnom()
+    const args = nomnom()
 		.script('olaf-find-student-courses')
 		.option('json', {
-			flag: true,
-			help: 'Print JSON structures',
-		})
+    flag: true,
+    help: 'Print JSON structures',
+})
 		.option('students', {
-			required: true,
-			metavar: 'FILE',
-			help: 'The student file(s) to load',
-			position: 0,
-			list: true,
-		})
+    required: true,
+    metavar: 'FILE',
+    help: 'The student file(s) to load',
+    position: 0,
+    list: true,
+})
 		.parse()
 
-	for (let student of args.students) {
-		populateStudent(student)
+    for (let student of args.students) {
+        populateStudent(student)
 			.then(evaluateStudentAgainstEachMajor)
-	}
+    }
 }
