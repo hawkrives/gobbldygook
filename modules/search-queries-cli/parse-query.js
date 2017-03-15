136c5b7d0623bd16b509e5912487e64e8304eb18
Hawken Rives <hawkrives@gmail.com>
2017-03-15 17:17:12 -0500

    prettify modules/search-queries-cli

--- modules/search-queries-cli/parse-query.js
+++ modules/search-queries-cli/parse-query.js
@@ -1,14 +1,16 @@
-import { buildQueryFromString } from '../search-queries/build-query-from-string'
+import {
+    buildQueryFromString,
+} from '../search-queries/build-query-from-string'
 import nom from 'nomnom'
 import stringify from 'stabilize'
 import yaml from 'js-yaml'
 
 export function cli() {
     const args = nom
-		.option('json', { flag: true, help: 'Print the result as valid JSON' })
-		.option('yaml', { flag: true, help: 'Print the result as YAML' })
-		.option('query', { position: 0, required: true })
-		.parse()
+        .option('json', { flag: true, help: 'Print the result as valid JSON' })
+        .option('yaml', { flag: true, help: 'Print the result as YAML' })
+        .option('query', { position: 0, required: true })
+        .parse()
 
     const query = buildQueryFromString(args.query)
 
