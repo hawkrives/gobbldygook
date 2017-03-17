325036a834c7b6f81e0d19a7a9b97435513b3ded
Hawken Rives <hawkrives@gmail.com>
2017-03-17 09:48:24 -0500

    update tests for findCourseWarnings

--- modules/object-student/__tests__/find-course-warnings.test.js
+++ modules/object-student/__tests__/find-course-warnings.test.js
@@ -8,13 +8,25 @@ import {
 
 describe('checkForInvalidYear', () => {
     it('checks for an invalid year on a course', () => {
-        expect(checkForInvalidYear({ year: 1994 }, 2012)).toMatchSnapshot()
+        expect(checkForInvalidYear({ year: 1994, semester: 1 }, 2012)).toMatchSnapshot()
+    })
+
+    it('returns null if no semester is present', () => {
+        expect(checkForInvalidYear({ year: 1994 }, 2012)).toBe(null)
+    })
+
+    it('returns null if the semester is "not from stolaf"', () => {
+        expect(checkForInvalidYear({ year: 1994, semester: 9 }, 2012)).toBe(null)
     })
 })
 
 describe('checkForInvalidSemester', () => {
     it('checks for an invalid semester on a course', () => {
         expect(checkForInvalidSemester({ semester: 2 }, 5)).toMatchSnapshot()
+    })
+
+    it('returns null if no semester is given', () => {
+        expect(checkForInvalidSemester({ semester: undefined }, 5)).toBe(null)
     })
 })
 
