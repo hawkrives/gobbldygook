962116a2aa882f8359aee8b0c54a2da18c6e4c24
Hawken Rives <hawkrives@gmail.com>
2017-03-18 12:38:22 -0500

    better enforce types in DegreeSummary

--- modules/web/modules/student/student-summary.js
+++ modules/web/modules/student/student-summary.js
@@ -215,16 +215,20 @@ export class DateSummary extends React.PureComponent {
 
 export class DegreeSummary extends React.PureComponent {
     props: {
-        studies: any[],
+        studies: {type: string, name: string}[],
     };
 
     render() {
+        const grouped: {
+            [key: string]: {type: string, name: string}[],
+        } = groupBy(this.props.studies, s => s.type)
+
         const {
             degree: dS = [],
             major: mS = [],
             concentration: cS = [],
             emphasis: eS = [],
-        } = groupBy(this.props.studies, s => s.type)
+        } = grouped
 
         const dCount = dS.length
         const mCount = mS.length
