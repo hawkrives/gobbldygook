a91ff2961cff9368a83e87a46c673689fec61d9a
Hawken Rives <hawkrives@gmail.com>
2017-03-17 22:19:36 -0500

    port List to styled-components

--- modules/web/components/list.js
+++ modules/web/components/list.js
@@ -5,8 +5,30 @@ import React, {
     cloneElement,
 } from 'react'
 import cx from 'classnames'
+import styled from 'styled-components'
 
-import './list.scss'
+const BaseList = `
+    margin: 0;
+    padding: 0;
+    list-style: none;
+`
+
+const InlineList = styled.ul`
+    ${BaseList}
+    display: inline-block;
+
+    & > .list-item {
+        display: inline-block;
+    }
+`
+
+const BulletedList = styled.ul`
+    ${BaseList}
+`
+
+const NumberedList = styled.ol`
+    ${BaseList}
+`
 
 type ListProps = {
     children?: any,
@@ -15,16 +37,8 @@ type ListProps = {
 };
 
 export default function List(props: ListProps) {
-    const {
-        children,
-        type = 'inline',
-    } = props
+    const { className, children, type = 'inline' } = props
 
-    let {
-        className,
-    } = props
-
-    // eslint-disable-next-line no-confusing-arrow
     const contents = ReactChildren.map(
         children,
         child =>
@@ -36,11 +50,12 @@ export default function List(props: ListProps) {
                 : child
     )
 
-    className = cx('list', `list--${type}`, className)
-
-    if (type === 'number') {
-        return <ol className={className}>{contents}</ol>
+    let ListType = BulletedList
+    if (type === 'inline') {
+        ListType = InlineList
+    } else if (type === 'number') {
+        ListType = NumberedList
     }
 
-    return <ul className={className}>{contents}</ul>
+    return <ListType className={className}>{contents}</ListType>
 }
