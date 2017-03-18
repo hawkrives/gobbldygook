70b5047e4cb0c2a41a0bd0455db2f02599cdab0e
Hawken Rives <hawkrives@gmail.com>
2017-03-17 21:02:44 -0500

    port scss variables and colors to js

---
+++ modules/web/theme/variables.js
@@ -0,0 +1,27 @@
+// @flow
+
+// colors
+export const background = '#e9eaed'
+export const textColor = '#2c281f'
+
+// fonts
+export const sansFontStack = 'Fira Sans, Helvetica, Verdana, sans-serif'
+export const bodyFontStack = sansFontStack
+export const headingFontStack = sansFontStack
+
+// spacing
+export const pageEdgePadding = '15px'
+export const blockEdgePadding = '5px'
+export const areaEdgePadding = '0.5em'
+
+// z-indices
+export const zSidebar = '3'
+export const zFloatingButton = '2'
+
+// media queries
+export const singleColumnWidth = '35em'
+export const yearCollapseWidth = '47.5em'
+
+export const materialDivider = '1px solid rgba(160, 160, 160, 0.2)'
+export const baseBorderRadius = '3px'
+export const sidebarWidth = '270px'
