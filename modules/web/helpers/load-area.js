7828697f281f2449251533cb0fd3156d746fc574
Hawken Rives <hawkrives@gmail.com>
2017-03-18 17:31:15 -0500

    add loadAreaFromNetwork function

--- modules/web/helpers/load-area.js
+++ modules/web/helpers/load-area.js
@@ -1,9 +1,14 @@
+// @flow
 import db from './db'
 import { enhanceHanson } from '../../hanson-format'
 import some from 'lodash/some'
 import maxBy from 'lodash/maxBy'
+import find from 'lodash/find'
+import kebabCase from 'lodash/kebabCase'
 import yaml from 'js-yaml'
 import debug from 'debug'
+import pluralizeArea from '../../examine-student/pluralize-area'
+import {status, text} from '../../lib/fetch-helpers'
 const log = debug('worker:load-area')
 
 function resolveArea(areas, query) {
@@ -16,18 +21,41 @@ function resolveArea(areas, query) {
     }
 }
 
-function loadArea(areaQuery) {
-    const { name, type, revision, source, isCustom } = areaQuery
+function transform(areaSource) {
+    return enhanceHanson(yaml.safeLoad(areaSource))
+}
 
-    let area = { ...areaQuery }
-    if (isCustom && source) {
-        return Promise.resolve({
-            ...areaQuery,
-            _area: enhanceHanson(yaml.safeLoad(source)),
-        })
+type AreaQueryType = {
+    name: string,
+    type: string,
+    revision: string,
+    source: string,
+    isCustom: string,
+};
+
+const baseUrl = 'https://hawkrives.github.io/gobbldygook-area-data'
+const networkCache = Object.create(null)
+function loadAreaFromNetwork({name, type, revision}: AreaQueryType) {
+    const id = `{${name}, ${type}, ${revision}}`
+    if (id in networkCache) {
+        return networkCache[id]
     }
 
-    let dbQuery = { name: [name], type: [type] }
+    const path = `${baseUrl}/${pluralizeArea(type)}/${kebabCase(name)}.yaml`
+
+    networkCache[id] = fetch(path).then(status).then(text).then(transform)
+
+    return networkCache[id].then(area => {
+        return {
+            name, type, revision, _area: area,
+        }
+    })
+}
+
+function loadAreaFromDatabase(areaQuery: AreaQueryType) {
+    const { name, type, revision } = areaQuery
+
+    let dbQuery: any = { name: [name], type: [type] }
     if (revision && revision !== 'latest') {
         dbQuery.revision = [revision]
     }
@@ -36,7 +64,7 @@ function loadArea(areaQuery) {
         .store('areas')
         .query(dbQuery)
         .then(result => {
-            if (result === undefined) {
+            if (!result || !result.length) {
                 return {
                     ...areaQuery,
                     _error: `the area "${name}" (${type}) could not be found with the query ${JSON.stringify(dbQuery)}`,
@@ -45,32 +73,28 @@ function loadArea(areaQuery) {
 
             if (result.length === 1) {
                 result = result[0]
-            } else if (result.length >= 2) {
-                result = resolveArea(result, dbQuery)
             } else {
-                return {
-                    name,
-                    type,
-                    revision,
-                    _error: `the area "${name}" (${type}) could not be found with the query ${JSON.stringify(dbQuery)}`,
-                }
+                result = resolveArea(result, dbQuery)
             }
 
             return { ...areaQuery, _area: enhanceHanson(result) }
         })
         .catch(err => {
             log(err) // we can probably remove this in the future
-            area._error = `Could not find area ${JSON.stringify(dbQuery)} (error: ${err.message})`
-            return area
+            return {
+                ...areaQuery,
+                _error: `Could not find area ${JSON.stringify(dbQuery)} (error: ${err.message})`,
+            }
         })
 }
 
 const promiseCache = Object.create(null)
 
 export default function getArea(
-    { name, type, revision, source, isCustom },
-    { cache = [] }
+    areaQuery: AreaQueryType,
+    { cache = [] }: {cache: any[]}
 ) {
+    const { name, type, revision, source, isCustom } = areaQuery
     let cachedArea = find(
         cache,
         a =>
@@ -83,16 +107,22 @@ export default function getArea(
         return cachedArea
     }
 
-    let id = `{${name}, ${type}, ${revision}}`
+    if (isCustom && source) {
+        return Promise.resolve({
+            ...areaQuery,
+            _area: transform(source),
+        })
+    }
+
+    const id = `{${name}, ${type}, ${revision}}`
     if (id in promiseCache) {
         return promiseCache[id]
     }
 
-    let promise = loadArea({ name, type, revision, source, isCustom })
-
-    promiseCache[id] = promise
+    promiseCache[id] = loadAreaFromDatabase({ name, type, revision, source, isCustom })
 
     return promiseCache[id].then(area => {
+        console.log(area)
         delete promiseCache[id]
         return area
     })
