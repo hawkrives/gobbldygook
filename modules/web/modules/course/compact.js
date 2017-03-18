759e6b742ac6c65f8698b3001e48d1c2160cf977
Hawken Rives <hawkrives@gmail.com>
2017-03-18 09:17:32 -0500

    split DraggableCourse into Draggable, Compact, and WithModal

---
+++ modules/web/modules/course/compact.js
@@ -0,0 +1,112 @@
+// @flow
+import React from 'react'
+import map from 'lodash/map'
+import noop from 'lodash/noop'
+import styled from 'styled-components'
+
+import { InlineList, InlineListItem } from '../../components/list'
+import CourseTitle from './course-title'
+import { buildDeptNum } from '../../../school-st-olaf-college/deptnums'
+import CourseWarnings from './warnings'
+
+const Container = styled.article`
+    display: block;
+
+    &:hover {
+        cursor: pointer;
+        background-color: ${props => props.theme.gray100};
+    }
+
+    &.is-dragging {
+        opacity: 0.5;
+    }
+`
+
+const Row = `
+    overflow: hidden;
+    line-height: 1.5;
+`
+
+const Title = styled(CourseTitle)`
+    ${Row}
+`
+
+const SummaryRow = styled.div`
+    ${Row}
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    font-size: 0.75em;
+
+    & > * + *:not(:empty)::before {
+        margin: 0 0.2em;
+        content: "·";
+    }
+`
+
+const GeReqItem = styled(InlineListItem)`
+    & + &::before {
+        margin: 0 0.2em;
+        content: "+";
+    }
+`
+
+const Identifier = styled.span`
+    font-feature-settings: "tnum";
+`
+
+const Type = styled.span``
+const Prereqs = styled.span``
+
+export default class CompactCourse extends React.PureComponent {
+    props: {
+        className?: string,
+        conflicts?: any[],
+        course: Object,
+        index?: number,
+        onClick?: (Event) => any,
+    };
+
+    render() {
+        const {
+            course,
+            conflicts = [],
+            index,
+            onClick = noop,
+        } = this.props
+
+        return (
+            <Container className={this.props.className} onClick={onClick}>
+                <CourseWarnings warnings={conflicts[index || 0]} />
+
+                <Title
+                    title={course.title}
+                    name={course.name}
+                    type={course.type}
+                />
+
+                <SummaryRow>
+                    <Identifier>
+                        {buildDeptNum(course, true)}
+                    </Identifier>
+                    {course.type !== 'Research' && <Type>{course.type}</Type>}
+                    {course.gereqs &&
+                        <InlineList>
+                            {map(course.gereqs, ge => (
+                                <GeReqItem key={ge}>{ge}</GeReqItem>
+                            ))}
+                        </InlineList>}
+                    {course.prerequisites &&
+                        <Prereqs title={course.prerequisites}>
+                            Prereq
+                        </Prereqs>}
+                </SummaryRow>
+                <SummaryRow>
+                    {map(course.times, timestring => (
+                        <span key={timestring}>{timestring}</span>
+                    ))}
+                </SummaryRow>
+            </Container>
+        )
+    }
+}
--- modules/web/modules/course/draggable.js
+++ modules/web/modules/course/draggable.js
@@ -1,155 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { DragSource } from 'react-dnd'
 import cx from 'classnames'
-import map from 'lodash/map'
-import styled from 'styled-components'
 import { IDENT_COURSE } from '../../../object-student/item-types'
-
-import { InlineList, InlineListItem } from '../../components/list'
-import CourseTitle from './course-title'
-import { buildDeptNum } from '../../../school-st-olaf-college/deptnums'
-import ModalCourse from './modal'
-import { CourseWarnings } from './warnings'
-
-const Container = styled.article`
-    display: block;
-
-    & p,
-    & ul {
-        margin: 0;
-    }
-
-    &:hover {
-        cursor: pointer;
-        background-color: ${props => props.theme.gray100};
-    }
-
-    &.is-dragging {
-        opacity: 0.5;
-    }
-`
-
-const Row = `
-    overflow: hidden;
-    line-height: 1.5;
-`
-
-const Title = styled(CourseTitle)`
-    ${Row}
-`
-
-const SummaryRow = styled.div`
-    ${Row}
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-    font-size: 0.75em;
-
-    & > * + *:not(:empty)::before {
-        content: " · ";
-    }
-`
-
-const GeReqsList = styled(InlineList)``
-
-const GeReqItem = styled(InlineListItem)`
-    & + &::before {
-        content: " + ";
-    }
-`
-
-const Identifier = styled.span`
-    font-feature-settings: "tnum";
-`
-
-const Type = styled.span``
-const Prereqs = styled.span``
+import CourseWithModal from './with-modal'
 
 type PropTypes = {
     className?: string,
-    conflicts?: any[],
     connectDragSource: () => any, // react-dnd
-    course: Object,
-    index?: number,
     isDragging: boolean, // react-dnd
-    scheduleId?: string,
-    studentId?: string,
 };
 
-class DraggableCourse extends Component {
+class DraggableCourse extends React.PureComponent {
     props: PropTypes;
 
-    state = {
-        isOpen: false,
-    };
-
-    shouldComponentUpdate(nextProps: PropTypes, nextState) {
-        return this.props.course !== nextProps.course ||
-            this.props.conflicts !== nextProps.conflicts ||
-            this.state.isOpen !== nextState.isOpen ||
-            this.props.isDragging !== nextProps.isDragging
-    }
-
-    closeModal = () => {
-        this.setState({ isOpen: false })
-    };
-
-    openModal = () => {
-        this.setState({ isOpen: true })
-    };
-
     render() {
-        const {
-            course,
-            conflicts = [],
-            index,
-            scheduleId,
-            studentId,
-        } = this.props
-
         const classSet = cx(this.props.className, {
             'is-dragging': this.props.isDragging,
         })
 
         return this.props.connectDragSource(
-            <Container className={classSet} onClick={this.openModal}>
-                <CourseWarnings warnings={conflicts[index || 0]} />
-
-                <Title
-                    title={course.title}
-                    name={course.name}
-                    type={course.type}
-                />
-                <SummaryRow>
-                    <Identifier>
-                        {buildDeptNum(course, true)}
-                    </Identifier>
-                    {course.type !== 'Research' && <Type>{course.type}</Type>}
-                    {course.gereqs &&
-                        <GeReqsList>
-                            {map(course.gereqs, ge => (
-                                <GeReqItem key={ge}>{ge}</GeReqItem>
-                            ))}
-                        </GeReqsList>}
-                    {course.prerequisites &&
-                        <Prereqs title={course.prerequisites}>
-                            Prereq
-                        </Prereqs>}
-                </SummaryRow>
-                <SummaryRow>
-                    {map(course.times, timestring => (
-                        <span key={timestring}>{timestring}</span>
-                    ))}
-                </SummaryRow>
-
-                {this.state.isOpen &&
-                    <ModalCourse
-                        onClose={this.closeModal}
-                        course={course}
-                        scheduleId={scheduleId}
-                        studentId={studentId}
-                    />}
-            </Container>
+            <CourseWithModal
+                className={classSet}
+                {...this.props}
+            />
         )
     }
 }
