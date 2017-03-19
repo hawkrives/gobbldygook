4778f3ee3f97d7bb5e140c556fef867f45f9de00
Hawken Rives <hawkrives@gmail.com>
2017-03-19 17:13:06 -0500

    centralize all dispatch calls in lib-dispatch
    and create a Notification class that manages a notification, so we don't need to pass as many
    arguments around.

--- modules/web/helpers/workers/load-data/lib-dispatch.js
+++ modules/web/helpers/workers/load-data/lib-dispatch.js
@@ -3,7 +3,46 @@
 import debug from 'debug'
 const log = debug('worker:load-data:dispatch')
 
-export default function dispatch(type: string, action: string, ...args: any[]) {
-    log([null, 'dispatch', { type, action, args }])
-    self.postMessage([null, 'dispatch', { type, action, args }])
+function dispatch(type: string, action: string, ...args: any[]) {
+    const toDispatch = [null, 'dispatch', { type, action, args }]
+
+    log(toDispatch)
+    self.postMessage(toDispatch)
+}
+
+export const refreshCourses = () => dispatch('courses', 'refreshCourses')
+export const refreshAreas = () => dispatch('areas', 'refreshAreas')
+export const quotaExceededError = (dbName: string) => {
+    dispatch('notifications', 'logError', {
+        id: 'db-storage-quota-exceeded',
+        message: `The database "${dbName}" has exceeded its storage quota.`,
+    })
+}
+
+export class Notification {
+    id: string;
+    type: string;
+    length = 1;
+    store = 'notifications';
+
+    constructor(notificationType: string) {
+        this.id = notificationType
+        this.type = notificationType
+    }
+
+    start(length: number) {
+        this.length = length
+
+        const msg = `Loading ${this.type}`
+        const args = { max: this.length, showButton: true }
+        dispatch(this.store, 'startProgress', this.id, msg, args)
+    }
+
+    increment() {
+        dispatch(this.store, 'incrementProgress', this.id)
+    }
+
+    remove() {
+        dispatch(this.store, 'removeNotification', this.id, 1500)
+    }
 }
