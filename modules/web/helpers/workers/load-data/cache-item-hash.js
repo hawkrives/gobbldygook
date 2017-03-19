f050c78dd02c0106ac78b6ebcb6119770b62ebe3
Hawken Rives <hawkrives@gmail.com>
2017-03-18 20:00:11 -0500

    fix remaining path issues

--- modules/web/helpers/workers/load-data/cache-item-hash.js
+++ modules/web/helpers/workers/load-data/cache-item-hash.js
@@ -1,6 +1,6 @@
 // @flow
 
-import db from '../db'
+import db from '../../db'
 import log from './lib-log'
 import getCacheStoreName from './get-cache-store-name'
 import type { InfoFileTypeEnum } from './types'
