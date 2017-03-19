cc648515096df54659e73a70ffc93242d57d15fc
Hawken Rives <hawkrives@gmail.com>
2017-03-19 08:52:55 -0500

    add call to db.__clear() to cacheItemHash tests

--- modules/web/helpers/workers/load-data/__tests__/cache-item-hash.test.js
+++ modules/web/helpers/workers/load-data/__tests__/cache-item-hash.test.js
@@ -6,8 +6,13 @@ jest.mock('../../../db')
 import db from '../../../db'
 import cacheItemHash from '../cache-item-hash'
 
+beforeEach(async () => {
+    await db.__clear()
+})
+
 test('cacheItemHash runs', () => {
-    expect(() => cacheItemHash('folder/file', 'courses', 'deadbeef')).not.toThrow()
+    expect(() =>
+        cacheItemHash('folder/file', 'courses', 'deadbeef')).not.toThrow()
 })
 
 test('cacheItemHash stores data', async () => {
