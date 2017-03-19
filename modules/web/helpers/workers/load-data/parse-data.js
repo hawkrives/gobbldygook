d3136522fc026cfedcd6aa9236b695cf27dc1725
Hawken Rives <hawkrives@gmail.com>
2017-03-18 20:59:24 -0500

    add try/catch to parseData

--- modules/web/helpers/workers/load-data/parse-data.js
+++ modules/web/helpers/workers/load-data/parse-data.js
@@ -4,12 +4,16 @@ import yaml from 'js-yaml'
 import type { InfoFileTypeEnum } from './types'
 
 export default function parseData(raw: string, type: InfoFileTypeEnum) {
-    if (type === 'courses') {
-        return JSON.parse(raw)
-    } else if (type === 'areas') {
-        let data = yaml.safeLoad(raw)
-        data.source = raw
-        return data
+    try {
+        if (type === 'courses') {
+            return JSON.parse(raw)
+        } else if (type === 'areas') {
+            let data = yaml.safeLoad(raw)
+            data.source = raw
+            return data
+        }
+    } catch (err) {
+        // ignoring the error
     }
     return {}
 }
