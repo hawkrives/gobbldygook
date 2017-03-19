03b7ce83b888bf4673d712753e4032d394923cc5
Hawken Rives <hawkrives@gmail.com>
2017-03-19 08:52:24 -0500

    rebuild db mock around indexeddbshim

--- modules/web/helpers/__mocks__/db.js
+++ modules/web/helpers/__mocks__/db.js
@@ -1,63 +1,27 @@
 /* eslint-env jest */
 // @flow
 
-// import range from 'idb-range'
+import setGlobals from 'indexeddbshim'
+let def
+setGlobals(global, (function() {
+    def = Object.defineProperty
+    // $FlowFixMe
+    Object.defineProperty = null
+})())
+// $FlowFixMe
+Object.defineProperty = def
 
-// const db = jest.genMockFromModule('../db')
+// $FlowFixMe
+const {default: db} = require.requireActual('../db')
 
-// const actual = require.requireActual('../db')
-
-class Store {
-    data = new Map()
-
-    index = name => ({
-        getAll: range => {
-
-        },
-    })
-
-    batch = async (ops) => {
-        for (const [key, value] of ops) {
-            if (key && value === null) {
-                await this.del(key) // eslint-disable-line no-await-in-loop
-            }
-        }
-        throw new Error('not implemented')
-    }
-
-    del = async key => {
-        this.data.delete(key)
-    }
-
-    get = async key => {
-        return this.data.get(key)
-    }
-
-    getAll = async () => {
-        return Array.from(this.data.values())
-    }
-
-    put = async ({id, ...data}) => {
-        this.data.set(id, {id, ...data})
-    }
-}
-
-class Database {
-    stores = new Map([
-        ['courses', new Store()],
-        ['courseCache', new Store()],
-        ['areas', new Store()],
-        ['areaCache', new Store()],
+db.__clear = async function clearDatabase() {
+    return Promise.all([
+        db.store('courses').clear(),
+        db.store('areas').clear(),
+        db.store('students').clear(),
+        db.store('courseCache').clear(),
+        db.store('areaCache').clear(),
     ])
-
-    store = (name: string) => {
-        // console.log(name)
-        return this.stores.get(name)
-    }
 }
 
-export default new Database()
-
-// console.log(actual.default.store('courses').put('foo'))
-
-// export default actual.default
+export default db
