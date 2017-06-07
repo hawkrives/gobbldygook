55a95d48c75a5c1ad7f34958ba6a330d34a87b22
Hawken Rives <hawkrives@gmail.com>
2017-06-06 22:05:12 -0500

    remove box-shadow from semesters

--- modules/web/theme/mixins.js
+++ modules/web/theme/mixins.js
@@ -13,33 +13,13 @@ export const materialShadow = `
     border-color: #e5e6e9 #dfe0e4 #d0d1d5;
 `
 
-export const elevation1 = `
-    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
-`
-
-export const elevation2 = `
-    box-shadow: 0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px -1px rgba(0,0,0,0.3);
-`
-
-export const elevation3 = `
-    box-shadow: 0 6px 10px 0 rgba(0,0,0,0.14), 0 1px 18px 0 rgba(0,0,0,0.12), 0 3px 5px -1px rgba(0,0,0,0.3);
-`
-
-export const elevation4 = `
-    box-shadow: 0 8px 10px 1px rgba(0,0,0,0.14), 0 3px 14px 2px rgba(0,0,0,0.12), 0 5px 5px -3px rgba(0,0,0,0.3);
-`
-
-export const elevation5 = `
-    box-shadow: 0 16px 24px 2px rgba(0,0,0,0.14), 0 6px 30px 5px rgba(0,0,0,0.12), 0 8px 10px -5px rgba(0,0,0,0.3);
-`
-
 export const baseCard = css`
     background-color: white;
     border-radius: ${props => props.theme.baseBorderRadius};
 `
 
 export const card = css`
-    ${elevation1}
+    ${materialShadow}
     ${baseCard}
 `
 
@@ -93,8 +73,7 @@ export const truncate = `
     text-overflow: ellipsis;
 `
 
-export const truncateWidth = (width: string) =>
-    `
+export const truncateWidth = (width: string) => `
     ${truncate}
     width: ${width};
 `
