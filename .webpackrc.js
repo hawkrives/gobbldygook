dbb7e85cedc49f548de42642da36069dd31507af
Hawken Rives <hawkrives@gmail.com>
2017-06-13 16:38:33 -0500

    add version hash to welcome screen

--- .webpackrc.js
+++ .webpackrc.js
@@ -240,6 +240,7 @@ function config() {
             // APP_BASE is used in react-router, to set its base appropriately
             // across both local dev and gh-pages.
             APP_BASE: JSON.stringify(publicPath),
+            COMMIT_HASH: JSON.stringify(process.env.TRAVIS_COMMIT),
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         }),
 
