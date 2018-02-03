2c2a3ad899e99347fc5666396ba28a4b261f9114
Hawken Rives <hawkrives@gmail.com>
2018-02-02 20:23:57 -0600

    conver avatar-letter to styled

--- modules/web/components/avatar-letter.js
+++ modules/web/components/avatar-letter.js
@@ -1,20 +1,38 @@
 // @flow
 import React from 'react'
-import cx from 'classnames'
 import isString from 'lodash/isString'
+import styled, {css} from 'styled-components'
 
-import './avatar-letter.scss'
+const Wrapper = styled.div`
+    font-family: Fira Sans, Helvetica Neue, Helvetica, Arial, sans-serif !important;
+    font-weight: 200;
+    font-style: normal;
 
-const AvatarLetter = ({
-    className,
-    value = '',
-}: {
+    text-align: center;
+    text-transform: uppercase;
+
+    display: inline-block;
+
+    margin: 0;
+    padding: 0;
+
+    ${({size = 48}) => css`
+        width: ${size}px;
+        height: ${size}px;
+        line-height: ${size}px;
+        font-size: ${size / 3 * 2}px;
+
+        border-radius: ${size}px;
+    `}
+`
+
+type Props = {
     className?: string,
     value: string,
-}) => (
-    <div className={cx('avatar-letter', className)}>
-        {isString(value) ? value[0] : ''}
-    </div>
-)
+}
 
-export default AvatarLetter
+export const AvatarLetter = ({className, value = '', }: Props) => (
+    <Wrapper className={className}>
+        {isString(value) ? value[0] : ''}
+    </Wrapper>
+)
