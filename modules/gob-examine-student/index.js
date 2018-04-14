a241def6df568179307d47bcfc6a72d3442047f5
Hawken Rives <hawkrives@gmail.com>
2018-04-14 13:13:20 -0500

    move examine-student sources into a /source folder

--- modules/gob-examine-student/index.js
+++ modules/gob-examine-student/index.js
@@ -1,9 +1,10 @@
 // @flow
-// can't use export * with babel: see https://github.com/babel/babel/issues/4446
-export {evaluate} from './evaluate'
-export {countCredits} from './count-credits'
-export {default as pathToOverride} from './path-to-override'
-export {default as pluralizeArea} from './pluralize-area'
-export {default as humanizeOperator} from './humanize-operator'
-export {default as simplifyCourse} from './simplify-course'
-export {default as isRequirementName} from './is-requirement-name'
+
+export {evaluate} from './source/evaluate'
+export {countCredits} from './source/count-credits'
+export {default as pathToOverride} from './source/path-to-override'
+export {default as pluralizeArea} from './source/pluralize-area'
+export {default as humanizeOperator} from './source/humanize-operator'
+export {default as simplifyCourse} from './source/simplify-course'
+export {default as isRequirementName} from './source/is-requirement-name'
+export * from './source/types'
