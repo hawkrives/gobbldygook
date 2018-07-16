26ab1b26494751a766bb912ef130c55caf05af30
Hawken Rives <hawkrives@fastmail.fm>
2018-07-16 09:48:58 -0500

    update deptNumRegex to handle Carleton's 999D.00 scheme

--- modules/gob-school-st-olaf-college/deptnums/dept-num-regex.js
+++ modules/gob-school-st-olaf-college/deptnums/dept-num-regex.js
@@ -1,3 +1,3 @@
 // @flow
 
-export const deptNumRegex = /(([A-Z]+)(?=\/)(?:\/)([A-Z]+)|[A-Z]+) *([0-9]{3,}) *([A-Z]?)/i
+export const deptNumRegex = /(([A-Z]+)(?=\/)(?:\/)([A-Z]+)|[A-Z]+) *([0-9]{3,}[A-Z]?)(?:\.(.+))?/i
