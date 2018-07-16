321a8920500f6f7ceed098710039a83dc3ee7f6b
Hawken Rives <hawkrives@fastmail.fm>
2018-07-16 09:35:45 -0500

    update prepare-course for carleton

--- modules/gob-worker-load-data/source/lib-prepare-course.js
+++ modules/gob-worker-load-data/source/lib-prepare-course.js
@@ -1,29 +1,16 @@
 // @flow
 
 import flatMap from 'lodash/flatMap'
-import {buildDeptString, buildDeptNum} from '@gob/school-st-olaf-college'
 import {splitParagraph} from '@gob/lib'
-import {convertTimeStringsToOfferings} from 'sto-sis-time-parser'
 
 export default function prepareCourse(course: any) {
 	const nameWords = splitParagraph(course.name)
-	const notesWords = splitParagraph((course.notes || []).join('\n'))
-	const titleWords = splitParagraph(course.title)
-	const descWords = splitParagraph((course.description || []).join('\n'))
+	const summaryWords = splitParagraph(course.summary || '')
+	const commentsWords = (course.comments || []).map(str => splitParagraph(str))
 
 	return {
-		name: course.name || course.title,
-		dept: course.dept || buildDeptString(course.departments),
-		deptnum: course.deptnum || buildDeptNum(course),
-		offerings: course.offerings || convertTimeStringsToOfferings(course),
-		words: [
-			...new Set([
-				...nameWords,
-				...notesWords,
-				...titleWords,
-				...descWords,
-			]),
-		],
+		words: [...new Set([...nameWords, ...commentsWords, ...summaryWords])],
 		profWords: [...new Set(flatMap(course.instructors, splitParagraph))],
+		term: `${course.year}${course.semester}`,
 	}
 }
