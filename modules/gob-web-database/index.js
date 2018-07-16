8b52ffec5819d35a492b7248d3123d4767605ed0
Hawken Rives <hawkrives@fastmail.fm>
2018-07-16 09:46:31 -0500

    update database schema for carleton

--- modules/gob-web-database/index.js
+++ modules/gob-web-database/index.js
@@ -1,6 +1,6 @@
 import treo, {Database} from 'treo'
 
-import Promise from 'es6-promise'
+//import Promise from 'es6-promise'
 treo.Promise = Promise
 
 import queryTreoDatabase from '@gob/treo-plugin-query'
@@ -8,5 +8,5 @@ import batchGet from '@gob/treo-plugin-batch-get'
 
 import defaultSchema from './schema'
 
-export const createDatabase = (name = 'gobbldygook', schema = defaultSchema) =>
+export const createDatabase = (name = 'gobbldygook-carleton', schema = defaultSchema) =>
 	new Database(name, schema).use(queryTreoDatabase()).use(batchGet())
