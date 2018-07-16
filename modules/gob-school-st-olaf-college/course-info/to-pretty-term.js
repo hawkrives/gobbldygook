f52cf5378de2351f2473830f12645d8c120bae66
Hawken Rives <hawkrives@fastmail.fm>
2018-07-16 09:50:10 -0500

    update toPrettyTerm for Carleton's string terms

--- modules/gob-school-st-olaf-college/course-info/to-pretty-term.js
+++ modules/gob-school-st-olaf-college/course-info/to-pretty-term.js
@@ -6,10 +6,9 @@ import {expandYear} from './expand-year'
 /* Takes a term and makes it pretty.
  * eg. {in: 20121, out: Fall 2012-13}
  */
-export function toPrettyTerm(term: number): string {
-	const strterm = String(term)
-	const year = strterm.substr(0, 4)
-	const sem = strterm.substr(4, 1)
+export function toPrettyTerm(term: string): string {
+	let year = term.substr(0, 4)
+	let sem = term.substr(4, 2)
 
 	return `${semesterName(sem)} ${expandYear(year)}`
 }
