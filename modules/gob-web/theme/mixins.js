b488a84fdacb93fe634a5e4a0814899295fdfbf4
Hawken Rives <hawkrives@fastmail.fm>
2018-07-22 19:44:42 -0500

    fix mixed indentation in mixins.js

--- modules/gob-web/theme/mixins.js
+++ modules/gob-web/theme/mixins.js
@@ -10,8 +10,8 @@ export const contentBlockSpacing = css`
 `
 
 export const materialShadow = `
-    border: 1px solid;
-    border-color: #e5e6e9 #dfe0e4 #d0d1d5;
+	border: 1px solid;
+	border-color: #e5e6e9 #dfe0e4 #d0d1d5;
 `
 
 export const baseCard = css`
@@ -25,7 +25,7 @@ export const card = css`
 `
 
 export const cardContent = `
-    padding: 1em;
+	padding: 1em;
 `
 
 export const cardActions = css`
@@ -34,47 +34,47 @@ export const cardActions = css`
 `
 
 export const headingNeutral = `
-    font-size: inherit;
-    font-weight: inherit;
-    margin-top: 0;
-    margin-bottom: 0;
+	font-size: inherit;
+	font-weight: inherit;
+	margin-top: 0;
+	margin-bottom: 0;
 `
 
 export const linkUndecorated = `
-    text-decoration: none;
-    color: inherit;
+	text-decoration: none;
+	color: inherit;
 `
 
 export const listInline = `
-    display: inline-block;
-    list-style: none;
-    margin-top: 0;
-    margin-bottom: 0;
-    padding-left: 0;
+	display: inline-block;
+	list-style: none;
+	margin-top: 0;
+	margin-bottom: 0;
+	padding-left: 0;
 
-    & > li {
-        display: inline-block;
-    }
+	& > li {
+		display: inline-block;
+	}
 `
 
 export const listUnstyled = `
-    margin: 0;
-    padding: 0;
-    list-style: none;
+	margin: 0;
+	padding: 0;
+	list-style: none;
 `
 
 export const noSelect = `
-    user-select: none;
-    cursor: default;
+	user-select: none;
+	cursor: default;
 `
 
 export const truncate = `
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
 `
 
 export const truncateWidth = (width: string) => `
-    ${truncate}
-    width: ${width};
+	${truncate}
+	width: ${width};
 `
