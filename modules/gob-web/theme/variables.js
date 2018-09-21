d84fac7ee36a75262c27e13827adf25ebcf49e2a
Hawken Rives <rives@stolaf.edu>
2018-09-21 12:10:53 -0500

    fix some css in styled-components

--- modules/gob-web/theme/variables.js
+++ modules/gob-web/theme/variables.js
@@ -31,13 +31,8 @@ export const sidebarWidth = '270px'
 // semesters
 export const semesterSpacing = '4px'
 
-export const semesterTopPadding = css`
-	${blockEdgePadding};
-`
-export const semesterSidePadding = css`
-    calc(${blockEdgePadding} * 1.5);
-`
+export const semesterSidePadding = `calc(${blockEdgePadding} * 1.5);`
 
 export const semesterPadding = css`
-	padding: ${semesterTopPadding} ${semesterSidePadding};
+	padding: ${blockEdgePadding} ${semesterSidePadding};
 `
