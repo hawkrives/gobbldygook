a6c61a6727c36a17b12262138211d5af59a0400e
Hawken Rives <hawkrives@fastmail.fm>
2018-09-21 01:42:25 -0500

    fix up code for js-yaml update

--- modules/gob-worker-load-data/source/parse-data.js
+++ modules/gob-worker-load-data/source/parse-data.js
@@ -8,7 +8,7 @@ export default function parseData(raw: string, type: InfoFileTypeEnum) {
 		if (type === 'courses') {
 			return JSON.parse(raw)
 		} else if (type === 'areas') {
-			let data = yaml.safeLoad(raw)
+			let data: any = yaml.safeLoad(raw)
 			data.source = raw
 			return data
 		}
