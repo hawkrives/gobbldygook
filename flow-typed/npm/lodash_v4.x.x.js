97817918150ec0ca431153e3d8a5a18db83f6d2c
Hawken Rives <hawkrives@fastmail.fm>
2018-09-21 00:01:48 -0500

    poke at lodash typedef more

--- flow-typed/npm/lodash_v4.x.x.js
+++ flow-typed/npm/lodash_v4.x.x.js
@@ -554,7 +554,7 @@ declare module "lodash" {
       array?: ?$ReadOnlyArray<T>,
       iteratee?: ?FlatMapIteratee<T, U>
     ): Array<U>;
-    flatMap<T: Array<*>, U>(
+    flatMap<T: Array<T>, U>(
       object: T,
       iteratee?: OFlatMapIteratee<T, U>
     ): Array<U>;
