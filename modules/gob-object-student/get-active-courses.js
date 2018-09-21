f8e92f633046c4303407ecd31b9ceff4a9892fe5
Hawken Rives <hawkrives@fastmail.fm>
2018-09-20 21:31:14 -0500

    return to _.filter for an object

--- modules/gob-object-student/get-active-courses.js
+++ modules/gob-object-student/get-active-courses.js
@@ -1,11 +1,12 @@
 // @flow
 
+import filter from 'lodash/filter'
 import uniqBy from 'lodash/uniqBy'
 import flatMap from 'lodash/flatMap'
 import type {StudentType} from './student'
 
 export function getActiveCourses(student: StudentType) {
-	const activeSchedules = student.schedules.filter(s => s.active === true)
+	const activeSchedules = filter(student.schedules, s => s.active === true)
 
 	let courses = flatMap(activeSchedules, s => s.courses)
 	courses = uniqBy(courses.filter(c => c), course => course.clbid)
