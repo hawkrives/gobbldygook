f03ba7a1a5c5b58b27a3daeb681ab84c0ba2a720
Hawken Rives <hawkrives@fastmail.fm>
2018-09-20 23:01:48 -0500

    fix type in listify

--- flow-typed/npm/listify_vx.x.x.js
+++ flow-typed/npm/listify_vx.x.x.js
@@ -2,5 +2,5 @@
 // flow-typed version: <<STUB>>/listify_v1.0.0/flow_v0.64.0
 
 declare module 'listify' {
-  declare export default function listify(Array<mixed>, ?{oxfordComma: boolean}): string;
+  declare export default function listify(Array<any>, ?{oxfordComma: boolean}): string;
 }
