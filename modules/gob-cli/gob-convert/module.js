1033352b93006cd419a53d36c6ed3a8c3aa5c5d7
Hawken Rives <hawkrives@fastmail.fm>
2018-09-21 22:03:23 -0500

    implement gob-convert

---
+++ modules/gob-cli/gob-convert/module.js
@@ -0,0 +1,35 @@
+// @flow
+
+const usage = `
+usage: gob-convert < file
+outputs the converted file to stdout
+`
+
+import meow from 'meow'
+import stdin from 'get-stdin'
+import loadJsonFile from 'load-json-file'
+import {getCourse} from '../lib'
+import {convertStudent} from '@gob/school-st-olaf-college'
+const {version} = require('../package.json')
+
+global.VERSION = version
+
+function args() {
+	return meow(usage, {
+		booleanDefault: false,
+	})
+}
+
+export default async function main() {
+	let {input} = args()
+
+	let data = (input.length ? await loadJsonFile(input[0]) : JSON.parse(await stdin()))
+
+	let hydrated = await convertStudent(data, getCourse)
+
+	for (let schedule of Object.values(hydrated.schedules)) {
+		delete (schedule: any).courses
+	}
+
+	console.log(JSON.stringify(hydrated))
+}
---
+++ modules/gob-cli/lib/index.js
@@ -0,0 +1,3 @@
+// @flow
+
+export {getCourse} from './get-course'
