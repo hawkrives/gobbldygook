71056460fe53de7bf9358c96f61f267c7796f667
Hawken Rives <hawkrives@fastmail.fm>
2018-09-21 19:17:31 -0500

    update find-course-warnings.test to use inline snapshots

--- modules/gob-object-student/__tests__/find-course-warnings.test.js
+++ modules/gob-object-student/__tests__/find-course-warnings.test.js
@@ -1,16 +1,18 @@
-// import {convertTimeStringsToOfferings} from 'sto-sis-time-parser'
 import {
 	checkForInvalidYear,
 	checkForInvalidSemester,
-	// checkForTimeConflicts,
-	// findWarnings,
 } from '../find-course-warnings'
 
 describe('checkForInvalidYear', () => {
 	it('checks for an invalid year on a course', () => {
-		expect(
-			checkForInvalidYear({year: 1994, semester: 1}, 2012),
-		).toMatchSnapshot()
+		expect(checkForInvalidYear({year: 1994, semester: 1}, 2012))
+			.toMatchInlineSnapshot(`
+Object {
+  "msg": "Wrong Year (originally from 1994–95)",
+  "type": "invalid-year",
+  "warning": true,
+}
+`)
 	})
 
 	it('returns null if no semester is present', () => {
@@ -24,7 +26,14 @@ describe('checkForInvalidYear', () => {
 
 describe('checkForInvalidSemester', () => {
 	it('checks for an invalid semester on a course', () => {
-		expect(checkForInvalidSemester({semester: 2}, 5)).toMatchSnapshot()
+		expect(checkForInvalidSemester({semester: 2}, 5))
+			.toMatchInlineSnapshot(`
+Object {
+  "msg": "Wrong Semester (originally from Interim)",
+  "type": "invalid-semester",
+  "warning": true,
+}
+`)
 	})
 
 	it('returns null if no semester is given', () => {
