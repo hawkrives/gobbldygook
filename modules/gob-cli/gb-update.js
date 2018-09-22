e891b2e1aacc7fdcc4a7f32c2ea2228e2757756e
Hawken Rives <hawkrives@fastmail.fm>
2018-09-21 22:02:06 -0500

    start reimplementing gob-cli

--- modules/gob-cli/gb-update.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const {checkForStaleData} = require('./lib/update-local-data-cache')
-
-module.exports = function update() {
-	// grab info.json
-	// apply loadData's algorithm to it
-	return checkForStaleData()
-}
