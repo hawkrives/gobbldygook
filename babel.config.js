81806f5357bcd2489959d524b79af6a87ef95e6e
Hawken Rives <hawkrives@fastmail.fm>
2018-09-22 17:51:24 -0500

    reformat babel config

--- babel.config.js
+++ babel.config.js
@@ -1,36 +1,42 @@
 module.exports = {
-	"presets": [
-		"react",
-		"flow",
-		["env", {
-			"modules": false,
-			"targets": {
-				"browsers": [
-					"last 2 versions",
-					"not android > 0",
-					"not ie > 0"
-				]
-			}
-		}]
+	presets: [
+		'react',
+		'flow',
+		[
+			'env',
+			{
+				modules: false,
+				targets: {
+					browsers: [
+						'last 2 versions',
+						'not android > 0',
+						'not ie > 0',
+					],
+				},
+			},
+		],
 	],
-	"plugins": [
-		"transform-class-properties",
-		"transform-object-rest-spread",
-		"transform-react-display-name",
-		"babel-plugin-styled-components",
-		["transform-runtime", {"polyfill": false, "regenerator": false}]
+	plugins: [
+		'transform-class-properties',
+		'transform-object-rest-spread',
+		'transform-react-display-name',
+		'babel-plugin-styled-components',
+		['transform-runtime', {polyfill: false, regenerator: false}],
 		// turns `import {sum} from 'lodash'`
 		// into `import sum from 'lodash/sum'`
 		// "lodash",
 	],
-	"env": {
-		"test": {
-			"presets": [
-				["env", {
-					"targets": {"node": true},
-					"modules": "commonjs"
-				}]
-			]
-		}
-	}
+	env: {
+		test: {
+			presets: [
+				[
+					'env',
+					{
+						targets: {node: true},
+						modules: 'commonjs',
+					},
+				],
+			],
+		},
+	},
 }
