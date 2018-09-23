0f0d0dd9be1c795e589dd26bb779742dc5d7d2f0
Hawken Rives <hawkrives@fastmail.fm>
2018-09-22 19:25:56 -0500

    enable @babel/plugin-syntax-dynamic-import

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = {
 		],
 	],
 	plugins: [
+		'@babel/plugin-syntax-dynamic-import',
 		'@babel/plugin-proposal-class-properties',
 		'@babel/plugin-proposal-object-rest-spread',
 		'@babel/plugin-transform-react-display-name',
