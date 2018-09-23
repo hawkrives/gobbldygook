90ef094225085f84c5b39f37e75be6d703560a6d
Hawken Rives <hawkrives@fastmail.fm>
2018-09-22 21:52:49 -0500

    actually import the new variables files

--- modules/gob-web/index.js
+++ modules/gob-web/index.js
@@ -4,7 +4,8 @@ import '@babel/polyfill'
 
 import 'typeface-fira-sans'
 import './styles/normalize.scss'
-import 'react-virtualized/styles.css'
+import './styles/css-colors.scss'
+import './styles/css-variables.scss'
 
 import debug from 'debug'
 const log = debug('web')
