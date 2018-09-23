39d389f719ba85f44c56d715ad1b4dae9a36cbed
Hawken Rives <hawkrives@fastmail.fm>
2018-09-23 10:42:58 -0500

    allow sortStudiesByType to operate on AreaQueryType

--- modules/gob-object-student/sort-studies-by-type.js
+++ modules/gob-object-student/sort-studies-by-type.js
@@ -1,7 +1,9 @@
 // @flow
 import sortBy from 'lodash/sortBy'
 
+import {type AreaQuery} from './types'
+
 const types = ['degree', 'major', 'concentration', 'emphasis']
-export function sortStudiesByType(studies: Array<{type: string}>) {
+export function sortStudiesByType(studies: Array<AreaQuery>) {
 	return sortBy(studies, s => types.indexOf(s.type))
 }
