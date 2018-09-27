42f35c7392a7ad4e8bc11bdb78d55f53f9656c43
Hawken Rives <rives@stolaf.edu>
2018-09-27 09:00:45 -0500

    rework collectMatches to use an exhaustive switch case

--- modules/gob-examine-student/source/collect-matches.js
+++ modules/gob-examine-student/source/collect-matches.js
@@ -5,61 +5,86 @@ import uniqBy from 'lodash/uniqBy'
 import stringify from 'stabilize'
 import type {Expression, Requirement, Course} from './types'
 
-/**
- * Collects matched courses from a result object
- * @private
- * @param {Result} expr - the result object to extract matches from
- * @returns {Course[]} matches - the list of matched courses
- */
-export default function collectMatches(
-	expr: Expression | Requirement,
-): Course[] {
-	assertKeys(expr, '$type')
+export default collectMatches
 
-	// start off with absolutely no matches
-	let matches = undefined
+// Collects matched courses from a result object
+function collectMatches(expr: Expression | Requirement): Array<Course> {
+	let matches = justCollectMatches(expr)
+
+	// we either return the matches, or an empty array if it's falsy
+	return matches ? uniqBy(matches, stringify) : []
+}
+
+function justCollectMatches(expr: Expression | Requirement): ?Array<Course> {
+	assertKeys(expr, '$type')
 
 	// if a course expression, and the course was used, return the course in
 	// an array. returning in an array allows the higher-level expressions to
 	// just run `flatten()` to collect all of the courses.
-	// this is the "base case."
-	if (expr.$type === 'course') {
-		/* istanbul ignore else: doesn't matter */
-		if (expr._result === true) {
-			matches = [expr]
-		}
-	} else if (expr.$type === 'requirement') {
-		// next, we have the "run collectMatches on all my children" cases.
-		if ('result' in expr) {
-			matches = collectMatches(expr.result)
-		} else {
-			matches = []
-		}
-	} else if (expr.$type === 'boolean') {
-		if (expr.$booleanType === 'and') {
-			matches = flatMap(expr.$and, collectMatches)
-		} else {
-			matches = flatMap(expr.$or, collectMatches)
-		}
-	} else if (expr.$type === 'of') {
-		matches = flatMap(expr.$of, collectMatches)
-	} else if (expr.$type === 'modifier') {
-		// finally, we have the "pre-computed _matches" cases, where the evaluation
-		// of the expression attached the matches to the expression itself.
-		matches = expr._matches
-	} else if (expr.$type === 'occurrence') {
-		matches = expr._matches
-	} else if (expr.$type === 'reference') {
-		matches = expr._matches
-	} else if (expr.$type === 'where') {
-		matches = expr._matches
-	} else {
-		throw new TypeError(
-			`collectMatches(): unknown expression type "${expr.$type ||
-				'undefined'}"`,
-		)
-	}
 
-	// then we either return the matches, or an empty array if it's falsy
-	return matches ? uniqBy(matches, stringify) : []
+	switch (expr.$type) {
+		// this is the "base case."
+		case 'course': {
+			/* istanbul ignore else: doesn't matter */
+			if (expr._result === true) {
+				return [expr]
+			}
+
+			return null
+		}
+
+		// next, we have the "run collectMatches on all my children" cases.
+		case 'requirement': {
+			if ('result' in expr) {
+				return collectMatches(expr.result)
+			}
+
+			return null
+		}
+		case 'boolean': {
+			if (expr.$booleanType === 'and') {
+				return flatMap(expr.$and, collectMatches)
+			}
+
+			if (expr.$booleanType === 'or') {
+				return flatMap(expr.$or, collectMatches)
+			}
+
+			return null
+		}
+		case 'of': {
+			return flatMap(expr.$of, collectMatches)
+		}
+
+		// next, we have the "pre-computed _matches" cases, where the evaluation
+		// of the expression attached the matches to the expression itself.
+		case 'modifier': {
+			return expr._matches
+		}
+		case 'occurrence': {
+			return expr._matches
+		}
+		case 'reference': {
+			return expr._matches
+		}
+		case 'where': {
+			return expr._matches
+		}
+
+		// finally, we have the "i have no idea what you are" cases
+		case 'filter': {
+			throw new TypeError(
+				'collectMatches(): unknown expression type "filter"',
+			)
+		}
+		case undefined: {
+			throw new TypeError('collectMatches(): undefined expression type')
+		}
+		default: {
+			;(expr.$type: empty)
+			throw new TypeError(
+				`collectMatches(): unknown expression type "${expr.$type}"`,
+			)
+		}
+	}
 }
