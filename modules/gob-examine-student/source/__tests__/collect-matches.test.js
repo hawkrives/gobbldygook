824a668c8d7f7563e8b1d971ce2daf610f2a3bc0
Hawken Rives <rives@stolaf.edu>
2018-09-27 13:36:54 -0500

    update collectMatches tests

--- modules/gob-examine-student/source/__tests__/collect-matches.test.js
+++ modules/gob-examine-student/source/__tests__/collect-matches.test.js
@@ -22,12 +22,12 @@ describe('collectMatches', () => {
 					{
 						$type: 'reference',
 						requirement: 'Child',
-						_matches: [{department: ['ASIAN'], number: 121}],
+						_matches: [{department: 'ASIAN', number: 121}],
 					},
 					{
 						$type: 'reference',
 						requirement: 'Child2',
-						_matches: [{department: ['CSCI'], number: 121}],
+						_matches: [{department: 'CSCI', number: 121}],
 					},
 				],
 			},
@@ -55,12 +55,12 @@ describe('collectMatches', () => {
 					{
 						_result: true,
 						$type: 'course',
-						$course: {department: ['ASIAN'], number: 121},
+						$course: {department: 'ASIAN', number: 121},
 					},
 					{
 						$type: 'reference',
 						requirement: 'Child2',
-						_matches: [{department: ['CSCI'], number: 121}],
+						_matches: [{department: 'CSCI', number: 121}],
 					},
 				],
 			},
@@ -75,7 +75,7 @@ describe('collectMatches', () => {
 			result: {
 				_result: true,
 				$type: 'course',
-				$course: {department: ['ASIAN'], number: 121},
+				$course: {department: 'ASIAN', number: 121},
 			},
 		}
 
@@ -91,8 +91,8 @@ describe('collectMatches', () => {
 				$what: 'children',
 				$children: 'all',
 				_matches: [
-					{department: ['ASIAN'], number: 121},
-					{department: ['CSCI'], number: 121},
+					{department: 'ASIAN', number: 121},
+					{department: 'CSCI', number: 121},
 				],
 			},
 		}
@@ -108,8 +108,8 @@ describe('collectMatches', () => {
 				// the occurrence is empty because the _matches are calculated
 				// in computeOccurrence
 				_matches: [
-					{department: ['ASIAN'], number: 121, year: 2014},
-					{department: ['ASIAN'], number: 121, year: 2015},
+					{department: 'ASIAN', number: 121, year: 2014},
+					{department: 'ASIAN', number: 121, year: 2015},
 				],
 			},
 		}
@@ -131,12 +131,12 @@ describe('collectMatches', () => {
 							{
 								$type: 'course',
 								_result: true,
-								$course: {department: ['ASIAN'], number: 121},
+								$course: {department: 'ASIAN', number: 121},
 							},
 							{
 								$type: 'reference',
 								requirement: 'Child2',
-								_matches: [{department: ['CSCI'], number: 121}],
+								_matches: [{department: 'CSCI', number: 121}],
 							},
 						],
 					},
@@ -146,8 +146,8 @@ describe('collectMatches', () => {
 						$what: 'children',
 						$children: 'all',
 						_matches: [
-							{department: ['MUSIC'], number: 121},
-							{department: ['ESTH'], number: 121},
+							{department: 'MUSIC', number: 121},
+							{department: 'ESTH', number: 121},
 						],
 					},
 				],
@@ -165,8 +165,8 @@ describe('collectMatches', () => {
 				// $where is empty because the _matches are calculated in computeWhere
 				$where: {},
 				_matches: [
-					{department: ['ASIAN'], number: 121},
-					{department: ['CSCI'], number: 121},
+					{department: 'ASIAN', number: 121},
+					{department: 'CSCI', number: 121},
 				],
 			},
 		}
@@ -180,7 +180,7 @@ describe('collectMatches', () => {
 			result: {
 				$type: 'reference',
 				requirement: 'Child',
-				_matches: [{department: ['ASIAN'], number: 121}],
+				_matches: [{department: 'ASIAN', number: 121}],
 			},
 		}
 
