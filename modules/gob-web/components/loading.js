5d5e8fcf37210a1d0a7d98929e889cc9f0a13ca0
Hawken Rives <hawkrives@fastmail.fm>
2018-09-28 22:07:38 -0500

    continue integrating react-spinkit spinner into the app

--- modules/gob-web/components/loading.js
+++ modules/gob-web/components/loading.js
@@ -1,52 +1,49 @@
 // @flow
 import React from 'react'
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
+import cx from 'classnames'
 
-// className: 'sk-wave', divCount: 5
+const divCount = 5
 
-// <div {...props} className={classes}>
-//   {[...Array(spinnerInfo.divCount)].map((_, idx) => <div key={idx} />)}
-// </div>
+const stretchDelay = keyframes`
+	0%,
+	40%,
+	100% {
+		transform: scaleY(0.4);
+	}
+	20% {
+		transform: scaleY(1);
+	}
+`
 
 const Spinner = styled.div`
-  width: 30px;
-  height: 27px;
+	width: 30px;
+	height: 27px;
 
-  color: var(--gray-600);
+	& > div {
+		background-color: currentColor;
+		height: 100%;
+		width: 6px;
+		display: inline-block;
 
-& > div {
-  background-color: currentColor;
-  height: 100%;
-  width: 6px;
-  display: inline-block;
+		animation: ${stretchDelay} 1.2s infinite ease-in-out;
+	}
 
-  animation: sk-stretchdelay 1.2s infinite ease-in-out;
-}
+	& > div:nth-child(2) {
+		animation-delay: -1.1s;
+	}
 
-& > div:nth-child(2) {
-  animation-delay: -1.1s;
-}
+	& > div:nth-child(3) {
+		animation-delay: -1s;
+	}
 
-& > div:nth-child(3) {
-  animation-delay: -1.0s;
-}
+	& > div:nth-child(4) {
+		animation-delay: -0.9s;
+	}
 
-& > div:nth-child(4) {
-  animation-delay: -0.9s;
-}
-
-& > div:nth-child(5) {
-  animation-delay: -0.8s;
-}
-
-@keyframes sk-stretchdelay {
-  0%, 40%, 100% {
-    transform: scaleY(0.4);
-  } 20% {
-    transform: scaleY(1.0);
-  }
-}
-
+	& > div:nth-child(5) {
+		animation-delay: -0.8s;
+	}
 `
 
 const Wrapper = styled.figure`
@@ -55,33 +52,45 @@ const Wrapper = styled.figure`
 	justify-content: center;
 	align-items: center;
 	flex-flow: column nowrap;
+
+	color: var(--gray-600);
+
+	&.info {
+		color: var(--green-600);
+	}
+	&.warning {
+		color: var(--orange-600);
+	}
+	&.error {
+		color: var(--red-600);
+	}
 `
 
 const Message = styled.figcaption`
 	font-weight: 300;
 	margin-top: 1em;
-	color: var(--gray);
-
-	&.info {
-		color: var(--green);
-	}
-	&.warning {
-		color: var(--orange);
-	}
-	&.error {
-		color: var(--red);
-	}
 `
 
 type LoadingProps = {
 	children?: any,
-	className?: string,
+	error?: boolean,
+	info?: boolean,
+	warning?: boolean,
 }
 
-export default function Loading({className, children}: LoadingProps) {
+export default function Loading({
+	info,
+	error,
+	warning,
+	children,
+}: LoadingProps) {
 	return (
-		<Wrapper className={className}>
-			<Spinner />
+		<Wrapper className={cx({info, error, warning})}>
+			<Spinner>
+				{[...Array(divCount)].map((_, idx) => (
+					<div key={idx} />
+				))}
+			</Spinner>
 			<Message>{children}</Message>
 		</Wrapper>
 	)
