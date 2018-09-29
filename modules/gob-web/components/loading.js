1e7c2aae64656ebf492a7aef82281077b66a1a33
Hawken Rives <hawkrives@fastmail.fm>
2018-09-28 22:01:51 -0500

    start removing react-spinkit

--- modules/gob-web/components/loading.js
+++ modules/gob-web/components/loading.js
@@ -1,8 +1,53 @@
 // @flow
 import React from 'react'
 import styled from 'styled-components'
-import * as theme from '../theme'
-import Spinner from 'react-spinkit'
+
+// className: 'sk-wave', divCount: 5
+
+// <div {...props} className={classes}>
+//   {[...Array(spinnerInfo.divCount)].map((_, idx) => <div key={idx} />)}
+// </div>
+
+const Spinner = styled.div`
+  width: 30px;
+  height: 27px;
+
+  color: var(--gray-600);
+
+& > div {
+  background-color: currentColor;
+  height: 100%;
+  width: 6px;
+  display: inline-block;
+
+  animation: sk-stretchdelay 1.2s infinite ease-in-out;
+}
+
+& > div:nth-child(2) {
+  animation-delay: -1.1s;
+}
+
+& > div:nth-child(3) {
+  animation-delay: -1.0s;
+}
+
+& > div:nth-child(4) {
+  animation-delay: -0.9s;
+}
+
+& > div:nth-child(5) {
+  animation-delay: -0.8s;
+}
+
+@keyframes sk-stretchdelay {
+  0%, 40%, 100% {
+    transform: scaleY(0.4);
+  } 20% {
+    transform: scaleY(1.0);
+  }
+}
+
+`
 
 const Wrapper = styled.figure`
 	margin: 0;
@@ -36,7 +81,7 @@ type LoadingProps = {
 export default function Loading({className, children}: LoadingProps) {
 	return (
 		<Wrapper className={className}>
-			<Spinner name="wave" color={theme.gray600} fadeIn="none" />
+			<Spinner />
 			<Message>{children}</Message>
 		</Wrapper>
 	)
