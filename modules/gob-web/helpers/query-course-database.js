416c70dd03785c2627e9072f3a622165572969fc
Hawken Rives <hawkrives@fastmail.fm>
2018-09-30 16:46:11 -0500

    clean up some of the implementation of queryCourseDatabase

--- modules/gob-web/helpers/query-course-database.js
+++ modules/gob-web/helpers/query-course-database.js
@@ -1,46 +1,38 @@
+// @flow
+
 import {db} from './db'
 import {buildQueryFromString} from '@gob/search-queries'
 import compact from 'lodash/compact'
-import filter from 'lodash/filter'
-import map from 'lodash/map'
-import some from 'lodash/some'
 import toPairs from 'lodash/toPairs'
 import fromPairs from 'lodash/fromPairs'
-import debug from 'debug'
-const log = debug('web:database')
 
-export default function queryCourseDatabase(queryString, baseQuery = {}) {
+export function queryCourseDatabase(
+	queryString: string,
+	baseQuery: Object = {},
+) {
 	let queryObject = buildQueryFromString(queryString, {
 		words: true,
 		profWords: true,
 	})
 
-	let query = {}
-	if ('year' in queryObject || 'semester' in queryObject) {
-		query = queryObject
-	} else {
-		query = {...baseQuery, ...queryObject}
-	}
-
 	// make sure that all values are wrapped in arrays
-	query = toPairs(query)
-	query = map(query, ([key, val]) => {
-		if (!Array.isArray(val)) {
-			val = [val]
-		}
-		if (some(val, v => v === undefined)) {
-			val = compact(val)
-		}
-		return [key, val]
-	})
-	query = filter(query, ([_, val]) => val.length)
-	query = fromPairs(query)
+	let filteredQuery = toPairs({...baseQuery, ...queryObject})
+		.map(([key, val]) => {
+			if (!Array.isArray(val)) {
+				val = [val]
+			}
+			if (val.some(v => v === undefined)) {
+				val = compact(val)
+			}
+			return [key, val]
+		})
+		.filter(([_, val]) => val.length)
 
-	log('query object', query)
+	let finalQuery = fromPairs(filteredQuery)
 
 	return db
 		.store('courses')
-		.query(query)
+		.query(finalQuery)
 		.catch(
 			err =>
 				new Error(
