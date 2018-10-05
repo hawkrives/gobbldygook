de69d3627f1e2886567fe518a806919f7adc2e19
Hawken Rives <rives@stolaf.edu>
2018-10-05 14:56:28 -0500

    update student-summary tests for the new look

--- modules/gob-web/modules/student/__tests__/student-summary.test.js
+++ modules/gob-web/modules/student/__tests__/student-summary.test.js
@@ -87,106 +87,6 @@ describe('DateSummary', () => {
 			shallow(<DateSummary matriculation={2000} graduation={2006} />),
 		).toMatchSnapshot()
 	})
-
-	it('disables editing the matriculation year if onChangeMatriculation is not given', () => {
-		const tree = shallow(
-			<DateSummary matriculation={2012} graduation={2016} />,
-		)
-
-		expect(tree).toMatchSnapshot()
-		expect(
-			tree
-				.find('ContentEditable')
-				.at(0)
-				.prop('disabled'),
-		).toBe(true)
-	})
-
-	it('disables editing the graduation year if onChangeGraduation is not given', () => {
-		const tree = shallow(
-			<DateSummary matriculation={2012} graduation={2016} />,
-		)
-
-		expect(tree).toMatchSnapshot()
-		expect(
-			tree
-				.find('ContentEditable')
-				.at(1)
-				.prop('disabled'),
-		).toBe(true)
-	})
-
-	it('allows editing the matriculation year if onChangeMatriculation is given', () => {
-		const onChangeMatriculation = jest.fn()
-		const tree = shallow(
-			<DateSummary
-				onChangeMatriculation={onChangeMatriculation}
-				matriculation={2012}
-				graduation={2016}
-			/>,
-		)
-
-		expect(tree).toMatchSnapshot()
-		expect(
-			tree
-				.find('ContentEditable')
-				.at(0)
-				.prop('disabled'),
-		).toBe(false)
-	})
-
-	it('allows editing the graduation year if onChangeGraduation is given', () => {
-		const onChangeGraduation = jest.fn()
-		const tree = shallow(
-			<DateSummary
-				onChangeGraduation={onChangeGraduation}
-				matriculation={2012}
-				graduation={2016}
-			/>,
-		)
-
-		expect(tree).toMatchSnapshot()
-		expect(
-			tree
-				.find('ContentEditable')
-				.at(1)
-				.prop('disabled'),
-		).toBe(false)
-	})
-
-	it('calls onChangeMatriculation when the matriculation year is changed', () => {
-		const onChangeMatriculation = jest.fn()
-		const tree = shallow(
-			<DateSummary
-				onChangeMatriculation={onChangeMatriculation}
-				matriculation={2012}
-				graduation={2016}
-			/>,
-		)
-
-		expect(tree).toMatchSnapshot()
-		tree.find('ContentEditable')
-			.at(0)
-			.simulate('blur', '2010')
-		expect(onChangeMatriculation).toHaveBeenCalledWith('2010')
-	})
-
-	it('calls onChangeGraduation when the graduation year is changed', () => {
-		const onChangeGraduation = jest.fn()
-		const tree = shallow(
-			<DateSummary
-				onChangeGraduation={onChangeGraduation}
-				matriculation={2012}
-				graduation={2016}
-			/>,
-		)
-
-		expect(tree).toMatchSnapshot()
-		tree.find('ContentEditable')
-			.at(1)
-			.simulate('blur', '2018')
-		expect(onChangeGraduation).toHaveBeenCalledWith('2018')
-	})
 })
 
 describe('DegreeSummary', () => {
@@ -290,115 +190,28 @@ describe('Header', () => {
 		expect(tree).toMatchSnapshot()
 	})
 
-	describe('with an avatar', () => {
-		it('handles the "can graduate" status', () => {
-			const tree = shallow(
-				<Header
-					canGraduate={true}
-					name="Susan"
-					helloMessage="Welcome, "
-					showAvatar={true}
-				/>,
-			)
-
-			expect(tree).toMatchSnapshot()
-		})
-		it('handles the "cannot graduate" status', () => {
-			const tree = shallow(
-				<Header
-					canGraduate={false}
-					name="Susan"
-					helloMessage="Welcome, "
-					showAvatar={true}
-				/>,
-			)
-
-			expect(tree).toMatchSnapshot()
-		})
-	})
-
-	describe('without an avatar', () => {
-		it('handles the "can graduate" status', () => {
-			const tree = shallow(
-				<Header
-					canGraduate={true}
-					name="Susan"
-					helloMessage="Welcome, "
-					showAvatar={false}
-				/>,
-			)
-
-			expect(tree).toMatchSnapshot()
-		})
-		it('handles the "cannot graduate" status', () => {
-			const tree = shallow(
-				<Header
-					canGraduate={false}
-					name="Susan"
-					helloMessage="Welcome, "
-					showAvatar={false}
-				/>,
-			)
-
-			expect(tree).toMatchSnapshot()
-		})
-	})
-
-	it('disables editing the name if onChangeName is not given', () => {
+	it('handles the "can graduate" status', () => {
 		const tree = shallow(
 			<Header
-				canGraduate={false}
-				name="Natasha"
+				canGraduate={true}
+				name="Susan"
 				helloMessage="Welcome, "
 				showAvatar={true}
 			/>,
 		)
 
 		expect(tree).toMatchSnapshot()
-		expect(
-			tree
-				.find('ContentEditable')
-				.at(0)
-				.prop('disabled'),
-		).toBe(true)
 	})
-	it('allows editing the name if onChangeName is given', () => {
-		const onChangeName = jest.fn()
+	it('handles the "cannot graduate" status', () => {
 		const tree = shallow(
 			<Header
 				canGraduate={false}
-				onChangeName={onChangeName}
-				name="Natasha"
+				name="Susan"
 				helloMessage="Welcome, "
 				showAvatar={true}
 			/>,
 		)
 
 		expect(tree).toMatchSnapshot()
-		expect(
-			tree
-				.find('ContentEditable')
-				.at(0)
-				.prop('disabled'),
-		).toBe(false)
-	})
-
-	it('calls onChangeName when the name is changed', () => {
-		const onChangeName = jest.fn()
-		const tree = shallow(
-			<Header
-				canGraduate={false}
-				onChangeName={onChangeName}
-				name="Natasha"
-				helloMessage="Welcome, "
-				showAvatar={true}
-			/>,
-		)
-
-		expect(tree).toMatchSnapshot()
-		tree.find('ContentEditable')
-			.at(0)
-			.simulate('blur', 'Black Widow')
-		expect(onChangeName).toHaveBeenCalledWith('Black Widow')
 	})
 })
