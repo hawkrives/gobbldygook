128ba45ec728cee1d3f7f2dbd5fddd5ef5999ac1
Hawken Rives <rives@stolaf.edu>
2018-10-08 16:05:12 -0500

    update StudentSummary to handle only active courses

--- modules/gob-web/modules/student/student-summary.js
+++ modules/gob-web/modules/student/student-summary.js
@@ -4,7 +4,7 @@ import React from 'react'
 import cx from 'classnames'
 import listify from 'listify'
 import sample from 'lodash/sample'
-import {List, Set} from 'immutable'
+import {List} from 'immutable'
 import {connect} from 'react-redux'
 import {Card} from '../../components/card'
 import {AvatarLetter} from '../../components/avatar-letter'
@@ -97,11 +97,8 @@ class StudentSummary extends React.Component<Props, State> {
 	countCredits = async (props: Props) => {
 		let {student} = props
 		let {schedules, fabrications} = student
-		let promises = Set(schedules.toList().flatMap(s => s.clbids)).map(
-			clbid => getCourse({clbid}, fabrications),
-		)
-		let courses = await Promise.all(promises)
-		let credits = countCredits([...courses])
+		let courses = await student.activeCourses(getCourse)
+		let credits = countCredits(courses)
 		this.setState(() => ({creditsTaken: credits}))
 	}
 
