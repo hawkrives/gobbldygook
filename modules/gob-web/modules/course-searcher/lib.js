157f438d497fb353b88f3eefd68f6ce3f78baf0c
Hawken Rives <hawkrives@gmail.com>
2018-10-10 16:40:02 -0500

    sort years after extraction
    closes #2307

--- modules/gob-web/modules/course-searcher/lib.js
+++ modules/gob-web/modules/course-searcher/lib.js
@@ -111,7 +111,7 @@ export function sortAndGroup(
 	let {sorting, grouping, filtering, limiting} = args
 	console.time('query: grouping/sorting')
 
-	let years = results.map(c => c.year).toSet()
+	let years = results.map(c => c.year).toSet().sort()
 	if (limiting) {
 		let year = parseInt(limiting, 10)
 		results = results.filter(c => c.year === year)
