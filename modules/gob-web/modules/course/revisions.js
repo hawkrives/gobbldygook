e35a4c85518263d90ef68377ed1c726ff57757da
Drew Volz <volz.drew@gmail.com>
2019-01-21 18:13:47 -0800

    switch to table format for revisions

--- modules/gob-web/modules/course/revisions.js
+++ modules/gob-web/modules/course/revisions.js
@@ -3,16 +3,38 @@
 import * as React from 'react'
 import toPairs from 'lodash/toPairs'
 import styled from 'styled-components'
-import {BulletedList, ListItem, IndentedListItem} from '../../components/list'
 import type {Course as CourseType} from '@gob/types'
 
 type Props = {
 	course: CourseType,
 }
 
-const ListContainer = styled.div`
-	margin-left: 36px;
+const RevisionsTable = styled.table`
+	border-collapse: collapse;
+	border-style: hidden;
 `
+
+const TableHead = styled.thead``
+
+const TableBody = styled.tbody``
+
+const Head = styled.th`
+	border: 1px solid black;
+	padding: 7px;
+	text-align: left;
+`
+
+const Row = styled.tr`
+	border: 1px solid black;
+`
+
+const Data = styled.td`
+	border: 1px solid black;
+	padding: 7px;
+	text-align: left;
+`
+
+const RevisionDate = styled.h2``
 
 export class Revisions extends React.Component<Props> {
 	render() {
@@ -24,33 +46,40 @@ export class Revisions extends React.Component<Props> {
 
 		const revisions = [...course.revisions].reverse()
 
-		const RevisionsList = revisions.map((items, index) => {
+		const Table = revisions.map((items, index) => {
 			const revisionDate = new Intl.DateTimeFormat('en-US').format(
 				new Date(items['_updated']),
 			)
 
 			return (
-				<ListContainer key={index}>
-					<BulletedList>
-						<ListItem>{revisionDate}</ListItem>
-						{toPairs(items).map(
-							([key, value]) =>
-								key === 'offerings' ? null : key !==
-								'_updated' ? (
-									<IndentedListItem key={key + index}>
-										{key}: {value}
-									</IndentedListItem>
-								) : null,
+				<>
+					<RevisionDate>{revisionDate}</RevisionDate>
+					<RevisionsTable>
+						<TableHead>
+							<Row>
+								<Head>Key</Head>
+								<Head>Before</Head>
+							</Row>
+						</TableHead>
+						{toPairs(items).map(([key, value]) =>
+							key === 'offerings' ? null : key !== '_updated' ? (
+								<TableBody key={key + index}>
+									<Row>
+										<Data>{key}</Data>
+										<Data>{value}</Data>
+									</Row>
+								</TableBody>
+							) : null,
 						)}
-					</BulletedList>
-				</ListContainer>
+					</RevisionsTable>
+				</>
 			)
 		})
 
 		return (
 			<details>
 				<summary>Revisions</summary>
-				{RevisionsList}
+				{Table}
 			</details>
 		)
 	}
