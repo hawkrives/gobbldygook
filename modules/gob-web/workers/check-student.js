5b9e118e48dbc63d7d2420322a75ef10c2e4f1d2
Drew Volz <volz.drew@gmail.com>
2025-04-05 13:32:33 -0700

    fix student.hashCode is not a function

--- modules/gob-web/workers/check-student.js
+++ modules/gob-web/workers/check-student.js
@@ -66,8 +66,28 @@ async function checkStudentAgainstArea(
 
 const memoized: typeof checkStudentAgainstArea = mem(checkStudentAgainstArea, {
 	cache: new QuickLRU({maxSize: 8}),
-	cacheKey: (student: Student, area: ParsedHansonFile) =>
-		JSON.stringify([student.hashCode(), area]),
+	cacheKey: (student: Student, area: ParsedHansonFile) => {
+		let studentHash
+
+		if (typeof student.hashCode === 'function') {
+			studentHash = student.hashCode()
+		} else {
+			const id = student.id || 'unknown'
+			let timestamp = Date.now()
+
+			if (student.dateLastModified) {
+				if (student.dateLastModified instanceof Date) {
+					timestamp = student.dateLastModified.getTime()
+				} else if (typeof student.dateLastModified === 'string') {
+					timestamp = new Date(student.dateLastModified).getTime()
+				}
+			}
+
+			studentHash = `${id}-${timestamp}`
+		}
+
+		return JSON.stringify([studentHash, area])
+	},
 	maxAge: 60000,
 })
 
