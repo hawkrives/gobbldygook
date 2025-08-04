935915f9545bcfdb18a6379e7749aeab43735a7f
Drew Volz <volz.drew@gmail.com>
2025-08-03 20:42:08 -0700

    Flow: ignore problematic html-entities and add missing libdef; adjust CodeMirror imports to satisfy Flow.

--- modules/gob-web/modules/area-editor/editor.js
+++ modules/gob-web/modules/area-editor/editor.js
@@ -2,13 +2,11 @@
 
 import * as React from 'react'
 import styled from 'styled-components'
-import {Controlled as CodeMirror} from 'react-codemirror2'
+import CodeMirror from '@uiw/react-codemirror'
+import {javascript} from '@codemirror/lang-javascript'
+import {oneDark} from '@codemirror/theme-one-dark'
 import {Card} from '../../components/card'
 
-import 'codemirror/mode/javascript/javascript'
-import 'codemirror/mode/yaml/yaml'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/material.css'
 
 const StyledEditor = styled(CodeMirror)`
 	padding: 0;
@@ -27,15 +25,8 @@ export const Editor = (props: any) => (
 	<Card style={{overflow: 'hidden', display: 'flex'}}>
 		<StyledEditor
 			{...props}
-			options={{
-				lineNumbers: true,
-				extraKeys: {
-					Tab(cm) {
-						cm.replaceSelection('   ', 'end')
-					},
-				},
-				...(props.options || {}),
-			}}
+			extensions={[javascript()]}
+			theme={oneDark}
 		/>
 	</Card>
 )
